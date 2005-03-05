a8f473bf87366ad5842ce12b62511fd4c0395b16
Ricardo Garcia <rickg22@codeblocks.org>
2005-03-05 05:20:23 +0000

    moved "xtra_res" files to the SDK. And added copyright notice (they were adapted from the wxxrc library)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@166 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/sdk/xtra_res.h
@@ -0,0 +1,29 @@
+#ifndef XTRA_RES_H
+#define XTRA_RES_H
+
+#include <wx/wxprec.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/xrc/xml.h>
+#include <wx/toolbar.h>
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+
+class WXDLLEXPORT wxXmlResourceHandler;
+
+class wxToolBarAddOnXmlHandler : public wxXmlResourceHandler
+{
+public:
+    wxToolBarAddOnXmlHandler();
+    virtual wxObject *DoCreateResource();
+    virtual bool CanHandle(wxXmlNode *node);
+
+protected:
+    bool m_isInside;
+    bool m_isAddon;
+    wxToolBar *m_toolbar;
+};
+
+#endif
