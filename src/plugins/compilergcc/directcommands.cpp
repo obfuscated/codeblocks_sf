71e68360c12251e6b1773ecbccea6819c7c4b053
Ricardo Garcia <rickg22@codeblocks.org>
2005-04-10 02:45:02 +0000

    + Added maintenance comments and CVS ID tags to directcommands.cpp
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@275 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/compilergcc/directcommands.cpp
+++ src/plugins/compilergcc/directcommands.cpp
@@ -1,3 +1,28 @@
+/*
+* This file is part of Code::Blocks Studio, an open-source cross-platform IDE
+* Copyright (C) 2003  Yiannis An. Mandravellos
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* Contact e-mail: Yiannis An. Mandravellos <mandrav@codeblocks.org>
+* Program URL   : http://www.codeblocks.org
+*
+* $Id$
+* $Date$
+*/
+
 #include <wx/log.h>
 #include <wx/intl.h>
 #include <wx/filename.h>
