9db3f1d12a94d3b9a3fd10e98ac1ce2976ff6c72
Bart³omiej ¦wiêcki <byo@codeblocks.org>
2005-06-29 12:40:54 +0000

    Added mouse-blocking event handler for default widget preview
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@490 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmith/defwidgets/wxstogglebutton.cpp
+++ src/plugins/contrib/wxSmith/defwidgets/wxstogglebutton.cpp
@@ -6,10 +6,10 @@ WXS_ST_BEGIN(wxsToggleButtonStyles)
     WXS_ST_DEFAULTS()
 WXS_ST_END(wxsToggleButtonStyles)
 
-wxsDWDefineBegin(wxsToggleButton,wxToggleButton,
+wxsDWDefineBeginExt(wxsToggleButton,wxToggleButton,
     ThisWidget = new wxToggleButton(parent,id,label,pos,size,style);
     ThisWidget->SetValue(checked);
-    )
+    ,true)
     wxsDWDefStr(label,"Label:","");
     wxsDWDefBool(checked,"Checked:",false);
 wxsDWDefineEnd()
--- src/plugins/contrib/wxSmith/wxseditor.h
+++ src/plugins/contrib/wxSmith/wxseditor.h
@@ -1,7 +1,6 @@
 #ifndef WXSEDITOR_H
 #define WXSEDITOR_H
 
-#include <settings.h>
 #include <editorbase.h>
 
 class wxsResource;
