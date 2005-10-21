12a4e4ee83423c9aa0d785b0952c998830a3563e
Paul A. Jimenez <ceniza@codeblocks.org>
2005-10-21 02:13:41 +0000

    Changed a tab to spaces
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1033 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/help_plugin/help_common.cpp
+++ src/plugins/contrib/help_plugin/help_common.cpp
@@ -8,14 +8,14 @@ int HelpCommon::m_DefaultHelpIndex = -1;
 
 void HelpCommon::LoadHelpFilesVector(HelpCommon::HelpFilesVector &vect)
 {
-	vect.clear();
+  vect.clear();
   long cookie;
   wxString entry;
   wxConfigBase *conf = ConfigManager::Get();
   wxString oldPath = conf->GetPath();
   conf->SetPath(_T("/help_plugin"));
   bool cont = conf->GetFirstEntry(entry, cookie);
-  
+
   while (cont)
   {
     if (entry == _T("default"))
@@ -25,16 +25,16 @@ void HelpCommon::LoadHelpFilesVector(HelpCommon::HelpFilesVector &vect)
     else
     {
       wxString file = conf->Read(entry, wxEmptyString);
-      
+
       if (!file.IsEmpty())
       {
         vect.push_back(make_pair(entry, file));
       }
     }
-    
+
     cont = conf->GetNextEntry(entry, cookie);
   }
-  
+
   conf->SetPath(oldPath);
 }
 
@@ -45,17 +45,17 @@ void HelpCommon::SaveHelpFilesVector(HelpCommon::HelpFilesVector &vect)
   wxString oldPath = conf->GetPath();
   conf->SetPath(_T("/help_plugin"));
   HelpFilesVector::iterator it;
-  
+
   for (it = vect.begin(); it != vect.end(); ++it)
   {
     wxString file = it->second;
-    
+
     if (!file.IsEmpty())
     {
       conf->Write(it->first, file);
     }
   }
-  
+
   conf->Write(_T("default"), m_DefaultHelpIndex);
   conf->SetPath(oldPath);
 }
