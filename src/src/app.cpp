b196720e6f83a3b38ecbbc60a0e23ad43e00bafe
Yiannis Mandravellos <mandrav@codeblocks.org>
2005-10-21 12:56:08 +0000

    * Added some eye-candy (courtesy by Thomas)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1044 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/src/app.cpp
+++ src/src/app.cpp
@@ -396,12 +396,12 @@ void CodeBlocksApp::ShowSplashScreen()
 	if (!m_NoSplash && ConfigManager::Get()->Read(_T("/environment/show_splash"), 1) == 1)
 	{
 		wxBitmap bitmap;
-		if (bitmap.LoadFile(CFG_READ(_T("/data_path")) + _T("/images/splash.png"), wxBITMAP_TYPE_PNG))
+		if (bitmap.LoadFile(CFG_READ(_T("/data_path")) + _T("/images/splash_new.png"), wxBITMAP_TYPE_PNG))
 		{
 			m_pSplash = new wxSplashScreen(bitmap,
 										wxSPLASH_CENTRE_ON_SCREEN,// | wxSPLASH_TIMEOUT,
 										6000, NULL, -1, wxDefaultPosition, wxDefaultSize,
-										wxSIMPLE_BORDER);// | wxSTAY_ON_TOP);
+										wxBORDER_NONE);
 		}
 		wxSafeYield();
 	}
