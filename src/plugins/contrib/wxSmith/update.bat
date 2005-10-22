0b9031326502734eec065ef0cc1d858a40e5229e
Bart³omiej ¦wiêcki <byo@codeblocks.org>
2005-10-22 14:06:13 +0000

    update.bat will put all resources also into output directory
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1062 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmith/update.bat
+++ src/plugins/contrib/wxSmith/update.bat
@@ -1,4 +1,7 @@
 @echo off
 zip ..\..\..\devel\share\CodeBlocks\wxsmith.zip *.xrc
+zip ..\..\..\output\share\CodeBlocks\wxsmith.zip *.xrc
 md ..\..\..\devel\share\CodeBlocks\images\wxsmith > nul 2>&1
+md ..\..\..\output\share\CodeBlocks\images\wxsmith > nul 2>&1
 copy smithicons\*.* ..\..\..\devel\share\CodeBlocks\images\wxsmith\ > nul 2>&1
+copy smithicons\*.* ..\..\..\output\share\CodeBlocks\images\wxsmith\ > nul 2>&1
