0a1450a64f38d18babfa5724c767b2cf9754631b
Yiannis Mandravellos <mandrav@codeblocks.org>
2005-10-23 20:58:31 +0000

    * Fixed debugger plugin not working correctly with unicode wx.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1067 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/debuggergdb/debuggergdb.cpp
+++ src/plugins/debuggergdb/debuggergdb.cpp
@@ -764,7 +764,7 @@ wxString DebuggerGDB::GetNextOutputLine(bool useStdErr)
 			useStdErr ? m_pProcess->IsErrorAvailable() : m_pProcess->IsInputAvailable() &&
 			!m_pOut->Eof())
 	{
-		char ch = m_pOut->GetC();
+		wxChar ch = m_pOut->GetC();
 		if (ch == _T('\n') || ch == _T('\r'))
 		{
 			while (useStdErr ? m_pProcess->IsErrorAvailable() : m_pProcess->IsInputAvailable() &&
