b026010dae8f099dd277bf5a087b39d7b56f4cf9
Paul A. Jimenez <ceniza@codeblocks.org>
2005-11-09 13:40:52 +0000

    The filename for the exported file is now suggested from the source filename
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1221 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/source_exporter/exporter.cpp
+++ src/plugins/contrib/source_exporter/exporter.cpp
@@ -191,7 +191,7 @@ void Exporter::ExportFile(BaseExporter *exp, const wxString &default_extension, 
 
   EditorManager *em = EDMAN();
   cbEditor *cb = em->GetBuiltinActiveEditor();
-  wxString filename = wxFileSelector(_("Choose the filename"), _T(""), _T(""), default_extension, wildcard, wxSAVE | wxOVERWRITE_PROMPT);
+  wxString filename = wxFileSelector(_("Choose the filename"), _T(""), wxFileName(cb->GetFilename()).GetName() + _T(".") + default_extension, default_extension, wildcard, wxSAVE | wxOVERWRITE_PROMPT);
 
   if (filename.IsEmpty())
   {
