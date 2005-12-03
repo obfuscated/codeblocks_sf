465be334ee3a58017191fac0adbf478d76d47a52
Yiannis Mandravellos <mandrav@codeblocks.org>
2005-12-03 22:20:10 +0000

    * Added missing files sdk/projectfile.cpp|h
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1435 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/sdk/projectfile.h
@@ -0,0 +1,169 @@
+#ifndef PROJECTFILE_H
+#define PROJECTFILE_H
+
+#include "settings.h"
+#include "globals.h"
+#include <wx/dynarray.h>
+#include <wx/filename.h>
+#include <wx/list.h>
+#include <wx/treectrl.h>
+
+#include "blockallocated.h"
+
+class cbProject;
+class ProjectBuildTarget;
+class pfDetails;
+
+WX_DECLARE_HASH_MAP(ProjectBuildTarget*, pfDetails*, wxPointerHash, wxPointerEqual, PFDMap);
+
+/** Represents a file in a Code::Blocks project. */
+class ProjectFile  : public BlockAllocated<ProjectFile, 1000>
+{
+    public:
+        /// Constructor
+        ProjectFile(cbProject* prj);
+        /// Destructor
+        ~ProjectFile();
+
+        /** Make this file belong to an additional build target.
+          * @param targetName The build target to add this file to. */
+        void AddBuildTarget(const wxString& targetName);
+
+        /** Rename a build target this file belongs in.
+          * @param oldTargetName The build target's old name.
+          * @param newTargetName The build target's new name.
+          * @note This does *not* change the build target's name, just the reference in the project file.
+          * This is actually used by cbProject::RenameBuildTarget(). */
+        void RenameBuildTarget(const wxString& oldTargetName, const wxString& newTargetName);
+
+        /** Remove this file from the specified build target.
+          * @param targetName The build target's name to remove this file from. */
+        void RemoveBuildTarget(const wxString& targetName);
+
+        /** Show the file properties dialog.
+          * @param parent The parent window for the dialog (can be NULL).
+          * @return True if the user closed the dialog with "OK", false if closed it with "Cancel".
+          */
+        bool ShowOptions(wxWindow* parent);
+
+        // take as example the relative file sdk/cbProject.cpp
+        /** @return The relative (to the project) filename without extension. */
+        wxString GetBaseName(); // returns sdk/cbProject
+
+        /** @return The generated object filename. */
+        const wxString& GetObjName(); // returns sdk/cbProject.o
+
+        /** Set the generated object filename.
+          * @param name The filename for the generated object. */
+        void SetObjName(const wxString& name);
+
+        /** @return The parent project. */
+        cbProject* GetParentProject(){ return project; }
+
+        /** This is called automatically when adding/removing build targets.
+          * @param target A pointer to the build target whose file details should be updated. */
+        void UpdateFileDetails(ProjectBuildTarget* target = 0);
+
+        /** Access the file details for this project file for the specified target.
+          * @param target A pointer to the build target whose file details should be updated.
+          * @return The details for this project file for the specified build target. */
+        const pfDetails& GetFileDetails(ProjectBuildTarget* target);
+
+        /** Set the visual state (modified, read-only, etc).
+          * @param state The new visual state. */
+		void SetFileState(FileVisualState state);
+
+        /** @return The visual state (modified, read-only, etc). */
+		FileVisualState GetFileState();
+
+        /** The full filename of this file. Usually you need to read from it and never write to it.
+          * @note If you set this anywhere in code,
+          * you *MUST* call UpdateFileDetails() afterwards or it won't compile anymore
+          */
+        wxFileName file;
+
+        /** The relative (to the project) filename of this file. Usually you need to read from it and never write to it.
+          * @note If you set this anywhere in code,
+          * you *MUST* call UpdateFileDetails() afterwards or it won't compile anymore
+          */
+        wxString relativeFilename;
+
+        /** The relative filename to the common top-level path.
+          * This is used mainly for the tree, as this is guaranteed to not contain '..' */
+        wxString relativeToCommonTopLevelPath;
+
+        /** Compile flag. If it's true, the file is compiled (generates object file) else it is not. */
+        bool compile;
+
+        /** Link flag. If it's true, the (generated object) file is linked else it is not. */
+        bool link;
+
+        /** The weight. This is a number between 0 and 100 (defaults to 50).
+          * Files with smaller weights are compiled earlier than those with larger weights. */
+        unsigned short int weight;
+
+        /** If true, the file is open inside an editor. */
+        bool editorOpen; // layout info
+
+        /** The last known caret position in an editor for this file. */
+        int editorPos; // layout info
+
+        /** The last known caret line in an editor for this file. */
+        int editorTopLine; // layout info
+
+        /** A custom build command for this file. @c useCustomBuildCommand must be true for this to be used. */
+        wxString buildCommand;
+
+        /** If true, @c buildCommand is used. */
+        bool useCustomBuildCommand;
+
+        /** DEPRECATED: A custom command to generate dependencies for this file. */
+        wxString customDeps;
+
+        /** DEPRECATED: If true, @c customDeps is used for dependencies generation. */
+        bool autoDeps;
+
+        /** The compiler variable used for this file (e.g CPP, CC, etc). */
+        wxString compilerVar;
+
+        /** An array of strings, containing the names of all the build targets this file belongs to. */
+        wxArrayString buildTargets;
+    protected:
+        friend class cbProject;
+        void DoUpdateFileDetails(ProjectBuildTarget* target);
+        cbProject* project;
+        FileVisualState m_VisualState;
+        wxTreeItemId m_TreeItemId; // set by the project when building the tree
+        wxString m_ObjName;
+        PFDMap m_PFDMap;
+};
+WX_DECLARE_LIST(ProjectFile, FilesList);
+
+/** This is a helper class that caches various filenames for one ProjectFile.
+  * These include the source filename, the generated object filename,
+  * relative and absolute versions of the above, etc.
+  * Mainly used by the compiler...
+  */
+class pfDetails : public BlockAllocated<pfDetails, 1000>
+{
+    public:
+        pfDetails(ProjectBuildTarget* target, ProjectFile* pf);
+        void Update(ProjectBuildTarget* target, ProjectFile* pf);
+        // all the members below, are set in the constructor
+        wxString source_file;
+        wxString object_file;
+        wxString dep_file;
+        wxString object_dir;
+        wxString dep_dir;
+        // those below, have no UnixFilename() applied, nor QuoteStringIfNeeded()
+        wxString source_file_native;
+        wxString object_file_native;
+        wxString dep_file_native;
+        wxString object_dir_native;
+        wxString dep_dir_native;
+        wxString source_file_absolute_native;
+        wxString object_file_absolute_native;
+        wxString dep_file_absolute_native;
+};
+
+#endif // PROJECTFILE_H
