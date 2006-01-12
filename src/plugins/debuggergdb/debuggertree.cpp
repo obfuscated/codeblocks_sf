d2535063b749079e4aa9fe4b6e75f05440e476ee
Yiannis Mandravellos <mandrav@codeblocks.org>
2006-01-12 17:52:07 +0000

    * Fixed debugger watches tree parsing error when parsing templates.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1731 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/debuggergdb/debuggertree.cpp
+++ src/plugins/debuggergdb/debuggertree.cpp
@@ -265,12 +265,12 @@ int DebuggerTree::FindCharOutsideQuotes(const wxString& str, wxChar ch)
 int DebuggerTree::FindCommaPos(const wxString& str)
 {
     // comma is a special case because it separates the fields
-    // but it can also appear in a function signature, where
+    // but it can also appear in a function/template signature, where
     // we shouldn't treat it as a field separator
 
     // what we 'll do now, is decide if the comma is inside
     // a function signature.
-    // we 'll do it by counting the opening and closing parenthesis
+    // we 'll do it by counting the opening and closing parenthesis/angled-brackets
     // *up to* the comma.
     // if they 're equal, it's a field separator.
     // if they 're not, it's in a function signature
@@ -279,19 +279,42 @@ int DebuggerTree::FindCommaPos(const wxString& str)
     int len = str.Length();
     int i = 0;
     int parCount = 0;
+    int braCount = 0;
     bool inQuotes = false;
     while (i < len)
     {
-        if (str.GetChar(i) == _T('(') && (i == 0 || (i > 0 && str.GetChar(i - 1) != '\\')))
-            ++parCount; // increment on opening parenthesis
-        else if (str.GetChar(i) == ')' && (i == 0 || (i > 0 && str.GetChar(i - 1) != '\\')))
-            --parCount; // decrement on opening parenthesis
+        wxChar ch = str.GetChar(i);
+        switch (ch)
+        {
+            case _T('('):
+                ++parCount; // increment on opening parenthesis
+                break;
+
+            case _T(')'):
+                --parCount; // decrement on closing parenthesis
+                break;
+
+            case _T('<'):
+                ++braCount; // increment on opening angle bracket
+                break;
+
+            case _T('>'):
+                --braCount; // decrement on closing angle bracket
+                break;
+
+            case _T('"'):
+                // fall through
+            case _T('\''):
+                inQuotes = !inQuotes; // toggle inQuotes flag
+                break;
+
+            default:
+                break;
+        }
 
         // if it's not inside quotes *and* we have parCount == 0, it's a field separator
-        if (!inQuotes && parCount == 0 && str.GetChar(i) == _T(','))
+        if (!inQuotes && parCount == 0 && braCount == 0 && ch == _T(','))
             return i;
-        else if (str.GetChar(i) == _T('"') && (i == 0 || (i > 0 && str.GetChar(i - 1) != _T('\\'))))
-            inQuotes = !inQuotes;
         ++i;
     }
     return -1;
