b9b037526e87807184677c082b6b3c5c48dfbcac
Thomas Denk <thomasdenk@codeblocks.org>
2006-01-26 10:09:50 +0000

    Added missing #includes
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@1866 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/uservarmanager.h
+++ src/sdk/uservarmanager.h
@@ -2,11 +2,13 @@
 #define USER_VARIABLE_MANAGER_H
 
 #include "settings.h"
+#include "manager.h"
+#include "macrosmanager.h"
 
 class DLLIMPORT UserVariableManager : public Mgr<UserVariableManager>
 {
         friend class Manager;
-        friend class Mgr<MacrosManager>;
+        friend class Mgr<UserVariableManager>;
         friend class MacrosManager;
         static UserVariableManager* instance;
 
