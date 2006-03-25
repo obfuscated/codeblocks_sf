84317ee6c4a10fdce1ad1f80213313be3d537b9c
zlika <zlika@codeblocks.org>
2006-03-25 15:26:38 +0000

    Codestat: The plugin configuration panel appeared in the wrong settings panel -> fixed.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@2257 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/codestat/codestat.h
+++ src/plugins/contrib/codestat/codestat.h
@@ -24,7 +24,7 @@ class CodeStat : public cbToolPlugin
 	public:
 		CodeStat();
 		~CodeStat();
-		int GetConfigurationGroup() const { return cgEditor; }
+		int GetConfigurationGroup() { return cgEditor; }
         cbConfigurationPanel* GetConfigurationPanel(wxWindow* parent);
 		int Execute();
 		void OnAttach(); // fires when the plugin is attached to the application
