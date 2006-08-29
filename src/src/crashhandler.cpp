91fdc06bcc1e470df4a4b186c4ffd7868cbf5c89
tiwag <tiwag@codeblocks.org>
2006-08-29 11:16:06 +0000

    - windows crashhandler uses "My documents\cb-crash-recover" folder   current editor files are saved in a subdirectory of the current date & time   files with same name are saved with appended increasing numbers   e.g. main.cpp.001, main.cpp.002, ...
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@2920 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/src/crashhandler.cpp
+++ src/src/crashhandler.cpp
@@ -9,45 +9,92 @@
 #include "configmanager.h"
 #include "editormanager.h"
 #include "globals.h"
-#endif
+#endif //CB_PRECOMP
 #include "crashhandler.h"
+#include <shlobj.h>
+
+void CrashHandlerSaveEditorFiles(wxString& buf)
+{
+    wxString path;
+    //get the "My Files" folder
+    HRESULT result = SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, 0, wxStringBuffer(path, MAX_PATH));
+    if (FAILED(result))
+    {   //get at least the profiles folder
+        path = ConfigManager::GetHomeFolder();
+    }
+    path << _T("\\cb-crash-recover");
+    if(!wxDirExists(path)) wxMkdir(path);
+
+    //make a sub-directory of the current date & time
+    wxDateTime now = wxDateTime::Now();
+    path << now.Format(_T("\\%Y%m%d-%H%M%S"));
+
+    EditorManager* em = Manager::Get()->GetEditorManager();
+    if(em)
+    {
+        bool AnyFileSaved = false;
+        wxMkdir(path);
+        if(wxDirExists(path))
+        {
+            for(int i = 0; i < em->GetEditorsCount(); ++i)
+            {
+                cbEditor* ed = em->GetBuiltinEditor(em->GetEditor(i));
+                if(ed)
+                {
+                    wxFileName fn(ed->GetFilename());
+                    wxString fnpath = path + _T("/") + fn.GetFullName();
+                    wxString newfnpath = fnpath;
+                    // add number if filename already exists e.g. main.cpp.001, main.cpp.002, ...
+                    int j = 1;
+                    while(wxFileExists(newfnpath))
+                    {
+                        newfnpath = fnpath + wxString::Format(wxT(".%03d"),j);
+                    }
+                    if(cbSaveToFile(newfnpath,
+                                    ed->GetControl()->GetText(),
+                                    ed->GetEncoding(),
+                                    ed->GetUseBom() ))
+                        AnyFileSaved = true;
+                }
+            }
+
+            if(AnyFileSaved)
+            {
+                buf << _("The currently opened files have been saved to the directory\n");
+                buf << path;
+                buf << _("\nHopefully, this will prevent you from losing recent modifications.\n\n");
+            }
+            else
+            {
+                wxRmdir(path);
+            }
+        }
+    }
+}
 
 LONG WINAPI CrashHandlerFunc(PEXCEPTION_POINTERS ExceptionInfo)
 {
+    static bool EditorFilesNotSaved = true;
     DWORD code = ExceptionInfo->ExceptionRecord->ExceptionCode;
 
     if(code != EXCEPTION_ACCESS_VIOLATION && code != EXCEPTION_ILLEGAL_INSTRUCTION)
         return EXCEPTION_CONTINUE_SEARCH;
 
     wxLogNull nl;
-    wxString path(ConfigManager::GetHomeFolder() + _T("/cb-crash-recover"));
-    wxMkdir(ConfigManager::GetHomeFolder() + _T("/cb-crash-recover"));
-    path << _T("/");
+    wxString buf;
+    buf.Printf(_("The application encountered a crash at address %u.\n\n"),
+               (unsigned int) ExceptionInfo->ContextRecord->Eip);
 
-    EditorManager* em = Manager::Get()->GetEditorManager();
-
-    if(em)
+    if(EditorFilesNotSaved)
     {
-    for(int i = 0; i < em->GetEditorsCount(); ++i)
-        {
-            cbEditor* ed = em->GetBuiltinEditor(em->GetEditor(i));
-            if(ed)
-            {
-            wxFileName fn(ed->GetFilename());
-            cbSaveToFile(path + fn.GetFullName(), ed->GetControl()->GetText(), ed->GetEncoding(), ed->GetUseBom());
-            }
-
-        }
+        CrashHandlerSaveEditorFiles(buf);
+        EditorFilesNotSaved = false;
     }
 
-    wxString buf;
-    buf.Printf(_T("The application encountered a crash at address %u.\n\n"
-                  "A snapshot of the present state of Code::Blocks has been saved to the directory cb-crash-recover inside your 'My Files' folder. Hopefully, this will prevent you from losing recent modifications.\n\n"
-                  "You now have three options:\n"
-                  "1. Press 'Abort' to pass control back to the system. This will normally display the standard 'application error' message and kill the program.\n"
-                  "2. Press 'Ignore' to step over the offending instruction. You may run into another access violation, but if you are lucky enough, you might get to save your work and close the program gracefully.\n"
-                  "3. Press 'Retry' to return to the offending instruction (this is almost certain to fail again, but might nevertheless work in rare cases)."),
-               (unsigned int) ExceptionInfo->ContextRecord->Eip);
+    buf << _("Now you have three options:\n"
+              "1. Press 'Abort' to pass control back to the system. This will normally display the standard 'application error' message and kill the program.\n"
+              "2. Press 'Ignore' to step over the offending instruction. You may run into another access violation, but if you are lucky enough, you might get to save your work and close the program gracefully.\n"
+              "3. Press 'Retry' to return to the offending instruction (this is almost certain to fail again, but might nevertheless work in rare cases).");
 
 
     switch(MessageBox(0, buf.c_str(), _T("Woah!"), MB_ABORTRETRYIGNORE))
@@ -87,5 +134,5 @@ CrashHandler::~CrashHandler()
 }
 
 
-#endif
+#endif //(__WXMSW__)
 
