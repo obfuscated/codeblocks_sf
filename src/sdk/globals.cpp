9a115fec71b1bcf75e641f4296d3f91a141f5aa8
Lieven de Cock <killerbot@codeblocks.org>
2006-09-05 21:06:54 +0000

    - little update to previous commit
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@2956 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/globals.cpp
+++ src/sdk/globals.cpp
@@ -598,7 +598,7 @@ bool UsesCommonControls6()
     bool result = false;
     HINSTANCE hinstDll;
     DWORD dwVersion = 0;
-    hinstDll = LoadLibrary("comctl32.dll");
+    hinstDll = LoadLibrary(_T("comctl32.dll"));
     if(hinstDll)
     {
         DLLGETVERSIONPROC pDllGetVersion;
