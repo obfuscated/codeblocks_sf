aec236a6a2c533c9e1ae017e8946594564cb0b94
Yiannis Mandravellos <mandrav@codeblocks.org>
2007-01-15 09:44:03 +0000

    * Fixed broken build of sc_dialog.cpp for 64bit CPUs. * Added more error checking in ShowDialog() script function.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@3487 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/scripting/bindings/sc_dialog.cpp
+++ src/sdk/scripting/bindings/sc_dialog.cpp
@@ -24,7 +24,10 @@ namespace ScriptBindings
             XrcDialog(wxWindow* parent, const wxString& dlgName, const wxString& callback)
                 : m_CallBack(callback)
             {
-                wxXmlResource::Get()->LoadDialog(this, parent, dlgName);
+                if (!wxXmlResource::Get()->LoadDialog(this, parent, dlgName))
+                {
+                    cbThrow(wxEmptyString);
+                }
             }
             ~XrcDialog(){ }
             void OnButton(wxCommandEvent& event);
@@ -69,16 +72,27 @@ namespace ScriptBindings
         if (wxXmlResource::Get()->Load(actual))
         {
             XrcDialog* old = s_ActiveDialog;
-            s_ActiveDialog = new XrcDialog(0, dlgName, callback);
-            int ret = s_ActiveDialog->ShowModal();
-            delete s_ActiveDialog;
-            s_ActiveDialog = old;
-            wxXmlResource::Get()->Unload(actual);
-            return ret;
+            try
+            {
+                s_ActiveDialog = new XrcDialog(0, dlgName, callback);
+                int ret = s_ActiveDialog->ShowModal();
+                delete s_ActiveDialog;
+                s_ActiveDialog = old;
+                wxXmlResource::Get()->Unload(actual);
+                return ret;
+            }
+            catch (cbException& e)
+            {
+                cbMessageBox(wxString::Format(_("Dialog \"%s\" not found...\n\nActual resource: \"%s\"\nOriginal resource: \"%s\""),
+                                            dlgName.c_str(),
+                                            actual.c_str(),
+                                            xrc.c_str()),
+                            _("Error"), wxICON_ERROR);
+            }
         }
         return -1;
     }
-    
+
     void EndModal(int retCode)
     {
         // valid only while in a ShowDialog() call
@@ -89,7 +103,7 @@ namespace ScriptBindings
         }
         cbMessageBox(_("EndModal() only valid while inside a ShowDialog() call..."), _("Error"), wxICON_ERROR);
     }
-    
+
     SQInteger XrcId(HSQUIRRELVM v)
     {
         // here we simulate XRCID by using wxWindow::FindWindowByName().
@@ -98,15 +112,15 @@ namespace ScriptBindings
         if (!s_ActiveDialog)
         {
             cbMessageBox(_("XRCID() only valid while inside a ShowDialog() call..."), _("Error"), wxICON_ERROR);
-            return sa.Return(-1);
+            return sa.Return((SQInteger)-1);
         }
-        
+
         wxWindow* win = 0;
         if (sa.GetType(2) == OT_STRING)
             win = wxWindow::FindWindowByName(cbC2U(sa.GetString(2)), s_ActiveDialog);
         else
             win = wxWindow::FindWindowByName(*SqPlus::GetInstance<wxString>(v, 2), s_ActiveDialog);
-        return sa.Return(win ? win->GetId() : -1);
+        return sa.Return((SQInteger)(win ? win->GetId() : -1));
     }
 
     void Register_Dialog()
