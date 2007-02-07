85275edd188b05b2afd63f98d476de4832d69b0d
Yiannis Mandravellos <mandrav@codeblocks.org>
2007-02-07 13:53:13 +0000

    - Updated annoying dialog to respect platform-specific UI guidelines regarding buttons placement.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@3586 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/annoyingdialog.cpp
+++ src/sdk/annoyingdialog.cpp
@@ -56,7 +56,6 @@ AnnoyingDialog::AnnoyingDialog(const wxString& caption, const wxString& message,
     mainArea->Add( 1, 1, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT|wxTOP, 5 );
 
     int numButtons = 0;
-    int growableCol = 0;
     int id1 = -1;
     int id2 = -1;
     int id3 = -1;
@@ -67,14 +66,12 @@ AnnoyingDialog::AnnoyingDialog(const wxString& caption, const wxString& message,
     if(style == OK || style == ONE_BUTTON)
     {
         numButtons = 1;
-        growableCol = -1;
         id1 = (style == OK ? wxID_OK : 1);
         bTxt1 = b1.IsEmpty() ? wxString(_("&OK")) : b1;
     }
     else if(style == YES_NO || style == OK_CANCEL || style == TWO_BUTTONS)
     {
         numButtons = 2;
-        growableCol = -1;
         id1 = (style == YES_NO ? wxID_YES : (style == OK_CANCEL ? wxID_OK     : 1));
         id2 = (style == YES_NO ? wxID_NO  : (style == OK_CANCEL ? wxID_CANCEL : 2));
         bTxt1 = b1.IsEmpty() ? (style == YES_NO ? wxString(_("&Yes")) : wxString(_("&OK")))     : b1;
@@ -83,7 +80,6 @@ AnnoyingDialog::AnnoyingDialog(const wxString& caption, const wxString& message,
     else if(style == YES_NO_CANCEL || style == THREE_BUTTONS)
     {
         numButtons = 3;
-        growableCol = separate ? 1 : -1;
         id1 = (style == YES_NO_CANCEL ? wxID_YES    : 1);
         id2 = (style == YES_NO_CANCEL ? wxID_NO     : 2);
         id3 = (style == YES_NO_CANCEL ? wxID_CANCEL : 3);
@@ -94,31 +90,25 @@ AnnoyingDialog::AnnoyingDialog(const wxString& caption, const wxString& message,
     else
         cbThrow(wxString(_T("Fatal error:\nUndefined style in dialog ")) << caption);
 
-    wxFlexGridSizer *buttonArea = new wxFlexGridSizer(4, 0, 0);
-
-    if(growableCol != -1)
-        buttonArea->AddGrowableCol(growableCol);
+    wxStdDialogButtonSizer *buttonArea = new wxStdDialogButtonSizer();
 
     wxButton *but1 = new wxButton(this, id1, bTxt1, wxDefaultPosition, wxDefaultSize, 0);
     but1->SetDefault();
-    buttonArea->Add(but1, 0, wxALIGN_CENTER|wxALL, 5);
-
-    if(growableCol != -1)
-        buttonArea->Add(1, 1, 0, wxGROW|wxALIGN_CENTER_VERTICAL, 5 );
+    buttonArea->AddButton(but1);
 
     if(numButtons > 1)
     {
         wxButton *but2 = new wxButton(this, id2, bTxt2, wxDefaultPosition, wxDefaultSize, 0);
         but2->SetDefault();
-        buttonArea->Add(but2, 0, wxALIGN_CENTER|wxTOP|wxBOTTOM|wxRIGHT, 5);
+        buttonArea->AddButton(but2);
     }
     if(numButtons > 2)
     {
         wxButton *but3 = new wxButton(this, id3, bTxt3, wxDefaultPosition, wxDefaultSize, 0);
         but3->SetDefault();
-        buttonArea->Add(but3, 0, wxALIGN_CENTER|wxTOP|wxBOTTOM|wxRIGHT, 5);
+        buttonArea->AddButton(but3);
     }
-//    mainArea->Add( buttonArea, 0, wxALIGN_CENTER|wxTOP, 10);
+    buttonArea->Realize();
 
     outerSizer->Add( mainArea, 0, wxALIGN_CENTER|wxALL, 5);
     outerSizer->Add( buttonArea, 0, wxALIGN_CENTER);
