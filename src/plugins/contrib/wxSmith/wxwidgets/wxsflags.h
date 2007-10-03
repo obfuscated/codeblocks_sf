16e7c3d953ad430d574173c5f9aee658c3c7b3be
Bart³omiej ¦wiêcki <byo@codeblocks.org>
2007-10-03 19:55:03 +0000

    wxSmith: Forgot to add one file
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4506 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/wxSmith/wxwidgets/wxsflags.h
@@ -0,0 +1,64 @@
+/*
+* This file is part of wxSmith plugin for Code::Blocks Studio
+* Copyright (C) 2006-2007  Bartlomiej Swiecki
+*
+* wxSmith is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*
+* $Revision$
+* $Id$
+* $HeadURL$
+*/
+
+#ifndef WXSFLAGS_H
+#define WXSFLAGS_H
+
+/** \brief Flags used for property filtering, while generating code and while generating preview */
+namespace wxsFlags
+{
+    // Property filtering flags
+    const long flVariable  = 0x0000001;  ///< \brief Item is using variable
+    const long flId        = 0x0000002;  ///< \brief Item is using identifier
+    const long flPosition  = 0x0000004;  ///< \brief Item is using position
+    const long flSize      = 0x0000008;  ///< \brief Item is using size
+    const long flEnabled   = 0x0000010;  ///< \brief Item is using Enabled property
+    const long flFocused   = 0x0000020;  ///< \brief Item is using Focused property
+    const long flHidden    = 0x0000040;  ///< \brief Item is using Hidden property
+    const long flColours   = 0x0000080;  ///< \brief Item is using colour properties (Fg and Bg)
+    const long flToolTip   = 0x0000100;  ///< \brief Item is using tooltips
+    const long flFont      = 0x0000200;  ///< \brief Item is using font
+    const long flHelpText  = 0x0000400;  ///< \brief Item is using help text
+    const long flSubclass  = 0x0000800;  ///< \brief Item is using subclassing
+    const long flMinMaxSize= 0x0001000;  ///< \brief Item is using SetMinSize / SetMaxSize functions
+    const long flExtraCode = 0x0002000;  ///< \brief Item is using extra item initialization code
+
+    const long flFile      = 0x8000000;  ///< \brief Edition in file mode
+    const long flSource    = 0x4000000;  ///< \brief Edition in source mode
+    const long flMixed     = 0x2000000;  ///< \brief Edition in mixed mode
+    const long flFwdDeclar = 0x1000000;  ///< \brief Using forward declarations where possible
+    const long flPchFilter = 0x0800000;  ///< \brief Use filtering of PCH files (include some headers only when there's no PCH)
+    const long flPointer   = 0x0400000;  ///< \brief Flag set when current item is as pointer
+    const long flLocal     = 0x0200000;  ///< \brief Flag set when current item is used only locally while building resource's content (is not member of class)
+    const long flRoot      = 0x0100000;  ///< \brief Flag set when current item is root item of resource
+
+    // Preview flags
+    const long pfExact     = 0x0000001;  ///< \brief Notify to create exact preview (without any editor-like goodies)
+
+    // Header flags
+    const long hfLocal     = 0x0000001;  ///< \brief Header is neede only locally (while manually generating resource)
+    const long hfInPCH     = 0x0000002;  ///< \brief This header has been included in default pch file
+    const long hfForbidFwd = 0x0000004;  ///< \brief This header (when included in resource's header file) can not be replaced by forward declaration
+}
+
+#endif
