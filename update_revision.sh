e47eb491a81cc2e45da0d559bfd32a5c44ea9a2b
Anders F Björklund <afb@codeblocks.org>
2007-10-13 08:19:17 +0000

    - use portable sed (avoid -i flag)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4529 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- update_revision.sh
+++ update_revision.sh
@@ -26,7 +26,9 @@ if [ "x$REV" != "x$OLD_REV" -o ! -r $REV_FILE ]; then
 	echo "m4_define([SVN_DATE], $LCD)" >> $REV_FILE
 
 	# Also change the revision number in debian/changelog for package versioning
-	sed -i "1 s/(1.0svn[^-)]*/(1.0svn$REV/" debian/changelog
+	mv debian/changelog debian/changelog.tmp
+	sed "1 s/(1.0svn[^-)]*/(1.0svn$REV/" < debian/changelog.tmp > debian/changelog
+	rm debian/changelog.tmp
 fi
 
 echo "OLD_REV=$REV" > ./.last_revision
