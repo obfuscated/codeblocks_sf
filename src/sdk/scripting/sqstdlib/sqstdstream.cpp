04a2236bb8f0318c825598a6e971db5de6f557ef
Lieven de Cock <killerbot@codeblocks.org>
2007-11-19 11:41:36 +0000

    * remove some warnings in squirrel : !!!!const!!!! SQChqr* in declare_stream
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4644 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/scripting/sqstdlib/sqstdstream.cpp
+++ src/sdk/scripting/sqstdlib/sqstdstream.cpp
@@ -277,7 +277,7 @@ void init_streamclass(HSQUIRRELVM v)
 	sq_pop(v,1);
 }
 
-SQRESULT declare_stream(HSQUIRRELVM v,SQChar* name,SQUserPointer typetag,SQChar* reg_name,SQRegFunction *methods,SQRegFunction *globals)
+SQRESULT declare_stream(HSQUIRRELVM v, const SQChar* name, SQUserPointer typetag, const SQChar* reg_name,SQRegFunction *methods,SQRegFunction *globals)
 {
 	if(sq_gettype(v,-1) != OT_TABLE)
 		return sq_throwerror(v,_SC("table expected"));
@@ -302,7 +302,7 @@ SQRESULT declare_stream(HSQUIRRELVM v,SQChar* name,SQUserPointer typetag,SQChar*
 		}
 		sq_createslot(v,-3);
 		sq_pop(v,1);
-		
+
 		i = 0;
 		while(globals[i].name!=0)
 		{
