7ff75eca275cb52d6f90d8dca65c700ad70bb2f3
Bart³omiej ¦wiêcki <byo@codeblocks.org>
2007-12-04 22:39:30 +0000

    wxSmith: * Added wxMultiChoiceDialog
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4701 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsmultichoicedialog.h
@@ -0,0 +1,46 @@
+/*
+* This file is part of wxSmith plugin for Code::Blocks Studio
+* Copyright (C) 2007  Bartlomiej Swiecki
+*
+* wxSmith is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*
+* $Revision: 4504 $
+* $Id: wxstimer.h 4504 2007-10-02 21:52:30Z byo $
+* $HeadURL: svn+ssh://byo@svn.berlios.de/svnroot/repos/codeblocks/trunk/src/plugins/contrib/wxSmith/wxwidgets/defitems/wxstimer.h $
+*/
+
+#ifndef WXSMULTICHOICEDIALOG_H
+#define WXSMULTICHOICEDIALOG_H
+
+#include "../wxstool.h"
+
+class wxsMultiChoiceDialog: public wxsTool
+{
+    public:
+
+        wxsMultiChoiceDialog(wxsItemResData* Data);
+
+    private:
+
+        virtual void OnBuildCreatingCode();
+        virtual void OnEnumToolProperties(long Flags);
+
+        wxString m_Message;
+        wxString m_Caption;
+        wxArrayString m_Content;
+};
+
+#endif
+
