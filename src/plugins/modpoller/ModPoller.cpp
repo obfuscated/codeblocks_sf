3d9d9fb1f32d44afe216a66e5e60902f36d06ad3
Thomas Denk <thomasdenk@codeblocks.org>
2008-01-22 12:37:36 +0000

    - Added ModPoller to source tree (is not in main build)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4839 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/modpoller/ModPoller.cpp
@@ -0,0 +1,8 @@
+#include <sdk.h> // Code::Blocks SDK
+#include <configurationpanel.h>
+#include "ModPoller.h"
+
+namespace
+{
+    PluginRegistrant<ModPoller> reg(_T("ModPoller"));
+}
---
+++ src/plugins/modpoller/ModPoller.h
@@ -0,0 +1,36 @@
+#ifndef MODPOLLER_H_INCLUDED
+#define MODPOLLER_H_INCLUDED
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include <cbplugin.h>
+
+class ModPoller : public cbPlugin
+{
+    public:
+        ModPoller(){};
+        virtual ~ModPoller(){};
+
+        virtual void BuildMenu(wxMenuBar* menuBar){}
+        virtual void BuildModuleMenu(const ModuleType type, wxMenu* menu, const FileTreeData* data = 0){}
+        virtual bool BuildToolBar(wxToolBar* toolBar){ return false; }
+
+    protected:
+
+        virtual void OnAttach()
+		{
+        	Manager::Get()->RegisterEventSink(cbEVT_EDITOR_ACTIVATED, new cbEventFunctor<ModPoller, CodeBlocksEvent>(this, &ModPoller::OnEditorActivated));
+		}
+        void OnEditorActivated(CodeBlocksEvent& event)
+        {
+        	EditorManager::Get()->CheckForExternallyModifiedFiles();
+		}
+
+        virtual void OnRelease(bool appShutDown){};
+};
+
+#endif
