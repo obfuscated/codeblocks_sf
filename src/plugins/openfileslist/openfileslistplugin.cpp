5e4745e34b99e7c5e69799b7476a1ecd49d5e30e
Biplab Kumar Modak <biplab@codeblocks.org>
2008-02-16 06:51:36 +0000

    * Fixed: Erratic behaviour (not all opened editor were shown with their relative path) of Opened files tree right after a Project load.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4880 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/openfileslist/openfileslistplugin.cpp
+++ src/plugins/openfileslist/openfileslistplugin.cpp
@@ -16,76 +16,78 @@
 
 namespace compatibility
 {
-	typedef TernaryCondTypedef<wxMinimumVersion<2,5>::eval, wxTreeItemIdValue, long int>::eval tree_cookie_t;
+    typedef TernaryCondTypedef<wxMinimumVersion<2,5>::eval, wxTreeItemIdValue, long int>::eval tree_cookie_t;
 }
 
 namespace
 {
-	// this auto-registers the plugin
+    // this auto-registers the plugin
     PluginRegistrant<OpenFilesListPlugin> reg(_T("OpenFilesList"));
 
-	const int idOpenFilesTree = wxNewId();
-	const int idViewOpenFilesTree = wxNewId();
+    const int idOpenFilesTree = wxNewId();
+    const int idViewOpenFilesTree = wxNewId();
 
-	class OpenFilesListData : public wxTreeItemData
-	{
-		public:
-			OpenFilesListData(EditorBase* ed) : ed(ed) {}
-			EditorBase* GetEditor() const { return ed; }
-		private:
-			EditorBase* ed;
-	};
+    class OpenFilesListData : public wxTreeItemData
+    {
+        public:
+            OpenFilesListData(EditorBase* ed) : ed(ed) {}
+            EditorBase* GetEditor() const { return ed; }
+        private:
+            EditorBase* ed;
+    };
 }
 
 BEGIN_EVENT_TABLE(OpenFilesListPlugin, cbPlugin)
-	EVT_UPDATE_UI(idViewOpenFilesTree, OpenFilesListPlugin::OnUpdateUI)
-	EVT_MENU(idViewOpenFilesTree, OpenFilesListPlugin::OnViewOpenFilesTree)
+    EVT_UPDATE_UI(idViewOpenFilesTree, OpenFilesListPlugin::OnUpdateUI)
+    EVT_MENU(idViewOpenFilesTree, OpenFilesListPlugin::OnViewOpenFilesTree)
     EVT_TREE_ITEM_ACTIVATED(idOpenFilesTree, OpenFilesListPlugin::OnTreeItemActivated)
     EVT_TREE_ITEM_RIGHT_CLICK(idOpenFilesTree, OpenFilesListPlugin::OnTreeItemRightClick)
 END_EVENT_TABLE()
 
 OpenFilesListPlugin::OpenFilesListPlugin()
 {
-	//ctor
+    //ctor
 }
 
 OpenFilesListPlugin::~OpenFilesListPlugin()
 {
-	//dtor
+    //dtor
 }
 
 void OpenFilesListPlugin::OnAttach()
 {
-	m_ViewMenu = 0;
-	
-	// create tree
-    m_pTree = new wxTreeCtrl(Manager::Get()->GetAppWindow(), idOpenFilesTree,wxDefaultPosition,wxSize(150, 100),
-							wxTR_HAS_BUTTONS | wxNO_BORDER | wxTR_HIDE_ROOT);
+    m_ViewMenu = 0;
 
-	// load bitmaps
-	wxBitmap bmp;
-	m_pImages = new wxImageList(16, 16);
-	wxString prefix = ConfigManager::GetDataFolder() + _T("/images/");
-	
-	bmp = cbLoadBitmap(prefix + _T("folder_open.png"), wxBITMAP_TYPE_PNG); // folder
-	m_pImages->Add(bmp);
-	
-	bmp = cbLoadBitmap(prefix + _T("ascii.png"), wxBITMAP_TYPE_PNG); // file
-	m_pImages->Add(bmp);
-	
-	bmp = cbLoadBitmap(prefix + _T("modified_file.png"), wxBITMAP_TYPE_PNG); // modified file
-	m_pImages->Add(bmp);
-	
-	bmp = cbLoadBitmap(prefix + _T("file-readonly.png"), wxBITMAP_TYPE_PNG); // read only file
-	m_pImages->Add(bmp);
-	
-	m_pTree->SetImageList(m_pImages);
-	m_pTree->AddRoot(_T("Opened Files"), 0, 0);
-    
+    m_EditorArray.Clear();
+
+    // create tree
+    m_pTree = new wxTreeCtrl(Manager::Get()->GetAppWindow(), idOpenFilesTree,wxDefaultPosition,wxSize(150, 100),
+                            wxTR_HAS_BUTTONS | wxNO_BORDER | wxTR_HIDE_ROOT);
+
+    // load bitmaps
+    wxBitmap bmp;
+    m_pImages = new wxImageList(16, 16);
+    wxString prefix = ConfigManager::GetDataFolder() + _T("/images/");
+
+    bmp = cbLoadBitmap(prefix + _T("folder_open.png"), wxBITMAP_TYPE_PNG); // folder
+    m_pImages->Add(bmp);
+
+    bmp = cbLoadBitmap(prefix + _T("ascii.png"), wxBITMAP_TYPE_PNG); // file
+    m_pImages->Add(bmp);
+
+    bmp = cbLoadBitmap(prefix + _T("modified_file.png"), wxBITMAP_TYPE_PNG); // modified file
+    m_pImages->Add(bmp);
+
+    bmp = cbLoadBitmap(prefix + _T("file-readonly.png"), wxBITMAP_TYPE_PNG); // read only file
+    m_pImages->Add(bmp);
+
+    m_pTree->SetImageList(m_pImages);
+    m_pTree->AddRoot(_T("Opened Files"), 0, 0);
+
     // first build of the tree
     RebuildOpenFilesTree();
 
-	// add the tree to the docking system
+    // add the tree to the docking system
     CodeBlocksDockEvent evt(cbEVT_ADD_DOCK_WINDOW);
     evt.name = _T("OpenFilesPane");
     evt.title = _("Open files list");
@@ -100,25 +102,27 @@ void OpenFilesListPlugin::OnAttach()
     // register event sinks
     Manager* pm = Manager::Get();
 
-	// basically everything editor related
+    // basically everything editor related
     pm->RegisterEventSink(cbEVT_EDITOR_ACTIVATED, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorActivated));
     pm->RegisterEventSink(cbEVT_EDITOR_CLOSE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorClosed));
     pm->RegisterEventSink(cbEVT_EDITOR_DEACTIVATED, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorDeactivated));
     pm->RegisterEventSink(cbEVT_EDITOR_MODIFIED, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorModified));
     pm->RegisterEventSink(cbEVT_EDITOR_OPEN, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorOpened));
     pm->RegisterEventSink(cbEVT_EDITOR_SAVE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorSaved));
+    pm->RegisterEventSink(cbEVT_PROJECT_ACTIVATE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectOpened));
+    pm->RegisterEventSink(cbEVT_PROJECT_OPEN, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectOpened));
 }
 
 void OpenFilesListPlugin::OnRelease()
 {
-	// remove registered event sinks
-	Manager::Get()->RemoveAllEventSinksFor(this);
+    // remove registered event sinks
+    Manager::Get()->RemoveAllEventSinksFor(this);
 
-	// remove tree from docking system
+    // remove tree from docking system
     CodeBlocksDockEvent evt(cbEVT_REMOVE_DOCK_WINDOW);
     evt.pWindow = m_pTree;
     Manager::Get()->ProcessEvent(evt);
-    
+
     // finally destroy the tree
     m_pTree->Destroy();
 }
@@ -154,10 +158,10 @@ int OpenFilesListPlugin::GetOpenFilesListIcon(EditorBase* ed)
 {
     int mod = 1;          // ascii
     if (ed->IsReadOnly())
-		mod = 3;          // read only
+        mod = 3;          // read only
     else if (ed->GetModified())
-		mod =2;           // modified
-	return mod;
+        mod =2;           // modified
+    return mod;
 }
 
 void OpenFilesListPlugin::RebuildOpenFilesTree()
@@ -165,8 +169,8 @@ void OpenFilesListPlugin::RebuildOpenFilesTree()
     if(Manager::IsAppShuttingDown())
         return;
 
-	EditorManager* mgr = Manager::Get()->GetEditorManager();
-	
+    EditorManager* mgr = Manager::Get()->GetEditorManager();
+
     m_pTree->Freeze();
     m_pTree->DeleteChildren(m_pTree->GetRootItem());
     if(!mgr->GetEditorsCount())
@@ -187,7 +191,7 @@ void OpenFilesListPlugin::RebuildOpenFilesTree()
         if(mgr->GetActiveEditor() == ed)
             m_pTree->SelectItem(item);
     }
-    
+
     m_pTree->SortChildren(m_pTree->GetRootItem());
     m_pTree->Expand(m_pTree->GetRootItem());
     m_pTree->Thaw();
@@ -198,14 +202,14 @@ void OpenFilesListPlugin::RefreshOpenFilesTree(EditorBase* ed, bool remove)
     if(Manager::IsAppShuttingDown())
         return;
 
-	EditorManager* mgr = Manager::Get()->GetEditorManager();
-	EditorBase* aed = mgr->GetActiveEditor();
+    EditorManager* mgr = Manager::Get()->GetEditorManager();
+    EditorBase* aed = mgr->GetActiveEditor();
 
     m_pTree->Freeze();
 
     compatibility::tree_cookie_t cookie = 0;
     wxTreeItemId item = m_pTree->GetFirstChild(m_pTree->GetRootItem(), cookie);
-	wxString shortname = ed->GetShortName();
+    wxString shortname = ed->GetShortName();
     bool found = false;
     // loop all tree items
     while (item)
@@ -213,28 +217,28 @@ void OpenFilesListPlugin::RefreshOpenFilesTree(EditorBase* ed, bool remove)
         EditorBase* data = static_cast<EditorBase*>(static_cast<OpenFilesListData*>(m_pTree->GetItemData(item))->GetEditor());
         if(data && ed == data)
         {
-        	// and apply changes to current item
-        	if (!remove)
-        	{
-				found = true;
-				int mod = GetOpenFilesListIcon(ed);
-				if(m_pTree->GetItemText(item) != shortname)
-					m_pTree->SetItemText(item, shortname);
-				if (m_pTree->GetItemImage(item) != mod)
-				{
-					m_pTree->SetItemImage(item, mod, wxTreeItemIcon_Normal);
-					m_pTree->SetItemImage(item, mod, wxTreeItemIcon_Selected);
-				}
-				if(ed == aed)
-					m_pTree->SelectItem(item);
-        	}
-        	else
-				m_pTree->Delete(item);
-			 break;
+            // and apply changes to current item
+            if (!remove)
+            {
+                found = true;
+                int mod = GetOpenFilesListIcon(ed);
+                if(m_pTree->GetItemText(item) != shortname)
+                    m_pTree->SetItemText(item, shortname);
+                if (m_pTree->GetItemImage(item) != mod)
+                {
+                    m_pTree->SetItemImage(item, mod, wxTreeItemIcon_Normal);
+                    m_pTree->SetItemImage(item, mod, wxTreeItemIcon_Selected);
+                }
+                if(ed == aed)
+                    m_pTree->SelectItem(item);
+            }
+            else
+                m_pTree->Delete(item);
+             break;
         }
         item = m_pTree->GetNextChild(m_pTree->GetRootItem(), cookie);
     }
-    
+
     // not found, not to-remove and valid name: add new item
     if (!found && !remove && ed->VisibleToTree() && !shortname.IsEmpty())
     {
@@ -242,10 +246,10 @@ void OpenFilesListPlugin::RefreshOpenFilesTree(EditorBase* ed, bool remove)
         wxTreeItemId item = m_pTree->AppendItem(m_pTree->GetRootItem(), shortname, mod, mod, new OpenFilesListData(ed));
         if(mgr->GetActiveEditor() == ed)
             m_pTree->SelectItem(item);
-		m_pTree->SortChildren(m_pTree->GetRootItem());
-		m_pTree->Expand(m_pTree->GetRootItem());
+        m_pTree->SortChildren(m_pTree->GetRootItem());
+        m_pTree->Expand(m_pTree->GetRootItem());
     }
-    
+
     m_pTree->Thaw();
 }
 
@@ -254,9 +258,9 @@ void OpenFilesListPlugin::OnTreeItemActivated(wxTreeEvent &event)
 {
     if(Manager::IsAppShuttingDown())
         return;
-	EditorBase* ed = static_cast<EditorBase*>(static_cast<OpenFilesListData*>(m_pTree->GetItemData(event.GetItem()))->GetEditor());
-	if(ed)
-		Manager::Get()->GetEditorManager()->SetActiveEditor(ed);
+    EditorBase* ed = static_cast<EditorBase*>(static_cast<OpenFilesListData*>(m_pTree->GetItemData(event.GetItem()))->GetEditor());
+    if(ed)
+        Manager::Get()->GetEditorManager()->SetActiveEditor(ed);
 }
 
 // tree item right-clicked
@@ -264,7 +268,7 @@ void OpenFilesListPlugin::OnTreeItemRightClick(wxTreeEvent &event)
 {
     if(Manager::IsAppShuttingDown())
         return;
-	EditorBase* ed = static_cast<EditorBase*>(static_cast<OpenFilesListData*>(m_pTree->GetItemData(event.GetItem()))->GetEditor());
+    EditorBase* ed = static_cast<EditorBase*>(static_cast<OpenFilesListData*>(m_pTree->GetItemData(event.GetItem()))->GetEditor());
     if(ed)
     {
         wxPoint pt = m_pTree->ClientToScreen(event.GetPoint());
@@ -294,36 +298,59 @@ void OpenFilesListPlugin::OnUpdateUI(wxUpdateUIEvent& event)
 
 void OpenFilesListPlugin::OnEditorActivated(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorActivated: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor());
+//  Manager::Get()->GetLogManager()->Log(_T("OnEditorActivated: ") + event.GetEditor()->GetFilename());
+    RefreshOpenFilesTree(event.GetEditor());
 }
 
 void OpenFilesListPlugin::OnEditorClosed(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorClosed: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor(), true);
+//  Manager::Get()->GetLogManager()->Log(_T("OnEditorClosed: ") + event.GetEditor()->GetFilename());
+    RefreshOpenFilesTree(event.GetEditor(), true);
 }
 
 void OpenFilesListPlugin::OnEditorDeactivated(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorDeactivated: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor());
+//  Manager::Get()->GetLogManager()->Log(_T("OnEditorDeactivated: ") + event.GetEditor()->GetFilename());
+    RefreshOpenFilesTree(event.GetEditor());
 }
 
 void OpenFilesListPlugin::OnEditorModified(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorModified: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor());
+//  Manager::Get()->GetLogManager()->Log(_T("OnEditorModified: ") + event.GetEditor()->GetFilename());
+    RefreshOpenFilesTree(event.GetEditor());
 }
 
 void OpenFilesListPlugin::OnEditorOpened(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorOpened: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor());
+    EditorBase* eb = event.GetEditor();
+    if (Manager::Get()->GetProjectManager()->IsBusy() && eb && (m_EditorArray.Index(eb) == wxNOT_FOUND))
+    {
+        m_EditorArray.Add(eb);
+    }
+    else
+    {
+        RefreshOpenFilesTree(eb);
+    }
 }
 
 void OpenFilesListPlugin::OnEditorSaved(CodeBlocksEvent& event)
 {
-//	Manager::Get()->GetLogManager()->Log(_T("OnEditorSaved: ") + event.GetEditor()->GetFilename());
-	RefreshOpenFilesTree(event.GetEditor());
+//  Manager::Get()->GetLogManager()->Log(_T("OnEditorSaved: ") + event.GetEditor()->GetFilename());
+    RefreshOpenFilesTree(event.GetEditor());
+}
+
+void OpenFilesListPlugin::OnProjectOpened(CodeBlocksEvent& event)
+{
+    if (m_EditorArray.IsEmpty())
+        return;
+
+    for (size_t i = 0; i < m_EditorArray.GetCount(); ++i)
+    {
+        EditorBase* eb = m_EditorArray[i];
+        if (eb)
+        {
+            RefreshOpenFilesTree(eb);
+        }
+    }
+    m_EditorArray.Clear();
 }
--- src/plugins/openfileslist/openfileslistplugin.h
+++ src/plugins/openfileslist/openfileslistplugin.h
@@ -3,48 +3,55 @@
 
 #include <cbplugin.h>
 
+#include <wx/dynarray.h>
+
 class wxTreeCtrl;
 class wxTreeEvent;
 class wxMenuBar;
 class wxImageList;
 class EditorBase;
 
+WX_DEFINE_ARRAY(EditorBase*, EditorArray);
+
 class OpenFilesListPlugin : public cbPlugin
 {
-	public:
-		OpenFilesListPlugin();
-		virtual ~OpenFilesListPlugin();
+    public:
+        OpenFilesListPlugin();
+        virtual ~OpenFilesListPlugin();
 
         virtual int GetConfigurationGroup() const { return cgEditor; }
 
-		virtual void BuildMenu(wxMenuBar* menuBar);
+        virtual void BuildMenu(wxMenuBar* menuBar);
         virtual void BuildModuleMenu(const ModuleType type, wxMenu* menu, const FileTreeData* data = 0){}
         virtual bool BuildToolBar(wxToolBar* toolBar){ return false; }
 
         virtual void OnAttach();
         virtual void OnRelease();
-	protected:
-		int GetOpenFilesListIcon(EditorBase* ed);
+    protected:
+        int GetOpenFilesListIcon(EditorBase* ed);
         void RebuildOpenFilesTree();
         void RefreshOpenFilesTree(EditorBase* ed, bool remove = false);
 
         void OnTreeItemActivated(wxTreeEvent &event);
         void OnTreeItemRightClick(wxTreeEvent &event);
-		void OnViewOpenFilesTree(wxCommandEvent& event);
-		void OnUpdateUI(wxUpdateUIEvent& event);
-		
-		void OnEditorActivated(CodeBlocksEvent& event);
-		void OnEditorClosed(CodeBlocksEvent& event);
-		void OnEditorDeactivated(CodeBlocksEvent& event);
-		void OnEditorModified(CodeBlocksEvent& event);
-		void OnEditorOpened(CodeBlocksEvent& event);
-		void OnEditorSaved(CodeBlocksEvent& event);
+        void OnViewOpenFilesTree(wxCommandEvent& event);
+        void OnUpdateUI(wxUpdateUIEvent& event);
+
+        void OnEditorActivated(CodeBlocksEvent& event);
+        void OnEditorClosed(CodeBlocksEvent& event);
+        void OnEditorDeactivated(CodeBlocksEvent& event);
+        void OnEditorModified(CodeBlocksEvent& event);
+        void OnEditorOpened(CodeBlocksEvent& event);
+        void OnEditorSaved(CodeBlocksEvent& event);
+
+        void OnProjectOpened(CodeBlocksEvent& event);
 
         wxTreeCtrl* m_pTree;
         wxImageList* m_pImages;
         wxMenu* m_ViewMenu;
-	private:
-		DECLARE_EVENT_TABLE();
+    private:
+        EditorArray m_EditorArray;
+        DECLARE_EVENT_TABLE();
 };
 
 
