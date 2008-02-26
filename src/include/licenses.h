a870199669ba6ec5c98558c6a9d258dea4fe8676
Yiannis Mandravellos <mandrav@codeblocks.org>
2008-02-26 21:55:24 +0000

    - Changed label's color from grey to black (was almost invisible) in GCV dialog. * Changed license text to GPL-3.0 (still the file headers remain), * Updated about-box.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4908 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/include/licenses.h
+++ src/include/licenses.h
@@ -2,8 +2,8 @@
 #define LICENSES_H
 
 // license macros
-#define LICENSE_GPL _("This program is licensed under the terms\nof the GNU General Public License version 2\n\n" \
-  "Available online under:\nhttp://www.gnu.org/licenses/old-licenses/gpl-2.0.html")
+#define LICENSE_GPL _("This program is licensed under the terms\nof the GNU General Public License version 3\n\n" \
+  "Available online under:\nhttp://www.gnu.org/licenses/gpl.html")
 
 #define LICENSE_WXWINDOWS _("This program is distributed under the wxWindows Library License version 3\n\n" \
   "http://www.wxwidgets.org/about/licence3.txt")
