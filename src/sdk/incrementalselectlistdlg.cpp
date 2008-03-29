feddec397c99215421f446af179ee9a55e0c8801
Biplab Kumar Modak <biplab@codeblocks.org>
2008-03-29 17:52:38 +0000

    * Added: Support for Page-Up/Down, Home, End key in IncrementalSelectListDlg.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@4976 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/incrementalselectlistdlg.cpp
+++ src/sdk/incrementalselectlistdlg.cpp
@@ -26,74 +26,95 @@ END_EVENT_TABLE()
 
 void myHandler::OnKeyDown(wxKeyEvent& event)
 {
-	//Manager::Get()->GetLogManager()->Log(mltDevDebug, "OnKeyDown");
-	switch (event.GetKeyCode())
-	{
-		case WXK_RETURN:
-			m_pParent->EndModal(wxID_OK);
-			break;
+    //Manager::Get()->GetLogManager()->Log(mltDevDebug, "OnKeyDown");
+    size_t sel = 0;
+    switch (event.GetKeyCode())
+    {
+        case WXK_RETURN:
+            m_pParent->EndModal(wxID_OK);
+            break;
 
-		case WXK_ESCAPE:
-			m_pParent->EndModal(wxID_CANCEL);
-			break;
+        case WXK_ESCAPE:
+            m_pParent->EndModal(wxID_CANCEL);
+            break;
 
-		case WXK_UP:
-			m_pList->SetSelection(m_pList->GetSelection() - 1);
-			break;
+        case WXK_UP:
+            sel = m_pList->GetSelection() - 1;
+            m_pList->SetSelection(sel == (size_t) -1 ? 0 : sel);
+            break;
 
-		case WXK_DOWN:
-			m_pList->SetSelection(m_pList->GetSelection() + 1);
-			break;
+        case WXK_DOWN:
+            m_pList->SetSelection(m_pList->GetSelection() + 1);
+            break;
 
-		default:
-			event.Skip();
-			break;
-	}
+        case WXK_PAGEUP:
+            sel = m_pList->GetSelection() - 10;
+            m_pList->SetSelection( sel > m_pList->GetCount() ? 0 : sel );
+            break;
+
+        case WXK_PAGEDOWN:
+            sel = m_pList->GetSelection() + 10;
+            m_pList->SetSelection( sel >= m_pList->GetCount() ? m_pList->GetCount() - 1 : sel );
+            break;
+
+        case WXK_HOME:
+            m_pList->SetSelection(0);
+            break;
+
+        case WXK_END:
+            m_pList->SetSelection( m_pList->GetCount() - 1 );
+            event.Skip();
+            break;
+
+        default:
+            event.Skip();
+            break;
+    }
 }
 
 BEGIN_EVENT_TABLE(IncrementalSelectListDlg, wxDialog)
-	EVT_TEXT(XRCID("txtSearch"), IncrementalSelectListDlg::OnSearch)
-	EVT_LISTBOX_DCLICK(XRCID("lstItems"), IncrementalSelectListDlg::OnSelect)
+    EVT_TEXT(XRCID("txtSearch"), IncrementalSelectListDlg::OnSearch)
+    EVT_LISTBOX_DCLICK(XRCID("lstItems"), IncrementalSelectListDlg::OnSelect)
 END_EVENT_TABLE()
 
 IncrementalSelectListDlg::IncrementalSelectListDlg(wxWindow* parent, const wxArrayString& items, const wxString& caption, const wxString& message)
-	: m_pMyEvtHandler(0L),
-	m_List(0L),
-	m_Text(0L),
-	m_Items(items)
+    : m_pMyEvtHandler(0L),
+    m_List(0L),
+    m_Text(0L),
+    m_Items(items)
 {
-	wxXmlResource::Get()->LoadDialog(this, parent, _T("dlgIncrementalSelectList"));
-	if (!caption.IsEmpty())
-		SetTitle(caption);
-	if (!message.IsEmpty())
-		XRCCTRL(*this, "lblMessage", wxStaticText)->SetLabel(message);
+    wxXmlResource::Get()->LoadDialog(this, parent, _T("dlgIncrementalSelectList"));
+    if (!caption.IsEmpty())
+        SetTitle(caption);
+    if (!message.IsEmpty())
+        XRCCTRL(*this, "lblMessage", wxStaticText)->SetLabel(message);
 
-	m_Text = XRCCTRL(*this, "txtSearch", wxTextCtrl);
-	m_List = XRCCTRL(*this, "lstItems", wxListBox);
+    m_Text = XRCCTRL(*this, "txtSearch", wxTextCtrl);
+    m_List = XRCCTRL(*this, "lstItems", wxListBox);
 
-	myHandler* m_pMyEvtHandler = new myHandler(this, m_Text, m_List);
-	m_Text->SetNextHandler(m_pMyEvtHandler);
-	m_List->SetNextHandler(m_pMyEvtHandler);
+    myHandler* m_pMyEvtHandler = new myHandler(this, m_Text, m_List);
+    m_Text->SetNextHandler(m_pMyEvtHandler);
+    m_List->SetNextHandler(m_pMyEvtHandler);
 
-	FillList();
+    FillList();
 }
 
 IncrementalSelectListDlg::~IncrementalSelectListDlg()
 {
-	m_Text->SetNextHandler(0L);
-	m_List->SetNextHandler(0L);
+    m_Text->SetNextHandler(0L);
+    m_List->SetNextHandler(0L);
 
-	delete m_pMyEvtHandler;
+    delete m_pMyEvtHandler;
 }
 
 wxString IncrementalSelectListDlg::GetStringSelection()
 {
-	return m_List->GetStringSelection();
+    return m_List->GetStringSelection();
 }
 
 int IncrementalSelectListDlg::GetSelection()
 {
-	return m_List->GetSelection();
+    return m_List->GetSelection();
 }
 
 void IncrementalSelectListDlg::FillList()
@@ -101,21 +122,21 @@ void IncrementalSelectListDlg::FillList()
     Freeze();
 
     // We put a star before and after pattern to find search expression everywhere in path
-	wxString search(wxT("*") + m_Text->GetValue().Lower() + wxT("*"));
+    wxString search(wxT("*") + m_Text->GetValue().Lower() + wxT("*"));
 
-	wxArrayString result;
-	//Manager::Get()->GetLogManager()->Log(mltDevDebug, "FillList(): '%s'", search.c_str());
-	m_List->Clear();
-	for (unsigned int i = 0; i < m_Items.GetCount(); ++i)
-	{
-		// 2 for before and after stars =~ empty string
-		if ((search.Length()==2) || m_Items[i].Lower().Matches(search.c_str()))
-			result.Add(m_Items[i]);
-//			m_List->Append(m_Items[i]);
-	}
-	m_List->Set(result);
-	if (m_List->GetCount())
-		m_List->SetSelection(0);
+    wxArrayString result;
+    //Manager::Get()->GetLogManager()->Log(mltDevDebug, "FillList(): '%s'", search.c_str());
+    m_List->Clear();
+    for (unsigned int i = 0; i < m_Items.GetCount(); ++i)
+    {
+        // 2 for before and after stars =~ empty string
+        if ((search.Length()==2) || m_Items[i].Lower().Matches(search.c_str()))
+            result.Add(m_Items[i]);
+//          m_List->Append(m_Items[i]);
+    }
+    m_List->Set(result);
+    if (m_List->GetCount())
+        m_List->SetSelection(0);
     Thaw();
 }
 
@@ -123,10 +144,10 @@ void IncrementalSelectListDlg::FillList()
 
 void IncrementalSelectListDlg::OnSearch(wxCommandEvent& event)
 {
-	FillList();
+    FillList();
 }
 
 void IncrementalSelectListDlg::OnSelect(wxCommandEvent& event)
 {
-	EndModal(wxID_OK);
+    EndModal(wxID_OK);
 }
