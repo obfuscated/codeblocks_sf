26f1f470852c2b3179f29daba7a10cb2a522b58b
Lieven de Cock <killerbot@codeblocks.org>
2009-02-02 18:59:06 +0000

    - remove warning
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5434 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/codecompletion/parser/tokenizer.cpp
+++ src/plugins/codecompletion/parser/tokenizer.cpp
@@ -399,7 +399,7 @@ bool Tokenizer::SkipUnwanted()
             (!m_IsOperator && CurrentChar() == '=') ||
             (!m_IsOperator && CurrentChar() == '[') ||
             CurrentChar() == '?' ||
-            CurrentChar() == '/' && (NextChar() == '/' || NextChar() == '*') )
+            (CurrentChar() == '/' && (NextChar() == '/' || NextChar() == '*') ))
     {
         bool skipPreprocessor = false; // used for #include
         while (m_Buffer.Mid(m_TokenIndex, 2) == _T("//") ||
