1fb173f6ac6318b5e610ed93845e1d3b29176d2f
Biplab Kumar Modak <biplab@codeblocks.org>
2009-05-01 13:28:31 +0000

    * wx-2.9 migration: Fixes for cbProject.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5557 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/cbproject.cpp
+++ src/sdk/cbproject.cpp
@@ -1726,7 +1726,11 @@ ProjectBuildTarget* cbProject::AddBuildTarget(const wxString& targetName)
     if (HasVirtualBuildTarget(targetName))
     {
         RemoveVirtualBuildTarget(targetName);
+        #if wxCHECK_VERSION(2, 9, 0)
+        Manager::Get()->GetLogManager()->LogWarning(F(_T("Deleted existing virtual target '%s' because real target was added with the same name"), targetName.wx_str()));
+        #else
         Manager::Get()->GetLogManager()->LogWarning(F(_T("Deleted existing virtual target '%s' because real target was added with the same name"), targetName.c_str()));
+        #endif
     }
 
     SetModified(true);
@@ -1984,7 +1988,11 @@ void cbProject::ReOrderTargets(const wxArrayString& nameOrder)
         ProjectBuildTarget* target = GetBuildTarget(nameOrder[i]);
         if (!target)
         {
+            #if wxCHECK_VERSION(2, 9, 0)
+            msgMan->DebugLog(F(_T("cbProject::ReOrderTargets() : Target \"%s\" not found..."), nameOrder[i].wx_str()));
+            #else
             msgMan->DebugLog(F(_T("cbProject::ReOrderTargets() : Target \"%s\" not found..."), nameOrder[i].c_str()));
+            #endif
             break;
         }
 
@@ -2016,14 +2024,22 @@ bool cbProject::DefineVirtualBuildTarget(const wxString& alias, const wxArrayStr
 {
     if (targets.GetCount() == 0)
     {
+        #if wxCHECK_VERSION(2, 9, 0)
+        Manager::Get()->GetLogManager()->LogWarning(F(_T("Can't define virtual build target '%s': Group of build targets is empty!"), alias.wx_str()));
+        #else
         Manager::Get()->GetLogManager()->LogWarning(F(_T("Can't define virtual build target '%s': Group of build targets is empty!"), alias.c_str()));
+        #endif
         return false;
     }
 
     ProjectBuildTarget* existing = GetBuildTarget(alias);
     if (existing)
     {
+        #if wxCHECK_VERSION(2, 9, 0)
+        Manager::Get()->GetLogManager()->LogWarning(F(_T("Can't define virtual build target '%s': Real build target exists with that name!"), alias.wx_str()));
+        #else
         Manager::Get()->GetLogManager()->LogWarning(F(_T("Can't define virtual build target '%s': Real build target exists with that name!"), alias.c_str()));
+        #endif
         return false;
     }
 
