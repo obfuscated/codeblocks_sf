7e692e795d6871e9740ac69450bb45e064c02edd
Pecan Heber <pecan@codeblocks.org>
2009-06-08 14:31:03 +0000

    -Adding CodeSnippets GenericMessageBox
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5625 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/codesnippets/GenericMessageBox.h
@@ -0,0 +1,70 @@
+#ifndef GenericMessageBox_H
+#define GenericMessageBox_H
+
+
+////class GenericMessageBox
+////{
+////    public:
+////        GenericMessageBox();
+////        virtual ~GenericMessageBox();
+////    protected:
+////    private:
+////};
+
+#endif // GenericMessageBox_H
+
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/generic/msgdlgg.h
+// Purpose:     Generic wxMessageDialog
+// Author:      Julian Smart
+// Modified by:
+// Created:     01/02/97
+// RCS-ID:      $Id: msgdlgg.h 49765 2007-11-09 18:32:38Z DE $
+// Copyright:   (c) Julian Smart
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef GenericMessageDialog_h
+#define GenericMessageDialog_h
+
+#include "wx/defs.h"
+#include "wx/dialog.h"
+#include "wx/msgdlg.h"
+#include "wx/generic/msgdlgg.h"
+
+// type is an 'or' (|) of wxOK, wxCANCEL, wxYES_NO
+// Returns wxYES/NO/OK/CANCEL
+
+//-extern WXDLLEXPORT_DATA(const wxChar) wxMessageBoxCaptionStr[];
+
+// ----------------------------------------------------------------------------
+class GenericMessageDialog: public wxDialog, public wxMessageDialogBase
+// ----------------------------------------------------------------------------
+{
+DECLARE_DYNAMIC_CLASS(GenericMessageDialog)
+
+public:
+    GenericMessageDialog(wxWindow *parent, const wxString& message,
+        const wxString& caption = wxMessageBoxCaptionStr,
+        long style = wxOK|wxCENTRE, const wxPoint& pos = wxDefaultPosition);
+
+    void OnYes(wxCommandEvent& event);
+    void OnNo(wxCommandEvent& event);
+    void OnCancel(wxCommandEvent& event);
+
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+// ----------------------------------------------------------------------------
+// GenericMessageBox: the simplest way to use GenericMessageDialog
+// ----------------------------------------------------------------------------
+
+int GenericMessageBox(const wxString& message,
+                             const wxString& caption = wxMessageBoxCaptionStr,
+                             long style = wxOK | wxCENTRE,
+                             wxWindow *parent = ::wxGetActiveWindow(),
+                             int x = wxDefaultCoord, int y = wxDefaultCoord);
+
+
+#endif // GenericMessageDialog.h
