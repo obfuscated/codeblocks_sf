c884824e4866688121cdbea62e36350f4baa2cfa
Lieven de Cock <killerbot@codeblocks.org>
2009-06-17 21:13:25 +0000

    * support vector debugging
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5653 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/debuggergdb/gdb_driver.cpp
+++ src/plugins/debuggergdb/gdb_driver.cpp
@@ -277,6 +277,10 @@ void GDB_driver::Prepare(ProjectBuildTarget* target, bool isConsole)
     m_Types.Clear();
     InitializeScripting();
 
+    wxString StlDebugCommand(_T("source $DATAPATH/scripts/stl-views-1.0.3.gdb"));
+    Manager::Get()->GetMacrosManager()->ReplaceMacros(StlDebugCommand);
+    QueueCommand(new DebuggerCmd(this, StlDebugCommand));
+
     // pass user init-commands
     wxString init = Manager::Get()->GetConfigManager(_T("debugger"))->Read(_T("init_commands"), wxEmptyString);
     // commands are passed in one go, in case the user defines functions in there
