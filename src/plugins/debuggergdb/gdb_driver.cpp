b49f78f9524327000ada129c0176d12f5d8b5235
Martin Halle <mortenmacfly@codeblocks.org>
2009-06-19 07:24:34 +0000

    * debugger: applied patch #2773 (UI to send command to the debugger directly). Thanks oBFusCATed! - couple of fixes, backport from wxaui branch
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5662 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/debuggergdb/gdb_driver.cpp
+++ src/plugins/debuggergdb/gdb_driver.cpp
@@ -624,7 +624,8 @@ void GDB_driver::MemoryDump()
 
 void GDB_driver::RunningThreads()
 {
-    QueueCommand(new GdbCmd_Threads(this, m_pThreads));
+    if (m_pThreads)
+        QueueCommand(new GdbCmd_Threads(this, m_pThreads));
 }
 
 void GDB_driver::InfoFrame()
