df21c3d14bf055eabdb2bf1551a368244d94a58d
Martin Halle <mortenmacfly@codeblocks.org>
2009-10-07 07:03:51 +0000

    - CC: added (simple) STL test cases
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5852 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/codecompletion/testing/stl.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+int main (void)
+{
+//  std::
+
+  std::string s;
+//  s.
+
+  std::vector v;
+//  v.
+}
---
+++ src/plugins/codecompletion/testing/stl_namespace.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+void nop (void)
+{
+//  std::
+
+  string s;
+//  s.
+
+  vector v;
+//  v.
+}
