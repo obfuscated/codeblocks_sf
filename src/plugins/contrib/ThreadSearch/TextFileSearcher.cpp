5b2dc10f468cac00280fd1ac2838774542632a08
Jens Lody <jens@codeblocks.org>
2009-10-26 15:22:01 +0000

    * ThreadSearch: skip empty files, otherwise we can get a "Failed to open" errormessage.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5890 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/ThreadSearch/TextFileSearcher.cpp
+++ src/plugins/contrib/ThreadSearch/TextFileSearcher.cpp
@@ -55,104 +55,108 @@ TextFileSearcher::eFileSearcherReturn TextFileSearcher::FindInFile(const wxStrin
     eFileSearcherReturn success=idStringNotFound;
     wxString line;
 
-    // Tests file existence
-    if ( !wxFileName::FileExists(filePath) )
+    // skip empty fles
+    if(wxFileName::GetSize(filePath) != 0)
     {
-        // We skip missing files without alerting user.
-        // If a file has disappeared, it is not our problem.
-        // cbMessageBox( filePath + _T(" does not exist."), _T("Error"), wxICON_ERROR);
-        return idFileNotFound;
-    }
-
-    // File open
-    EncodingDetector enc(filePath, false);
-    if( !enc.IsOK())
-    {
-        return idFileOpenError;
-    }
-    wxFontEncoding fe =  enc.GetFontEncoding();
-
-    if ( fe == wxFONTENCODING_UTF7 )
-    {
-        wxMBConvUTF7 conv;
-        if ( !m_TextFile.Open(filePath, conv) )
+        // Tests file size and existence
+        if ( !wxFileName::FileExists(filePath) )
         {
-            return idFileOpenError;
-        }
-    }
-    else if ( fe == wxFONTENCODING_UTF8 )
-    {
-        wxMBConvUTF8 conv;
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
-        }
-    }
-    else if ( fe == wxFONTENCODING_UTF16BE )
-    {
-        wxMBConvUTF16BE conv;
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
-        }
-    }
-    else if ( fe == wxFONTENCODING_UTF16LE )
-    {
-        wxMBConvUTF16LE conv;
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
-        }
-    }
-    else if ( fe == wxFONTENCODING_UTF32BE )
-    {
-        wxMBConvUTF32BE conv;
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
-        }
-    }
-    else if ( fe == wxFONTENCODING_UTF32LE )
-    {
-        wxMBConvUTF32LE conv;
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
-        }
-    }
-    else
-    {
-        wxCSConv conv(fe);
-        if ( !m_TextFile.Open(filePath, conv) )
-        {
-            return idFileOpenError;
+            // We skip missing files without alerting user.
+            // If a file has disappeared, it is not our problem.
+            // cbMessageBox( filePath + _T(" does not exist."), _T("Error"), wxICON_ERROR);
+            return idFileNotFound;
         }
 
-    }
-
-
-    // Tests all file lines
-    for ( size_t i = 0; i < m_TextFile.GetLineCount(); ++i )
-    {
-        line = m_TextFile.GetLine(i);
-        if ( MatchLine(line) )
+        // File open
+        EncodingDetector enc(filePath, false);
+        if( !enc.IsOK())
         {
-            success=idStringFound;
-            // An interesting line is found. We clean and add it to the provided array
-            line.Replace(_T("\t"), _T(" "));
-            line.Replace(_T("\r"), _T(" "));
-            line.Replace(_T("\n"), _T(" "));
-            line.Trim(false);
-            line.Trim(true);
-
-            foundLines.Add(wxString::Format(wxT("%d"), i + 1));
-            foundLines.Add(line);
+    //        return idFileOpenError;
+            return idFileNotFound;
         }
+        wxFontEncoding fe =  enc.GetFontEncoding();
+
+        if ( fe == wxFONTENCODING_UTF7 )
+        {
+            wxMBConvUTF7 conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else if ( fe == wxFONTENCODING_UTF8 )
+        {
+            wxMBConvUTF8 conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else if ( fe == wxFONTENCODING_UTF16BE )
+        {
+            wxMBConvUTF16BE conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else if ( fe == wxFONTENCODING_UTF16LE )
+        {
+            wxMBConvUTF16LE conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else if ( fe == wxFONTENCODING_UTF32BE )
+        {
+            wxMBConvUTF32BE conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else if ( fe == wxFONTENCODING_UTF32LE )
+        {
+            wxMBConvUTF32LE conv;
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+        }
+        else
+        {
+            wxCSConv conv(fe);
+            if ( !m_TextFile.Open(filePath, conv) )
+            {
+                return idFileOpenError;
+            }
+
+        }
+
+
+        // Tests all file lines
+        for ( size_t i = 0; i < m_TextFile.GetLineCount(); ++i )
+        {
+            line = m_TextFile.GetLine(i);
+            if ( MatchLine(line) )
+            {
+                success=idStringFound;
+                // An interesting line is found. We clean and add it to the provided array
+                line.Replace(_T("\t"), _T(" "));
+                line.Replace(_T("\r"), _T(" "));
+                line.Replace(_T("\n"), _T(" "));
+                line.Trim(false);
+                line.Trim(true);
+
+                foundLines.Add(wxString::Format(wxT("%d"), i + 1));
+                foundLines.Add(line);
+            }
+        }
+
+        // File close
+        m_TextFile.Close();
     }
-
-    // File close
-    m_TextFile.Close();
-
     return success;
 }
 
