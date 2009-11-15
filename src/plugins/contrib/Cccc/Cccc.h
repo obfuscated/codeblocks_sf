76a645a52725687ed7595ee112fce765232437d5
Lieven de Cock <killerbot@codeblocks.org>
2009-11-15 17:27:43 +0000

    * added CCCC (C and C++ Code Counter) plug-in
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@5915 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/Cccc/Cccc.h
@@ -0,0 +1,32 @@
+/***************************************************************
+ * Name:      Cccc.h
+ * Purpose:   Code::Blocks Valgrind plugin: main functions
+ * Author:    Lieven de Cock (aka killerbot)
+ * Created:   12/11/2009
+ * Copyright: (c) Lieven de Cock (aka killerbot)
+ * License:   GPL
+ **************************************************************/
+
+#ifndef CCCC_H_INCLUDED
+#define CCCC_H_INCLUDED
+
+#include "cbplugin.h" // the base class we 're inheriting
+
+class TextCtrlLogger;
+
+class Cccc : public cbToolPlugin
+{
+public:
+	Cccc();
+	~Cccc();
+	void OnAttach(); // fires when the plugin is attached to the application
+	void OnRelease(bool appShutDown); // fires when the plugin is released from the application
+	int Execute();
+private:
+	void AppendToLog(const wxString& Text);
+
+	TextCtrlLogger*	m_CcccLog;	//!< log tab in the message pane
+	int m_LogPageIndex;	//!< index of our log tab (can this change during run time ??)
+};
+
+#endif // CCCC_H_INCLUDED
