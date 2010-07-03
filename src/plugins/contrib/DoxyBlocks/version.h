e075379399ddadbde7ac3ef5b4cae40989fb56b2
Lieven de Cock <killerbot@codeblocks.org>
2010-07-03 07:17:24 +0000

    - version
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@6382 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/DoxyBlocks/version.h
+++ src/plugins/contrib/DoxyBlocks/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "27";
-	static const char MONTH[] = "06";
+	static const char DATE[] = "03";
+	static const char MONTH[] = "07";
 	static const char YEAR[] = "2010";
-	static const char UBUNTU_VERSION_STYLE[] = "10.06";
+	static const char UBUNTU_VERSION_STYLE[] = "10.07";
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 6;
-	static const long BUILD = 551;
-	static const long REVISION = 7781;
+	static const long BUILD = 552;
+	static const long REVISION = 7786;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1175;
-	#define RC_FILEVERSION 1,6,551,7781
-	#define RC_FILEVERSION_STRING "1, 6, 551, 7781\0"
-	static const char FULLVERSION_STRING[] = "1.6.551.7781";
+	#define RC_FILEVERSION 1,6,552,7786
+	#define RC_FILEVERSION_STRING "1, 6, 552, 7786\0"
+	static const char FULLVERSION_STRING[] = "1.6.552.7786";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 24;
+	static const long BUILD_HISTORY = 25;
 	
 
 }
