bb7a98514ce71d508e2840afbb4f81ff1bdb0c7a
Martin Halle <mortenmacfly@codeblocks.org>
2010-09-20 19:11:27 +0000

    * "fixed" build bug by applying path to remove dependency on libintl (thanks Loaden) - removed target "exchndl" anyways from virtual target "All" thus it won't built until explicitly done
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@6609 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/base/exchndl/intlimpl.c
@@ -0,0 +1,9 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU Lesser General Public License, version 3
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ */
+
+const char* libintl_dgettext (const char *__domainname, const char *__msgid)
+{
+    return __msgid;
+}
