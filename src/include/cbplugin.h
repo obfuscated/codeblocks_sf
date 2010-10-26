99bb77172ef313aa5ed34cb08f53fbaa234281da
Yuchen Deng <loaden@codeblocks.org>
2010-10-26 08:22:36 +0000

    * update sdk version
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@6766 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/include/cbplugin.h
+++ src/include/cbplugin.h
@@ -36,7 +36,7 @@
 // it will change when the SDK interface breaks
 #define PLUGIN_SDK_VERSION_MAJOR 1
 #define PLUGIN_SDK_VERSION_MINOR 11
-#define PLUGIN_SDK_VERSION_RELEASE 12
+#define PLUGIN_SDK_VERSION_RELEASE 13
 
 // class decls
 class wxMenuBar;
