2e7fcefbe1b843841e9c413eab9559777efb4ca8
Yuchen Deng <loaden@codeblocks.org>
2010-12-01 10:29:40 +0000

    * apply patch to fixed get stripped arguments error, thank ollydbg!
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@6867 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/codecompletion/parser/token.cpp
+++ src/plugins/codecompletion/parser/token.cpp
@@ -279,6 +279,10 @@ wxString Token::GetFormattedArgs() const
 wxString Token::GetStrippedArgs() const
 {
     wxString args;
+    // the argument should have the format (xxxx = y, ....) or just an empty string
+    // if it is empty, we just return an empty string
+    if (m_Args.IsEmpty())
+        return args;
     args.Alloc(m_Args.Len() + 1);
     bool skipDefaultValue = false;
     for (size_t i = 0; i < m_Args.Len(); ++i)
