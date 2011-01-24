7749482d3647f302ceacb672c08df9efcad53a42
Jens Lody <jens@codeblocks.org>
2011-01-24 14:54:22 +0000

    * updated encoding-detection to latest version from mozillas git-repo
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@6933 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/include/mozilla_chardet/nsSBCSGroupProber.h
+++ src/include/mozilla_chardet/nsSBCSGroupProber.h
@@ -40,7 +40,7 @@
 #define nsSBCSGroupProber_h__
 
 
-#define NUM_OF_SBCS_PROBERS    16
+#define NUM_OF_SBCS_PROBERS    16 // include latin-2 probers jens 2011-01-20
 
 class nsCharSetProber;
 class nsSBCSGroupProber: public nsCharSetProber {
