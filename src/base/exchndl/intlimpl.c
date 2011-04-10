48c6a469781eb6af85910c9436f8f71976498385
Martin Halle <mortenmacfly@codeblocks.org>
2011-04-10 12:58:43 +0000

    - streamlining and synchronising of C::B project files (C::B core and core plugins only) - removed linintl hack for exchdl, as this will not work with recent compilers (e.g. TDM 4.5.2): solution: dependency (link against) on libintl
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7088 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/base/exchndl/intlimpl.c
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of the Code::Blocks IDE and licensed under the GNU Lesser General Public License, version 3
- * http://www.gnu.org/licenses/lgpl-3.0.html
- */
-
-const char* libintl_dgettext (const char *__domainname, const char *__msgid)
-{
-    return __msgid;
-}
