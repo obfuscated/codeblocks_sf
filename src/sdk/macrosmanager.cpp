973ca4185759fcf9332e1d4c02cb1553dc159691
Lieven de Cock <killerbot@codeblocks.org>
2011-06-03 08:22:33 +0000

    *fix regex to be not to gready
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7179 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/macrosmanager.cpp
+++ src/sdk/macrosmanager.cpp
@@ -84,7 +84,7 @@ void MacrosManager::Reset()
     m_RE_If.Compile(_T("\\$if\\(([^)]*)\\)[::space::]*(\\{([^}]*)\\})(\\{([^}]*)\\})?"), wxRE_EXTENDED | wxRE_NEWLINE);
     m_RE_IfSp.Compile(_T("[^=!<>]+|(([^=!<>]+)[ ]*(=|==|!=|>|<|>=|<=)[ ]*([^=!<>]+))"), wxRE_EXTENDED | wxRE_NEWLINE);
     m_RE_Script.Compile(_T("(\\[\\[(.*)\\]\\])"), wxRE_EXTENDED | wxRE_NEWLINE);
-    m_RE_ToFullPath.Compile(_T("\\$TO_ABSOLUTE_PATH{(.*)}"), wxRE_ADVANCED);
+    m_RE_ToFullPath.Compile(_T("\\$TO_ABSOLUTE_PATH{([^}]*)}"), wxRE_ADVANCED);
     m_UserVarMan = Manager::Get()->GetUserVariableManager();
     srand(time(0));
     assert(m_RE_Unix.IsValid());
