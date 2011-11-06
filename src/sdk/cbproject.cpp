69058c6bf8c78a7ddafac98f4ccec18b4908b8fa
Jens Lody <jens@codeblocks.org>
2011-11-06 18:29:12 +0000

    * revert commit 7307 and 7460, to increase loadspeed for large projects and fix the calculating of the common toplevel path, see: http://forums.codeblocks.org/index.php/topic,15430.msg103943.html#msg103943 for details
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7567 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/cbproject.cpp
+++ src/sdk/cbproject.cpp
@@ -53,34 +53,6 @@
 
 namespace compatibility { typedef TernaryCondTypedef<wxMinimumVersion<2,5>::eval, wxTreeItemIdValue, long int>::eval tree_cookie_t; };
 
-class ProjectDirTraverser : public wxDirTraverser
-{
-public:
-    ProjectDirTraverser() : m_IsValidPath(false)
-    {}
-
-    virtual wxDirTraverseResult OnFile(const wxString& filename)
-    {
-        const FileType ft = FileTypeOf(filename);
-        if (ft == ftCodeBlocksProject || ft == ftCodeBlocksWorkspace)
-        {
-            m_IsValidPath = true;
-            return wxDIR_STOP;
-        }
-        return wxDIR_CONTINUE;
-    }
-
-    virtual wxDirTraverseResult OnDir(const wxString& dirname)
-    {
-        return wxDIR_CONTINUE;
-    }
-
-    bool IsValidPath() { return m_IsValidPath; }
-
-private:
-    bool m_IsValidPath;
-};
-
 // class constructor
 cbProject::cbProject(const wxString& filename)
     : m_CustomMakefile(false),
@@ -376,17 +348,9 @@ void cbProject::CalculateCommonTopLevelPath()
 
         wxFileName tmpbaseF(tmpbase);
         tmpbaseF.Normalize(wxPATH_NORM_DOTS);
-        if (tmpbaseF.GetDirCount() && tmpbaseF.GetDirCount() < base.GetDirCount())
-        {
-            wxDir dir(tmpbaseF.GetPath());
-            if (dir.IsOpened())
-            {
-                ProjectDirTraverser traverser;
-                dir.Traverse(traverser, wxEmptyString, wxDIR_DIRS | wxDIR_FILES);
-                if (traverser.IsValidPath())
-                    base = tmpbaseF;
-            }
-        }
+        if ( tmpbaseF.GetDirCount() < base.GetDirCount() &&
+             base.GetPath().StartsWith(tmpbaseF.GetPath()))
+            base = tmpbaseF;
     }
 
     m_CommonTopLevelPath = base.GetFullPath();
