33fc5584c36b73314701fb3aea190f46615f3260
Martin Halle <mortenmacfly@codeblocks.org>
2012-02-01 10:10:15 +0000

    - consistent SVN EOL style and SVN keyword substitution for other files (finally...)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7757 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/wxscintilla/src/scintilla_generator/gen_iface.py
+++ src/sdk/wxscintilla/src/scintilla_generator/gen_iface.py
@@ -6,7 +6,7 @@
 # Author:       Robin Dunn
 #
 # Created:      5-Sept-2000
-# RCS-ID:       $Id: gen_iface.py 7291 2011-07-25 08:12:01Z mortenmacfly $
+# RCS-ID:       $Id$
 # Copyright:    (c) 2000 by Total Control Software
 # Licence:      wxWindows licence
 #----------------------------------------------------------------------------
