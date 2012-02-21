7dbd24b5dcf92635cd0e422227f98efc268a93dd
Martin Halle <mortenmacfly@codeblocks.org>
2012-02-21 19:55:28 +0000

    * added 64 bit project files (Windows), using a custom 64 bit compiler -> make a copy of "GNU GCC Compiler", rename it it to "GNU GCC 64 Compiler", so the project file works
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7829 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/batch_build_core_29.bat
@@ -0,0 +1,36 @@
+@echo off
+rem ----------------------------------------
+rem Setup C::B root folder of *binaries* (!)
+rem ----------------------------------------
+set CB_ROOT=C:\Devel\CodeBlocks
+rem ------------------------------------------
+rem Setup GCC root folder with "bin" subfolder
+rem ------------------------------------------
+set GCC_ROOT=C:\Devel\GCC46TDM
+
+rem -------------------------------------------
+rem Usually below here no changes are required.
+rem -------------------------------------------
+if not exist "%CB_ROOT%"  goto ErrNoCB
+if not exist "%GCC_ROOT%" goto ErrNoGCC
+set PATH=%CB_ROOT%;%GCC_ROOT%;%PATH%
+
+set START_CMD=start "Code::Blocks Build" /D%~dp0 /min /b
+set CB_EXE="%CB_ROOT%\codeblocks.exe"
+set CB_PARAMS=--batch-build-notify --no-batch-window-close
+set CB_CMD=--build "%~dp0CodeBlocks_wx29.cbp"
+
+set CB_TARGET=--target=All
+%START_CMD% %CB_EXE% %CB_PARAMS% %CB_TARGET% %CB_CMD%
+echo Do not forget to run "update_29.bat" after successful build!
+goto TheEnd
+
+:ErrNoCB
+echo Error: C::B root folder not found. Adjust batch file accordingly
+goto TheEnd
+
+:ErrNoGCC
+echo Error: GCC root folder not found. Adjust batch file accordingly
+goto TheEnd
+
+:TheEnd
