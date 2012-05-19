cd43d875ccafcabfda122dd7b39f3c76bc7a11cd
Martin Halle <mortenmacfly@codeblocks.org>
2012-05-19 12:17:32 +0000

    - editor style (no functional change)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7978 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/include/compiletargetbase.h
+++ src/include/compiletargetbase.h
@@ -34,7 +34,7 @@ enum TargetType
 {
     ttExecutable    = 0, /**< Target produces an executable */
     ttConsoleOnly   = 1, /**< Target produces a console executable (without GUI) (distinction between ttExecutable and ttConsoleOnly happens only under Win32) */
-    ttStaticLib        = 2, /**< Target produces a static library */
+    ttStaticLib     = 2, /**< Target produces a static library */
     ttDynamicLib    = 3, /**< Target produces a dynamic library */
     ttCommandsOnly  = 4, /**< Target only runs commands in pre-build and/or post-build steps */
     ttNative        = 5, /**< Target produces a native binary */
