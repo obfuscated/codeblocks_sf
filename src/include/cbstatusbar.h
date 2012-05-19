1f165b031e49d592ac3e8051d6405c3238f1f58d
Martin Halle <mortenmacfly@codeblocks.org>
2012-05-19 12:14:38 +0000

    * cbStatusBar updates / refinements, some slipped already in with last commit
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@7974 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/include/cbstatusbar.h
+++ src/include/cbstatusbar.h
@@ -8,11 +8,10 @@
 
 #include <vector>
 #include <wx/statusbr.h>
+
 #include "settings.h"
 
-
 class cbPlugin;
-
 
 /** @brief A custom status bar which can contain controls, icons...
   */
@@ -20,31 +19,25 @@ class DLLIMPORT cbStatusBar : public wxStatusBar
 {
 private:
     friend class MainFrame; // gets initialized from mainframe
-    cbStatusBar(wxWindow *parent,  wxWindowID id, long style, const wxString& name); // this will be used from friend mainframe
-    cbStatusBar(){};
+
+    // this will be used from friend mainframe
+    cbStatusBar(wxWindow* parent,  wxWindowID id, long style, const wxString& name);
+    cbStatusBar() { ; };
     cbStatusBar ( const cbStatusBar& );
-    ~cbStatusBar();
-//private:
-//    virtual bool GetFieldRect(int i, wxRect& rect) const
-//    virtual wxString GetStatusText(int i = 0) const
-//    void PopStatusText(int field = 0)
-//    void PushStatusText(const wxString& string, int field = 0)
-//    virtual void SetFieldsCount(int number = 1, int* widths = NULL)
-//    virtual void SetStatusText(const wxString& text, int i = 0)
-//    virtual void SetStatusStyles(int n, int *styles)
+    virtual ~cbStatusBar();
 
 public:
     /** Sets the text for one field.
         @param text  The text to be set.
         @param plugin  The pugin which wants to set its status text
       */
-    virtual void SetStatusText(const wxString& text, cbPlugin *plugin);
+    virtual void SetStatusText(const wxString& text, cbPlugin* plugin);
 
     /** Returns the string associated with a status bar field.
         @param  plugin  The plugin which wants to get its status text
         @return The  status field string if the field id is valid, otherwise the empty string.
       */
-    virtual wxString GetStatusText(cbPlugin *plugin) const;
+    virtual wxString GetStatusText(cbPlugin* plugin) const;
 
     /** Returns the size and position of a field's internal bounding rectangle.
         @param  plugin  The plugin which wants to know its status-field size.
@@ -53,54 +46,47 @@ public:
       */
     virtual bool GetFieldRect(cbPlugin *plugin, wxRect& rect) const;
 
-private:
-    void AdjustFieldsSize();
-    void PushStatusText(const wxString& text,cbPlugin *plugin){};
-    void PopStatusText(cbPlugin *plugin){};
-
-public:
     // functions used for the mainframe:
     virtual void SetStatusWidths(int n, const int* widths);
-private:
-    std::vector<int> m_mainswidths;
 
-public:
     /** Add a new field, which contains a control, to the status bar.
         @param   plugin The plugin which wants to add a new statusfield
         @param   ctrl   Pointer to the control to show on the new field.
         @param   width  Width of the new field. See wxStatusBar::SetStatusWidths for the meaning of this parameter.
       **/
-    void AddField(cbPlugin *plugin, wxWindow *ctrl, int width );
+    void AddField(cbPlugin* plugin, wxWindow* ctrl, int width);
 
     /** Add a new field to the status bar. The field contains no control.
         @see cbStatusBar::AddField(cbPlugin *plugin, wxWindow *ctrl, int width) for details.
       **/
-    void AddField(cbPlugin *plugin, int width);
+    void AddField(cbPlugin* plugin, int width);
 
     /** Remove a field from the status bar.
         If a control is associated wit this filed, RemoveField will destroy it.
         @param id   The plugin which wants to remove its field
       **/
-    void RemoveField(cbPlugin *plugin);
+    void RemoveField(cbPlugin* plugin);
 
-
-private: // event handlers
+private:
+    // event handlers
     void OnSize(wxSizeEvent& event);
     void UpdateWidths();
 
-private:
+    void AdjustFieldsSize();
+    int  GetFieldNumberOfPlugin(cbPlugin* plugin) const;
 
-    class cbStatusBarElement{
-        public:
-            int width;
-            wxWindow *control;
-            cbPlugin *plugin;
+    void PushStatusText(const wxString& text,cbPlugin *plugin) { ; };
+    void PopStatusText(cbPlugin* plugin)                       { ; };
+
+    struct cbStatusBarElement
+    {
+        int       width;
+        wxWindow *control;
+        cbPlugin *plugin;
     };
     typedef std::vector<cbStatusBarElement> ElementVector;
-    ElementVector m_elements;
-
-private:
-    int GetFieldNumberOfPlugin(cbPlugin *plugin)const;
+    ElementVector                           m_Elements;
+    std::vector<int>                        m_MainWidths;
 
     DECLARE_EVENT_TABLE()
 };
--- src/sdk/cbstatusbar.cpp
+++ src/sdk/cbstatusbar.cpp
@@ -23,26 +23,84 @@
 
 BEGIN_EVENT_TABLE(cbStatusBar, wxStatusBar)
     EVT_SIZE(cbStatusBar::OnSize)
-//    EVT_TIMER(wxID_ANY, MyStatusBar::OnTimer)
 END_EVENT_TABLE()
 
 
-cbStatusBar::cbStatusBar(wxWindow *parent,  wxWindowID id, long style, const wxString& name)
+cbStatusBar::cbStatusBar(wxWindow* parent,  wxWindowID id, long style, const wxString& name)
     : wxStatusBar(parent, wxID_ANY, style, name)
 {
-    //single = this;
 }
-//cbStatusBar* cbStatusBar::single = NULL;
-//cbStatusBar* cbStatusBar::GetInstance()
-//{
-//    return single;
-//}
 
 cbStatusBar::~cbStatusBar()
 {
-    //single = NULL;
 }
 
+void cbStatusBar::SetStatusText(const wxString& text, cbPlugin* plugin)
+{
+    int pos = GetFieldNumberOfPlugin(plugin);
+    if (pos != -1)
+        wxStatusBar::SetStatusText(text, pos);
+}
+
+wxString cbStatusBar::GetStatusText(cbPlugin* plugin) const
+{
+    int pos = GetFieldNumberOfPlugin(plugin);
+    if (pos != -1)
+        return wxStatusBar::GetStatusText(pos);
+    return wxEmptyString;
+}
+
+bool cbStatusBar::GetFieldRect(cbPlugin* plugin, wxRect& rect) const
+{
+    int pos = GetFieldNumberOfPlugin(plugin);
+    if (pos != -1)
+        return wxStatusBar::GetFieldRect(pos, rect);
+    return false;
+}
+
+void cbStatusBar::SetStatusWidths(int n, const int* widths)
+{
+    if (widths)
+    {
+        m_MainWidths.clear();
+        for (int i = 0 ; i < n ; i++)
+            m_MainWidths.push_back(widths[i]);
+        UpdateWidths();
+    }
+}
+
+void cbStatusBar::AddField(cbPlugin* plugin, wxWindow* ctrl, int width)
+{
+    cbStatusBarElement elem;
+    elem.control = ctrl;
+    elem.width = width;
+    elem.plugin =  plugin;
+
+    m_Elements.push_back(elem);
+
+    UpdateWidths();
+
+    return;
+}
+void cbStatusBar::AddField(cbPlugin* plugin, int width)
+{
+    return AddField(plugin, (wxControl*)NULL, width);
+}
+
+void cbStatusBar::RemoveField(cbPlugin* plugin)
+{
+    for (ElementVector::iterator it = m_Elements.begin() ; it != m_Elements.end() ; it++)
+    {
+        if (it->plugin == plugin)
+        {
+            if (it->control)
+                it->control->Destroy();
+            m_Elements.erase(it);
+            break;
+        }
+    }
+    UpdateWidths();
+}
 
 void cbStatusBar::OnSize(wxSizeEvent& event)
 {
@@ -50,106 +108,43 @@ void cbStatusBar::OnSize(wxSizeEvent& event)
     event.Skip();
 }
 
-void cbStatusBar::AdjustFieldsSize()
-{
-    for ( unsigned int i = 0 ; i < m_elements.size(); i++)
-    {
-        wxWindow *ctrl = m_elements[i].control;
-        if ( ctrl )
-        {
-            wxRect rect;
-            GetFieldRect(m_elements[i].plugin, rect);
-            ctrl->SetSize(rect.x + 2, rect.y + 2, rect.width - 4, rect.height - 4);
-        }
-    }
-
-}
-
-
-void cbStatusBar::AddField(cbPlugin *plugin, wxWindow *ctrl, int width )
-{
-    cbStatusBarElement elem;
-    elem.control = ctrl;
-    elem.width = width;
-    elem.plugin =  plugin;
-
-    m_elements.push_back(elem);
-
-    UpdateWidths();
-
-    return;
-}
-void cbStatusBar::AddField(cbPlugin *plugin, int width)
-{
-    return AddField(plugin, (wxControl*)NULL, width);
-}
-void cbStatusBar::RemoveField(cbPlugin *plugin)
-{
-    for ( ElementVector::iterator it = m_elements.begin() ; it != m_elements.end() ; it++ )
-    {
-        if ( it->plugin == plugin )
-        {
-            if ( it->control )
-                it->control->Destroy();
-            m_elements.erase(it);
-            break;
-        }
-    }
-    UpdateWidths();
-}
 void cbStatusBar::UpdateWidths()
 {
-    unsigned int n = m_mainswidths.size() + m_elements.size();
+    unsigned int n = m_MainWidths.size() + m_Elements.size();
 
     int *widths = new int[n];
     unsigned int i = 0;
-    for ( ; i < m_mainswidths.size() ; i++)
-        widths[i] = m_mainswidths[i];
-    for (int k= 0 ; i < n ; i++, k++ )
-        widths[i] = m_elements[k].width;
+    for (; i < m_MainWidths.size() ; i++)
+        widths[i] = m_MainWidths[i];
+    for (int k= 0 ; i < n ; i++, k++)
+        widths[i] = m_Elements[k].width;
 
     wxStatusBar::SetFieldsCount(n);
     wxStatusBar::SetStatusWidths(n, widths);
     delete[] widths;
 }
-void cbStatusBar::SetStatusWidths(int n, const int* widths)
+
+void cbStatusBar::AdjustFieldsSize()
 {
-    if (widths)
+    for (unsigned int i = 0 ; i < m_Elements.size(); i++)
     {
-        m_mainswidths.clear();
-        for (int i = 0 ; i < n ; i++)
-            m_mainswidths.push_back(widths[i]);
-        UpdateWidths();
+        wxWindow *ctrl = m_Elements[i].control;
+        if (ctrl)
+        {
+            wxRect rect;
+            GetFieldRect(m_Elements[i].plugin, rect);
+            ctrl->SetSize(rect.x + 2, rect.y + 2, rect.width - 4, rect.height - 4);
+        }
     }
 }
-int cbStatusBar::GetFieldNumberOfPlugin(cbPlugin *plugin) const
+
+int cbStatusBar::GetFieldNumberOfPlugin(cbPlugin* plugin) const
 {
-    for ( unsigned int i = 0 ; i < m_elements.size() ; i++ )
+    for (unsigned int i = 0 ; i < m_Elements.size() ; i++)
     {
-        if ( m_elements[i].plugin == plugin )
-            return i+m_mainswidths.size();
+        if (m_Elements[i].plugin == plugin)
+            return i+m_MainWidths.size();
     }
     // return -1 to indicate that the id does not exist
     return -1;
-}
-
-void cbStatusBar::SetStatusText(const wxString& text, cbPlugin *plugin)
-{
-    int pos = GetFieldNumberOfPlugin(plugin);
-    if ( pos != -1 )
-        wxStatusBar::SetStatusText(text, pos);
-}
-wxString cbStatusBar::GetStatusText(cbPlugin *plugin) const
-{
-    int pos = GetFieldNumberOfPlugin(plugin);
-    if ( pos != -1 )
-        return wxStatusBar::GetStatusText(pos);
-    return wxEmptyString;
-}
-bool cbStatusBar::GetFieldRect(cbPlugin *plugin, wxRect& rect) const
-{
-    int pos = GetFieldNumberOfPlugin(plugin);
-    if ( pos != -1 )
-        return wxStatusBar::GetFieldRect(pos, rect);
-    return false;
 }
