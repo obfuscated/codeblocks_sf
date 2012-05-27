4fabfb5e5f7a76ec2a25e9007e33e9b24d128d03
Jens Lody <jens@codeblocks.org>
2012-05-27 16:29:07 +0000

    - linux: fix update_revision.sh to work with git-svn also; silent warning on non-debian systems
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8000 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- update_revision.sh
+++ update_revision.sh
@@ -9,6 +9,10 @@
 # $HeadURL$
 #
 
+
+# make sure [git-]svn answers in english
+export LC_ALL="C"
+
 REV_FILE=./revision.m4
 
 # let's import OLD_REV (if there)
@@ -21,9 +25,12 @@ fi
 if svn --xml info >/dev/null 2>&1; then
 	REV=`svn --xml info | tr -d '\r\n' | sed -e 's/.*<commit.*revision="\([0-9]*\)".*<\/commit>.*/\1/'`
 	LCD=`svn --xml info | tr -d '\r\n' | sed -e 's/.*<commit.*<date>\([0-9\-]*\)\T\([0-9\:]*\)\..*<\/date>.*<\/commit>.*/\1 \2/'`
-elif svn --version --quiet >/dev/null 2>&1; then
+elif svn --info >/dev/null 2>&1; then
 	REV=`svn info | grep "^Revision:" | cut -d" " -f2`
 	LCD=`svn info | grep "^Last Changed Date:" | cut -d" " -f4,5`
+elif git svn --version >/dev/null 2>&1; then
+	REV=`git svn info | grep "^Revision:" | cut -d" " -f2`
+	LCD=`git svn info | grep "^Last Changed Date:" | cut -d" " -f4,5`
 else
 	REV=0
 	LCD=""
@@ -35,7 +42,7 @@ if [ "x$REV" != "x$OLD_REV" -o ! -r $REV_FILE ]; then
 	echo "m4_define([SVN_DATE], $LCD)" >> $REV_FILE
 
 	# Also change the revision number in debian/changelog for package versioning
-	DCH=`which dch`
+	DCH=`which dch 2> /dev/null`
 	if [ "x$DCH" != "x" ]; then
 		if [ -x "$DCH" ]; then
 			AKT_REV=`sed -e 's/.*svn\([0-9]*\).*/\1/' -e 'q' < debian/changelog`
