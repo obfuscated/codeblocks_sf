e737cabed3c20d1825bb2c4cae3c889b72401c27
Martin Halle <mortenmacfly@codeblocks.org>
2012-07-06 13:59:22 +0000

    * improve and clarify auto-detect compilers dialog a lot
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8086 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/autodetectcompilers.cpp
+++ src/sdk/autodetectcompilers.cpp
@@ -11,14 +11,18 @@
 #include "autodetectcompilers.h"
 
 #ifndef CB_PRECOMP
-    #include "compiler.h"
-    #include "compilerfactory.h"
     #include <wx/button.h>
+    #include <wx/filename.h>
     #include <wx/intl.h>
     #include <wx/listctrl.h>
     #include <wx/stattext.h>
     #include <wx/string.h>
     #include <wx/xrc/xmlres.h>
+
+    #include "compiler.h"
+    #include "compilerfactory.h"
+    #include "manager.h"
+    #include "macrosmanager.h"
 #endif
 
 BEGIN_EVENT_TABLE(AutoDetectCompilers, wxScrollingDialog)
@@ -35,8 +39,8 @@ AutoDetectCompilers::AutoDetectCompilers(wxWindow* parent)
     if (list)
     {
         list->ClearAll();
-        list->InsertColumn(0, _("Compiler"), wxLIST_FORMAT_LEFT, 240);
-        list->InsertColumn(1, _("Status"), wxLIST_FORMAT_LEFT, 76);
+        list->InsertColumn(0, _("Compiler"), wxLIST_FORMAT_LEFT, 380);
+        list->InsertColumn(1, _("Status"),   wxLIST_FORMAT_LEFT, 100);
 
         for (size_t i = 0; i < CompilerFactory::GetCompilersCount(); ++i)
         {
@@ -46,22 +50,64 @@ AutoDetectCompilers::AutoDetectCompilers(wxWindow* parent)
 
             list->InsertItem(list->GetItemCount(), compiler->GetName());
 
+            wxString path = compiler->GetMasterPath();
+            wxString path_no_macros = compiler->GetMasterPath();
+            Manager::Get()->GetMacrosManager()->ReplaceMacros(path_no_macros);
+
             int idx = list->GetItemCount() - 1;
-            if (!compiler->GetParentID().IsEmpty()) // not built-in
-                list->SetItem(idx, 1, _("User-defined"));
-            else
+            if (path.IsEmpty())
             {
-                wxString path = compiler->GetMasterPath();
-                bool detected = compiler->AutoDetectInstallationDir() == adrDetected;
-                if (detected && (path.IsEmpty() || path == compiler->GetMasterPath()))
-                    list->SetItem(idx, 1, _("Detected"));
-                else if (!path.IsEmpty())
+                // Here, some user-interaction is required not to show this
+                // dialog again on each new start-up of C::B.
+                list->SetItem(idx, 1, _("Invalid (not set!)"));
+                // So we better clearly HIGHLIGHT this entry:
+                wxListItem li; li.SetId(idx);
+                if ( list->GetItem(li) ) // Why shouldn't this work?!
                 {
-                    list->SetItem(idx, 1, _("User-defined"));
+                    li.SetBackgroundColour(*wxRED);
+                    list->SetItem(li);
+                }
+            }
+            else // The compiler is *probably*Ü invalid, but at least a master-path is set
+                list->SetItem(idx, 1, _("Invalid"));
+
+            // Inspect deeper and probably try to auto-detect invalid compilers:
+            if (compiler->GetParentID().IsEmpty()) // built-in compiler
+            {
+                // Try auto-detection (which is for built-in compilers only)
+                bool detected = compiler->AutoDetectInstallationDir() == adrDetected;
+                wxString pathDetected( compiler->GetMasterPath() );
+
+                // In case auto-detection was successful:
+                if (detected)
+                {
+                    // No path setup before OR path detected as it was setup before
+                    if (path.IsEmpty() || path == pathDetected || path_no_macros == pathDetected)
+                        list->SetItem(idx, 1, _("Detected")); // OK
+                    else
+                        list->SetItem(idx, 1, _("User-defined")); // OK
+                }
+                // In case auto-detection failed but a path was setup before:
+                else if ( !path.IsEmpty() )
+                {
+                    // Check, if the master path is valid:
+                    if ( wxFileName::DirExists(path_no_macros) )
+                        list->SetItem(idx, 1, _("User-defined")); // OK
+
+                    // Assume the user did the setup on purpose, so reset the old settings anyways:
                     compiler->SetMasterPath(path);
                 }
             }
+            else // no built-in, but user-defined (i.e. copied) compiler
+            {
+                // Check, if the master path is valid:
+                if ( !path.IsEmpty() && wxFileName::DirExists(path_no_macros) )
+                    list->SetItem(idx, 1, _("User-defined")); // OK
+            }
         }
+        // Resize columns so one can read the whole stuff:
+        list->SetColumnWidth(0, wxLIST_AUTOSIZE);
+        list->SetColumnWidth(1, wxLIST_AUTOSIZE);
     }
 
     XRCCTRL(*this, "lblDefCompiler", wxStaticText)->SetLabel(CompilerFactory::GetDefaultCompiler()->GetName());
