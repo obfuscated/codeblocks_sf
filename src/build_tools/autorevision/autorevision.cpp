4c9052f5a1f7e03e89aab8d6bda6f6f41e616b5b
Jens Lody <jens@codeblocks.org>
2012-09-04 12:00:33 +0000

    * make autorevision work with git-svn and older version of svn that are not able to create xml-output
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8351 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/build_tools/autorevision/autorevision.cpp
+++ src/build_tools/autorevision/autorevision.cpp
@@ -26,6 +26,14 @@ using namespace std;
     inline void set_env(const char* k, const char* v) { setenv(k, v, 1); };
 #endif
 
+#if !defined WIFEXITED
+    #define WIFEXITED(x) 1
+#endif
+
+#if !defined WEXITSTATUS
+    #define WEXITSTATUS(x) x
+#endif
+
 bool QuerySvn(const string& workingDir, string& revision, string &date);
 bool ParseFile(const string& docFile, string& revision, string &date);
 bool WriteOutput(const string& outputFile, string& revision, string& date);
@@ -96,46 +104,123 @@ bool QuerySvn(const string& workingDir, string& revision, string &date)
 {
     revision = "0";
     date = "unknown date";
+
     string svncmd("svn info --xml --non-interactive ");
     svncmd.append(workingDir);
 
     FILE *svn = popen(svncmd.c_str(), "r");
 
+    // first try svn info with xml-output
     if(svn)
     {
         char buf[16384] = {'0'};
         fread(buf, 16383, 1, svn);
-        pclose(svn);
-
-        TiXmlDocument doc;
-        doc.Parse(buf);
-
-        if(doc.Error())
-            return 0;
-
-        TiXmlHandle hCommit(&doc);
-        hCommit = hCommit.FirstChildElement("info").FirstChildElement("entry").FirstChildElement("commit");
-        if(const TiXmlElement* e = hCommit.ToElement())
+        int ret = pclose(svn);
+        if(WIFEXITED(ret) && (WEXITSTATUS(ret) == 0))
         {
-            revision = e->Attribute("revision") ? e->Attribute("revision") : "";
-            const TiXmlElement* d = e->FirstChildElement("date");
-            if(d && d->GetText())
+            TiXmlDocument doc;
+            doc.Parse(buf);
+
+            if(doc.Error())
+                return 0;
+
+            TiXmlHandle hCommit(&doc);
+            hCommit = hCommit.FirstChildElement("info").FirstChildElement("entry").FirstChildElement("commit");
+            if(const TiXmlElement* e = hCommit.ToElement())
             {
-                date = d->GetText();
-                string::size_type pos = date.find('T');
-                if (pos != string::npos)
+                revision = e->Attribute("revision") ? e->Attribute("revision") : "";
+                const TiXmlElement* d = e->FirstChildElement("date");
+                if(d && d->GetText())
                 {
-                    date[pos] = ' ';
-                }
-                pos = date.rfind('.');
-                if (pos != string::npos)
-                {
-                    date = date.substr(0, pos);            }
-                }
-            return 1;
+                    date = d->GetText();
+                    string::size_type pos = date.find('T');
+                    if (pos != string::npos)
+                    {
+                        date[pos] = ' ';
+                    }
+                    pos = date.rfind('.');
+                    if (pos != string::npos)
+                    {
+                        date = date.substr(0, pos);            }
+                    }
+                return 1;
+            }
+            return 0;
         }
     }
-    return 0;
+
+    // ensure we have an english environment, needed
+    // to correctly parse output of localized (git) svn info
+#ifndef __MINGW32__
+    setenv("LC_ALL", "C", 1);
+#else
+    setlocale(LC_ALL, "C");
+#endif
+
+    svncmd = "git svn info ";
+    svncmd.append(workingDir);
+    svn = popen(svncmd.c_str(), "r");
+    // second try git svn info
+    if(svn)
+    {
+        char buf[16384] = {'0'};
+        fread(buf, 16383, 1, svn);
+        int ret = pclose(svn);
+        if (!WIFEXITED(ret) || (WEXITSTATUS(ret) != 0))
+        {
+            svncmd = "svn info --non-interactive ";
+            svncmd.append(workingDir);
+            svn = popen(svncmd.c_str(), "r");
+
+            // third try oldstyle (outated) svn info (should not be needed anymore)
+            if(svn)
+            {
+                memset(buf, 0, 16384);
+                fread(buf, 16383, 1, svn);
+                int ret = pclose(svn);
+                if (!WIFEXITED(ret) || (WEXITSTATUS(ret) != 0))
+                    return 1;
+            }
+            else
+                return 1;
+        }
+        string what("Last Changed Rev: ");
+        string output(buf);
+        string::size_type pos = string::npos;
+        string::size_type len = 0;
+        pos = output.find(what);
+        if (pos != string::npos)
+        {
+            pos += what.length();
+            len = 0;
+            // revision must be numeric
+            while (buf[ pos + len ] >= '0' && buf[ pos + len++ ] <= '9')
+                ;
+        }
+        if (len != 0)
+            revision = output.substr(pos, len);
+
+        what = "Last Changed Date: ";
+        pos = output.find(what);
+        if (pos != string::npos)
+        {
+            pos += what.length();
+            len = output.find(" ", pos);
+            // we want the position of the second space
+            if (len != string::npos)
+                len = output.find(" ", len + 1);
+            if (len != string::npos)
+                len -= pos;
+            else
+                len = 0;
+        }
+        if (len != 0)
+            date = output.substr(pos, len);
+
+        return 0;
+    }
+    // if we are here, we could not read the info
+    return 1;
 }
 
 
