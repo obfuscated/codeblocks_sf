ce18344f8dc24662f96a91c25d6fce8ccc324f59
Martin Halle <mortenmacfly@codeblocks.org>
2012-12-16 14:10:32 +0000

    * compilation fix (max_logs -> LogManager::max_logs)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8679 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/loghacker/loghacker.cpp
+++ src/plugins/loghacker/loghacker.cpp
@@ -50,25 +50,25 @@ void LogHacker::OnRelease(bool /*appShutDown*/)
 
 class CfgPanel: public cbConfigurationPanel
 {
-	ConfigManagerContainer::StringToStringMap c2l;
-	ConfigManagerContainer::StringToStringMap c2f;
+    ConfigManagerContainer::StringToStringMap c2l;
+    ConfigManagerContainer::StringToStringMap c2f;
 
-    wxListBox*	channel;
-    wxListBox*	logger;
-    wxTextCtrl*	filename;
+    wxListBox*    channel;
+    wxListBox*    logger;
+    wxTextCtrl*    filename;
 
-	//DECLARE_EVENT_TABLE
+    //DECLARE_EVENT_TABLE
 public:
 
     void Create(wxWindow* parent);
 
-	virtual wxString GetTitle() const { return _T("Log Hacker"); };
-	virtual wxString GetBitmapBaseName() const { return _T(""); };
+    virtual wxString GetTitle() const { return _T("Log Hacker"); };
+    virtual wxString GetBitmapBaseName() const { return _T(""); };
 
-	virtual void OnApply();
-	virtual void OnCancel(){};
+    virtual void OnApply();
+    virtual void OnCancel(){};
 
-	virtual void Change();
+    virtual void Change();
 };
 
 
@@ -79,31 +79,30 @@ public:
 
 void CfgPanel::Create(wxWindow* parent)
 {
-	wxPanel::Create(parent,-1);
+    wxPanel::Create(parent,-1);
 
     wxArrayString channelStrings;
 
-	int slot = 0;
-	while(slot < max_logs)
-	{
-		const wxString t = LogManager::Get()->Slot(slot).title;
+    int slot = 0;
+    while (slot < LogManager::max_logs)
+    {
+        const wxString t = LogManager::Get()->Slot(slot).title;
 
-		if(!!t)
-			channelStrings.Add(t);
+        if (!!t)
+            channelStrings.Add(t);
 
-		++slot;
-	}
-
+        ++slot;
+    }
 
     wxArrayString loggerStrings = LogManager::Get()->ListAvailable();
-	loggerStrings.Insert(_T("<application default>"), 0);
+    loggerStrings.Insert(_T("<application default>"), 0);
 
     wxFlexGridSizer* flex = new wxFlexGridSizer(2, 2, 0, 0);
     flex->AddGrowableRow(1);
     flex->AddGrowableCol(0);
     flex->AddGrowableCol(1);
 
-	wxStaticText* txt;
+    wxStaticText* txt;
     txt = new wxStaticText(this, wxID_STATIC, _("Log Source"));
     flex->Add(txt, 0, wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT|wxTOP, 5);
     txt = new wxStaticText(this, wxID_STATIC, _("Associated Logger"));
@@ -114,10 +113,10 @@ void CfgPanel::Create(wxWindow* parent)
     flex->Add(channel, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
     flex->Add(logger,  0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
-	channel->SetSelection(0);
-	logger->SetSelection(0);
+    channel->SetSelection(0);
+    logger->SetSelection(0);
 
-	// spacer
+    // spacer
     flex->Add(5, 5, 0, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL|wxALL, 5);
 
     filename = new wxTextCtrl(this, id_text);
@@ -130,21 +129,21 @@ void CfgPanel::Create(wxWindow* parent)
 
 cbConfigurationPanel* LogHacker::GetConfigurationPanel(wxWindow* parent)
 {
-	CfgPanel *p = new CfgPanel();
-	p->Create(parent);
-	return p;
+    CfgPanel *p = new CfgPanel();
+    p->Create(parent);
+    return p;
 }
 
 
 
 void CfgPanel::Change()
 {
-	wxString c = channel->GetStringSelection();
-	wxString l = logger->GetStringSelection();
-	filename->Enable(LogManager::Get()->FilenameRequired(l));
+    wxString c = channel->GetStringSelection();
+    wxString l = logger->GetStringSelection();
+    filename->Enable(LogManager::Get()->FilenameRequired(l));
 
-	if(l.at(0) == _T('<'))  // "<application default>"
-		l.Empty();
+    if (l.at(0) == _T('<'))  // "<application default>"
+        l.Empty();
 
 //  blah... do something, update the maps according to what is selected
 }
@@ -152,21 +151,21 @@ void CfgPanel::Change()
 
 void CfgPanel::OnApply()
 {
-	LogManager *m = LogManager::Get();
+    LogManager *m = LogManager::Get();
 
-	for(ConfigManagerContainer::StringToStringMap::iterator i = c2l.begin(); i != c2l.end(); ++i)
-		{
-			if(!!i->second.IsEmpty())
-			{
-				int slot = 0;
-				while(slot <= max_logs && m->Slot(slot).title != i->second)
-					++slot;
+    for (ConfigManagerContainer::StringToStringMap::iterator i = c2l.begin(); i != c2l.end(); ++i)
+    {
+        if (!!i->second.IsEmpty())
+        {
+            int slot = 0;
+            while (slot <= LogManager::max_logs && m->Slot(slot).title != i->second)
+                ++slot;
 
-				m->SetLog(m->New(i->second), slot);
-			}
-		}
+            m->SetLog(m->New(i->second), slot);
+        }
+    }
 
-	// since we use ConfigManagerContainer::StringToStringMap, we could store whatever settings we have like:
-	//   ConfigManager::Get()->Write(_T("/loghacker/c2l"), c2l);
-	//   ConfigManager::Get()->Write(_T("/loghacker/c2f"), c2f);
+    // since we use ConfigManagerContainer::StringToStringMap, we could store whatever settings we have like:
+    //   ConfigManager::Get()->Write(_T("/loghacker/c2l"), c2l);
+    //   ConfigManager::Get()->Write(_T("/loghacker/c2f"), c2f);
 }
