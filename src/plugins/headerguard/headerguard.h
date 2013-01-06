002784493fe352577f9126f4e5221c6192ba3adc
Martin Halle <mortenmacfly@codeblocks.org>
2013-01-06 07:39:11 +0000

    - headerguard: removed some warnings
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8767 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/headerguard/headerguard.h
+++ src/plugins/headerguard/headerguard.h
@@ -12,6 +12,7 @@ class wxMenu;
 class FileTreeData;
 
 #include "cbplugin.h"
+#include "prep.h"
 
 class HeaderGuard : public cbPlugin
 {
@@ -20,8 +21,8 @@ class HeaderGuard : public cbPlugin
         virtual ~HeaderGuard(){};
 
         virtual void BuildMenu(wxMenuBar* menuBar){}
-        virtual void BuildModuleMenu(const ModuleType type, wxMenu* menu, const FileTreeData* data = 0){}
-        virtual bool BuildToolBar(wxToolBar* toolBar){ return false; }
+        virtual void BuildModuleMenu(cb_unused const ModuleType type, cb_unused wxMenu* menu, cb_unused const FileTreeData* data = 0){}
+        virtual bool BuildToolBar(cb_unused wxToolBar* toolBar){ return false; }
 
     private:
 
@@ -29,7 +30,7 @@ class HeaderGuard : public cbPlugin
 
         void OnSave(CodeBlocksEvent& event);
 
-        virtual void OnRelease(bool appShutDown){};
+        virtual void OnRelease(cb_unused bool appShutDown){};
 };
 
 #endif // header guard
