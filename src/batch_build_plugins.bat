1cec121b576937918f1b58f8f8546da52cf84882
Biplab Kumar Modak <biplab@codeblocks.org>
2013-03-20 13:59:51 +0000

    * Applied: [Patch #3303] Enhance the provided batch build scripts. Patch for wx29 will follow.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8914 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/batch_build_plugins.bat
+++ src/batch_build_plugins.bat
@@ -2,18 +2,19 @@
 rem ----------------------------------------
 rem Setup C::B root folder of *binaries* (!)
 rem ----------------------------------------
-set CB_ROOT=C:\Devel\CodeBlocks
+if not defined CB_ROOT set CB_ROOT=C:\Devel\CodeBlocks
 rem ------------------------------------------
 rem Setup GCC root folder with "bin" subfolder
 rem ------------------------------------------
-set GCC_ROOT=%CB_ROOT%\MinGW
+if not defined GCC_ROOT set GCC_ROOT=%CB_ROOT%\MinGW
 
 rem -------------------------------------------
 rem Usually below here no changes are required.
 rem -------------------------------------------
 if not exist "%CB_ROOT%"  goto ErrNoCB
 if not exist "%GCC_ROOT%" goto ErrNoGCC
-set PATH=%CB_ROOT%;%GCC_ROOT%;%PATH%
+rem quotes are here to prevent problems with spaces in paths
+set "PATH=%CB_ROOT%;%GCC_ROOT%;%PATH%"
 
 set BUILD_TYPE=--build
 if "%1"=="r"        set BUILD_TYPE=--rebuild
@@ -21,22 +22,23 @@ if "%1"=="-r"       set BUILD_TYPE=--rebuild
 if "%1"=="rebuild"  set BUILD_TYPE=--rebuild
 if "%1"=="-rebuild" set BUILD_TYPE=--rebuild
 
-set START_CMD=start "Code::Blocks Build" /D"%~dp0" /min /b
+if not defined START_CMD set START_CMD=start "Code::Blocks Build" /D"%~dp0" /min /b
 set CB_EXE="%CB_ROOT%\codeblocks.exe"
-set CB_PARAMS=--batch-build-notify --no-batch-window-close
+if not defined CB_PARAMS set CB_PARAMS=--batch-build-notify --no-batch-window-close
 set CB_CMD=%BUILD_TYPE% "%~dp0ContribPlugins.workspace"
 
-set CB_TARGET=--target=All
+if not defined CB_TARGET set CB_TARGET=--target=All
 %START_CMD% %CB_EXE% %CB_PARAMS% %CB_TARGET% %CB_CMD%
-echo Do not forget to run "update.bat" after successful build!
+echo.
+if %CB_RUN_UPDATE_BAT% NEQ 0 call "%~dp0update.bat" else echo Do not forget to run "update.bat" after successful build!
 goto TheEnd
 
 :ErrNoCB
-echo Error: C::B root folder not found. Adjust batch file accordingly
+echo Error: C::B root folder not found. Adjust batch file or supply parameter accordingly
 goto TheEnd
 
 :ErrNoGCC
-echo Error: GCC root folder not found. Adjust batch file accordingly
+echo Error: GCC root folder not found. Adjust batch file or supply parameter accordingly
 goto TheEnd
 
 :TheEnd
