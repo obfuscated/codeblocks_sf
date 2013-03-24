94be88a035901b21e9b22f1290b992beb3a98143
Biplab Kumar Modak <biplab@codeblocks.org>
2013-03-24 04:46:09 +0000

    * Updated: updateChangeLog.sh script to point to sf.net repo.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@8917 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- updateChangeLog.sh
+++ updateChangeLog.sh
@@ -6,13 +6,13 @@
 #
 # $Revision$
 # $Id$
-# $HeadURL:$
+# $HeadURL$
 #
 
 REV_TO=${1:-"HEAD"}
 REV_LAST=`cat ChangeLog | head -3 - | tr -d '\r\n' | sed -e 's/.*svn\([0-9]*\).*/\1/'`
 
-REV_MAX=`svn --xml info 'http://svn.berlios.de/svnroot/repos/codeblocks/trunk' | tr -d '\r\n' | sed -e 's/.*<commit.*revision="\([0-9]*\)".*<\/commit>.*/\1/'`
+REV_MAX=`svn --xml info 'http://svn.code.sf.net/p/codeblocks/code/trunk' | tr -d '\r\n' | sed -e 's/.*<commit.*revision="\([0-9]*\)".*<\/commit>.*/\1/'`
 
 REV_FROM=${2:-$(($REV_LAST + 1))}
 
@@ -22,7 +22,7 @@ fi
 
 echo "Downloading svn-log from revision $REV_FROM to revision $REV_MAX" 
 
-svn --verbose --xml log 'http://svn.berlios.de/svnroot/repos/codeblocks/trunk' -r "$REV_TO:$REV_FROM" | xsltproc --stringparam strip-prefix "trunk"  --stringparam linelen "75" --stringparam groupbyday "no" --stringparam separate-daylogs "no"  --stringparam include-rev "yes" --stringparam breakbeforemsg "no" --stringparam reparagraph "no" --stringparam authorsfile "" --stringparam title "ChangeLog" --stringparam revision-link "#r" --stringparam ignore-message-starting "" --nowrite --nomkdir --nonet "ChangeLog.xsl" - > "ChangeLog.new"
+svn --verbose --xml log 'http://svn.code.sf.net/p/codeblocks/code/trunk' -r "$REV_TO:$REV_FROM" | xsltproc --stringparam strip-prefix "trunk"  --stringparam linelen "75" --stringparam groupbyday "no" --stringparam separate-daylogs "no"  --stringparam include-rev "yes" --stringparam breakbeforemsg "no" --stringparam reparagraph "no" --stringparam authorsfile "" --stringparam title "ChangeLog" --stringparam revision-link "#r" --stringparam ignore-message-starting "" --nowrite --nomkdir --nonet "ChangeLog.xsl" - > "ChangeLog.new"
 
 cat "ChangeLog" >> "ChangeLog.new"
 
