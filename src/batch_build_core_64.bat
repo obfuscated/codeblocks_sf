aba2139a815ad64afe8ba3e8b7a65ca67ba8c62a
Martin Halle <mortenmacfly@codeblocks.org>
2013-05-04 18:26:35 +0000

    * updated project files concerning wrong/missing files * added 64 bit project file and batch build for 64 bit to use, do *BEFORE* (!!!) you open the 64 bit project file: - make a copy of the GCC compiler - name it exactly "GNU GCC 64 compiler" - let it point to e.g. the base folder of the TDM GCC 64 bit release
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@9054 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/batch_build_core_64.bat
@@ -0,0 +1,44 @@
+@echo off
+rem ----------------------------------------
+rem Setup C::B root folder of *binaries* (!)
+rem ----------------------------------------
+if not defined CB_ROOT set CB_ROOT=C:\Devel\CodeBlocks
+rem ------------------------------------------
+rem Setup GCC root folder with "bin" subfolder
+rem ------------------------------------------
+if not defined GCC_ROOT set GCC_ROOT=%CB_ROOT%\MinGW64
+rem run update.bat after build
+rem if not defined CB_RUN_UPDATE_BAT set CB_RUN_UPDATE_BAT=0
+
+rem -------------------------------------------
+rem Usually below here no changes are required.
+rem -------------------------------------------
+if not exist "%CB_ROOT%"  goto ErrNoCB
+if not exist "%GCC_ROOT%" goto ErrNoGCC
+set PATH=%CB_ROOT%;%GCC_ROOT%;%PATH%
+
+set BUILD_TYPE=--build
+if "%1"=="r"        set BUILD_TYPE=--rebuild
+if "%1"=="-r"       set BUILD_TYPE=--rebuild
+if "%1"=="rebuild"  set BUILD_TYPE=--rebuild
+if "%1"=="-rebuild" set BUILD_TYPE=--rebuild
+
+if not defined START_CMD set START_CMD=start "Code::Blocks Build" /D"%~dp0" /min /b
+set CB_EXE="%CB_ROOT%\codeblocks.exe"
+if not defined CB_PARAMS set CB_PARAMS=--batch-build-notify --no-batch-window-close
+set CB_CMD=%BUILD_TYPE% "%~dp0CodeBlocks_wx29.cbp"
+
+set CB_TARGET=--target=All
+%START_CMD% %CB_EXE% %CB_PARAMS% %CB_TARGET% %CB_CMD%
+echo Do not forget to run "update29.bat" after successful build!
+goto TheEnd
+
+:ErrNoCB
+echo Error: C::B root folder not found. Adjust batch file or supply parameter accordingly
+goto TheEnd
+
+:ErrNoGCC
+echo Error: GCC root folder not found. Adjust batch file or supply parameter accordingly
+goto TheEnd
+
+:TheEnd
