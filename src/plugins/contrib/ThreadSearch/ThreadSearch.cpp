1abcb90b4ba6b59b526fe3604d6450ebf1a2f2a6
T Petrov <tpetrov@codeblocks.org>
2013-06-30 13:58:38 +0000

    - ThreadSearch: Change default setting (hide the preview and disable the delete before search option)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@9165 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/ThreadSearch/ThreadSearch.cpp
+++ src/plugins/contrib/ThreadSearch/ThreadSearch.cpp
@@ -541,8 +541,8 @@ void ThreadSearch::LoadConfig(bool& showPanel, int& sashPosition,
     m_UseDefValsForThreadSearch  = pCfg->ReadBool(wxT("/UseDefaultValues"),      true);
     m_ShowSearchControls         = pCfg->ReadBool(wxT("/ShowSearchControls"),    true);
     m_ShowDirControls            = pCfg->ReadBool(wxT("/ShowDirControls"),       false);
-    m_ShowCodePreview            = pCfg->ReadBool(wxT("/ShowCodePreview"),       true);
-    m_DeletePreviousResults      = pCfg->ReadBool(wxT("/DeletePreviousResults"), true);
+    m_ShowCodePreview            = pCfg->ReadBool(wxT("/ShowCodePreview"),       false);
+    m_DeletePreviousResults      = pCfg->ReadBool(wxT("/DeletePreviousResults"), false);
     m_DisplayLogHeaders          = pCfg->ReadBool(wxT("/DisplayLogHeaders"),     true);
     m_DrawLogLines               = pCfg->ReadBool(wxT("/DrawLogLines"),          false);
 
