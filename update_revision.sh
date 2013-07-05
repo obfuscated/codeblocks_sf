6c632eaff17b1f1d6d8ac10b0a26bc736d001ec2
T Petrov <tpetrov@codeblocks.org>
2013-07-05 21:18:37 +0000

    - improve the update_revision.sh script to work better with git
    * Disable the 'git svn info' path, because it fails badly with git 1.8.x.
    * Rework the 'git log' path to work better - it finds the last svn commit
      in the graph and extracts the revision.
    * Rework the 'git log' path to get the date of the last commit in a format
      similar to the way svn gets it (YYYY-MM-DD hh-mm-ss).
    
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@9175 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- update_revision.sh
+++ update_revision.sh
@@ -23,21 +23,27 @@ else
 fi
 
 if svn --xml info >/dev/null 2>&1; then
+	echo "Using 'svn --xml info' to get the revision"
 	REV=`svn --xml info | tr -d '\r\n' | sed -e 's/.*<commit.*revision="\([0-9]*\)".*<\/commit>.*/\1/'`
 	LCD=`svn --xml info | tr -d '\r\n' | sed -e 's/.*<commit.*<date>\([0-9\-]*\)\T\([0-9\:]*\)\..*<\/date>.*<\/commit>.*/\1 \2/'`
 elif svn --info >/dev/null 2>&1; then
+	echo "Using 'svn info' to get the revision"
 	REV=`svn info | grep "^Revision:" | cut -d" " -f2`
 	LCD=`svn info | grep "^Last Changed Date:" | cut -d" " -f4,5`
-elif git svn --version >/dev/null 2>&1; then
-	REV=`git svn info | grep "^Revision:" | cut -d" " -f2`
-	LCD=`git svn info | grep "^Last Changed Date:" | cut -d" " -f4,5`
+#elif git svn --version >/dev/null 2>&1; then
+#	echo "Using 'git svn info' to get the revision"
+#	REV=`git svn info | grep "^Revision:" | cut -d" " -f2`
+#	LCD=`git svn info | grep "^Last Changed Date:" | cut -d" " -f4,5`
 elif git version >/dev/null 2>&1; then
-	REV=`git log --max-count=1 | grep -o -e "@\([0-9]*\)" | tr -d '@ '`
-	LCD=`git log --max-count=1 | grep -o -e "Date: \(.*\)" | cut -d " " -f 5-9`
+	echo "Using 'git log --graph' to get the revision"
+	REV=`git log --graph | grep 'git-svn-id' | head -n 1 | grep -o -e "@\([0-9]*\)" | tr -d '@ '`
+	LCD=`git log --date=iso --max-count=1 | grep -o -e "Date: \(.*\)" | cut -d ' ' -f 2- | sed 's/^ *//' | cut -f -2 -d ' '`
 else
 	REV=0
 	LCD=""
 fi
+
+echo "Found revision: '${REV}' '${LCD}'"
 
 if [ "x$REV" != "x$OLD_REV" -o ! -r $REV_FILE ]; then
 	echo "m4_define([SVN_REV], $REV)" > $REV_FILE
