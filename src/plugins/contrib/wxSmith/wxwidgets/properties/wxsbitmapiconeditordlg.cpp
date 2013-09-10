76693ed98ecdaccbc8585b3a7c7810da85064f17
Jens Lody <jens@codeblocks.org>
2013-09-10 21:52:52 +0000

    * wxSmith-plugin: fix assert and possible crash in wxsbitmapiconeditordlg, due to incorrect setting of growable col in flexgridsizer
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@9302 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmith/wxwidgets/properties/wxsbitmapiconeditordlg.cpp
+++ src/plugins/contrib/wxSmith/wxwidgets/properties/wxsbitmapiconeditordlg.cpp
@@ -26,8 +26,8 @@
 #include <wx/filedlg.h>
 
 //(*InternalHeaders(wxsBitmapIconEditorDlg)
-#include <wx/intl.h>
 #include <wx/string.h>
+#include <wx/intl.h>
 //*)
 
 #define TIMER_DELAY    250
@@ -127,19 +127,19 @@ wxsBitmapIconEditorDlg::wxsBitmapIconEditorDlg(wxWindow* parent,wxsBitmapIconDat
     Data(_Data)
 {
     //(*Initialize(wxsBitmapIconEditorDlg)
-    wxBoxSizer* BoxSizer4;
-    wxBoxSizer* BoxSizer5;
+    wxBoxSizer* BoxSizer3;
     wxBoxSizer* BoxSizer7;
     wxBoxSizer* BoxSizer2;
+    wxBoxSizer* BoxSizer4;
     wxBoxSizer* BoxSizer1;
-    wxBoxSizer* BoxSizer3;
+    wxBoxSizer* BoxSizer5;
 
     Create(parent, wxID_ANY, _("Image editor"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER, _T("wxID_ANY"));
     BoxSizer1 = new wxBoxSizer(wxVERTICAL);
     BoxSizer5 = new wxBoxSizer(wxHORIZONTAL);
     StaticBoxSizer1 = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Image options"));
     FlexGridSizer1 = new wxFlexGridSizer(0, 1, 0, 0);
-    FlexGridSizer1->AddGrowableCol(1);
+    FlexGridSizer1->AddGrowableCol(0);
     NoImage = new wxRadioButton(this, ID_RADIOBUTTON1, _("No image"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_RADIOBUTTON1"));
     FlexGridSizer1->Add(NoImage, 0, wxEXPAND|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
     ImageFile = new wxRadioButton(this, ID_RADIOBUTTON2, _("Image From File:"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_RADIOBUTTON2"));
--- src/plugins/contrib/wxSmith/wxwidgets/properties/wxsbitmapiconeditordlg.h
+++ src/plugins/contrib/wxSmith/wxwidgets/properties/wxsbitmapiconeditordlg.h
@@ -24,14 +24,14 @@
 #define WXSBITMAPICONEDITORDLG_H
 
 //(*Headers(wxsBitmapIconEditorDlg)
+#include <wx/combobox.h>
 #include <wx/sizer.h>
+#include <wx/button.h>
+#include <wx/radiobut.h>
+#include "scrollingdialog.h"
 #include <wx/stattext.h>
 #include <wx/textctrl.h>
-#include <wx/radiobut.h>
 #include <wx/statbmp.h>
-#include <wx/button.h>
-#include "scrollingdialog.h"
-#include <wx/combobox.h>
 //*)
 #include <wx/timer.h>
 
@@ -75,23 +75,23 @@ class wxsBitmapIconEditorDlg: public wxScrollingDialog
 
         //(*Declarations(wxsBitmapIconEditorDlg)
         wxStaticBoxSizer* StaticBoxSizer2;
-        wxStaticBitmap* Preview;
-        wxRadioButton* ImageArt;
+        wxRadioButton* NoImage;
+        wxFlexGridSizer* FlexGridSizer1;
+        wxFlexGridSizer* FlexGridSizer2;
+        wxComboBox* ArtId;
+        wxButton* Button1;
+        wxButton* Button2;
+        wxButton* Button3;
+        wxTextCtrl* FileName;
+        wxStaticText* StaticText1;
+        wxComboBox* ArtClient;
         wxRadioButton* ImageCode;
         wxStaticText* StaticText2;
         wxTextCtrl* CodeText;
-        wxButton* Button1;
-        wxTextCtrl* FileName;
-        wxFlexGridSizer* FlexGridSizer2;
-        wxStaticText* StaticText1;
-        wxButton* Button2;
+        wxRadioButton* ImageArt;
         wxRadioButton* ImageFile;
-        wxButton* Button3;
-        wxComboBox* ArtClient;
-        wxRadioButton* NoImage;
-        wxComboBox* ArtId;
+        wxStaticBitmap* Preview;
         wxStaticBoxSizer* StaticBoxSizer1;
-        wxFlexGridSizer* FlexGridSizer1;
         //*)
         wxTimer* Timer1;
 
