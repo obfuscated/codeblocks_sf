d21f5067120be926d3518efca3fe875b7a7cf0c7
Alpha <alpha@codeblocks.org>
2015-01-23 03:32:13 +0000

    - comments: doxygen main page (no functional change)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10078 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/src/app.h
+++ src/src/app.h
@@ -32,6 +32,76 @@
 #include "manager.h"
 #include "main.h"
 
+/** \mainpage Code::Blocks SDK
+ *
+ * The open source, cross platform, free C, C++ and Fortran IDE. The majority
+ * of features in Code::Blocks are implemented through a variety of plugins.
+ *
+ * The following are some useful places to jump into the sdk.
+ *
+ * Plugin interfaces:
+ * \dot
+ * digraph Plugins
+ * {
+ *   node [fontname="FreeSans", fontsize="10", shape=record];
+ *   rankdir="LR";
+ *   a [label="cbPlugin", URL="\ref cbPlugin", tooltip="Base class for all Code::Blocks plugins.", height=0.2];
+ *
+ *   b [label="cbCodeCompletionPlugin", URL="\ref cbCodeCompletionPlugin", tooltip="Base class for code-completion plugins.",  height=0.2];
+ *   a -> b [dir="back", color="MidnightBlue"];
+ *   c [label="cbCompilerPlugin",       URL="\ref cbCompilerPlugin",       tooltip="Base class for compiler plugins.",         height=0.2];
+ *   a -> c [dir="back", color="MidnightBlue"];
+ *   d [label="cbDebuggerPlugin",       URL="\ref cbDebuggerPlugin",       tooltip="Base class for debugger plugins.",         height=0.2];
+ *   a -> d [dir="back", color="MidnightBlue"];
+ *   e [label="cbMimePlugin",           URL="\ref cbMimePlugin",           tooltip="Base class for mime plugins.",             height=0.2];
+ *   a -> e [dir="back", color="MidnightBlue"];
+ *   f [label="cbSmartIndentPlugin",    URL="\ref cbSmartIndentPlugin",    tooltip="Base class for automatic indent plugins.", height=0.2];
+ *   a -> f [dir="back", color="MidnightBlue"];
+ *   g [label="cbToolPlugin",           URL="\ref cbToolPlugin",           tooltip="Base class for tool plugins.",             height=0.2];
+ *   a -> g [dir="back", color="MidnightBlue"];
+ *   h [label="cbWizardPlugin",         URL="\ref cbWizardPlugin",         tooltip="Base class for wizard plugins.",           height=0.2];
+ *   a -> h [dir="back", color="MidnightBlue"];
+ * }
+ * \enddot
+ *
+ * Managers:
+ * \dot
+ * digraph Managers
+ * {
+ *   node [fontname="FreeSans", fontsize="10", shape=record];
+ *   rankdir="LR";
+ *   a [label="Mgr\<MgrT\>", URL="\ref Mgr", tooltip="Singleton base for managers.", height=0.2];
+ *
+ *   b [label="CCManager",        URL="\ref CCManager",        tooltip="Governs access and shared code-completion functions.",  height=0.2];
+ *   a -> b [dir="back", color="MidnightBlue"];
+ *   c [label="ColourManager",    URL="\ref ColourManager",    tooltip="Manages configurable colour settings.",                 height=0.2];
+ *   a -> c [dir="back", color="MidnightBlue"];
+ *   d [label="ConfigManager",    URL="\ref ConfigManager",    tooltip="Unified settings storage.",                             height=0.2];
+ *   a -> d [dir="back", color="MidnightBlue"];
+ *   e [label="DebuggerManager",  URL="\ref DebuggerManager",  tooltip="Handles debugger plugins.",                             height=0.2];
+ *   a -> e [dir="back", color="MidnightBlue"];
+ *   f [label="EditorManager",    URL="\ref EditorManager",    tooltip="Handles open editors.",                                 height=0.2];
+ *   a -> f [dir="back", color="MidnightBlue"];
+ *   g [label="FileManager",      URL="\ref FileManager",      tooltip="Manages low level file access.",                        height=0.2];
+ *   a -> g [dir="back", color="MidnightBlue"];
+ *   h [label="LogManager",       URL="\ref LogManager",       tooltip="Access shared logging functions.",                      height=0.2];
+ *   a -> h [dir="back", color="MidnightBlue"];
+ *   i [label="MacrosManager",    URL="\ref MacrosManager",    tooltip="Handles text replacement macros.",                      height=0.2];
+ *   a -> i [dir="back", color="MidnightBlue"];
+ *   j [label="PluginManager",    URL="\ref PluginManager",    tooltip="Manages installed plugins.",                            height=0.2];
+ *   a -> j [dir="back", color="MidnightBlue"];
+ *   k [label="ProjectManager",   URL="\ref ProjectManager",   tooltip="Manages projects and the workspace.",                   height=0.2];
+ *   a -> k [dir="back", color="MidnightBlue"];
+ *   l [label="ScriptingManager", URL="\ref ScriptingManager", tooltip="Handles script registration and usage.",                height=0.2];
+ *   a -> l [dir="back", color="MidnightBlue"];
+ * }
+ * \enddot
+ *
+ * Also see the <a href="http://wiki.codeblocks.org/index.php?title=Developer_documentation">developer documentation</a>
+ * in the wiki.
+ */
+
+
 class cbSplashScreen;
 
 class CodeBlocksApp : public wxApp
