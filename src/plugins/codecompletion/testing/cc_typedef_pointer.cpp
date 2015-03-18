300209318c67cbda342555bf35953e0d57041286
asmwarrior <asmwarrior@codeblocks.org>
2015-03-18 14:58:14 +0000

    - CCTest: add a test case about struct pointer typedef. (SF ticket #34)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10154 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/codecompletion/testing/cc_typedef_pointer.cpp
@@ -0,0 +1,18 @@
+// bug report here:
+// Code::Blocks / Tickets / #34 Code completion does not recognize struct pointer typedef - https://sourceforge.net/p/codeblocks/tickets/34/
+
+typedef struct foo1
+{
+    int bar;
+} foo1;
+
+typedef struct foo1 * foo1Ptr;
+
+typedef struct foo2
+{
+    int bar;
+} foo2, * foo2Ptr;
+
+
+//foo1Ptr-> //bar
+//foo2Ptr-> //bar
