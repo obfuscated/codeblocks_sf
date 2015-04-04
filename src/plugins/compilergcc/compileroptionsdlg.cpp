2dd9c17404d4402e81260ec3c846b355791430b8
T Petrov <tpetrov@codeblocks.org>
2015-04-04 15:51:50 +0000

    * compiler: Correctly delete multiple selected search directories (fixes ticket #151)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10176 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/compilergcc/compileroptionsdlg.cpp
+++ src/plugins/compilergcc/compileroptionsdlg.cpp
@@ -1876,8 +1876,9 @@ void CompilerOptionsDlg::OnRemoveDirClick(cb_unused wxCommandEvent& event)
                      _("Confirmation"),
                      wxOK | wxCANCEL | wxICON_QUESTION) == wxID_OK)
     {
-        for (size_t i=0; i<selections.GetCount(); ++i)
-            control->Delete(selections[static_cast<unsigned int>(i)]);
+        std::sort(selections.begin(), selections.end());
+        for (unsigned int i=selections.GetCount(); i>0; --i)
+            control->Delete(selections[i-1]);
         m_bDirty = true;
     }
 } // OnRemoveDirClick
