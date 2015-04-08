9c4dd6c8117c61586c452ba61372d62ad17a7c43
asmwarrior <asmwarrior@codeblocks.org>
2015-04-08 08:30:46 +0000

    * CC: fix a copy-paste error introduced in r10189.
    See discussion on http://forums.codeblocks.org/index.php/topic,20183.0.html.
    
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10198 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/codecompletion/nativeparser.cpp
+++ src/plugins/codecompletion/nativeparser.cpp
@@ -2000,8 +2000,7 @@ bool NativeParser::AddCompilerPredefinedMacrosGCC(const wxString& compilerId, cb
     if (!compiler)
         return false;
 
-    if (   !parser->Options().platformCheck
-        || (parser->Options().platformCheck && compiler->SupportsCurrentPlatform()) )
+    if (parser->Options().platformCheck && !compiler->SupportsCurrentPlatform())
     {
         TRACE(_T("NativeParser::AddCompilerPredefinedMacrosGCC: Not supported on current platform!"));
         return false;
@@ -2116,8 +2115,7 @@ bool NativeParser::AddCompilerPredefinedMacrosVC(const wxString& compilerId, wxS
     if (!compiler)
         return false;
 
-    if (   !parser->Options().platformCheck
-        || (parser->Options().platformCheck && compiler->SupportsCurrentPlatform()) )
+    if (parser->Options().platformCheck && !compiler->SupportsCurrentPlatform())
     {
         TRACE(_T("NativeParser::AddCompilerPredefinedMacrosVC: Not supported on current platform!"));
         return false;
