bb42dbb0969e0fc7028b5edfd4166b364104e1c2
Jens Lody <jens@codeblocks.org>
2015-05-15 10:56:59 +0000

    - OccurrencesHighlight-plugin: alignment fixes to avoid asserts and broken layout with wx3
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10269 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/occurrenceshighlighting/occurrencespanel.cpp
+++ src/plugins/occurrenceshighlighting/occurrencespanel.cpp
@@ -31,7 +31,7 @@ OccurrencesPanel::OccurrencesPanel(wxWindow* parent,wxWindowID id)
     Create(parent, id, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("id"));
     BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
     ListCtrl1 = new wxListCtrl(this, ID_LISTCTRL1, wxDefaultPosition, wxDefaultSize, wxLC_LIST|wxLC_AUTOARRANGE|wxLC_SORT_ASCENDING, wxDefaultValidator, _T("ID_LISTCTRL1"));
-    BoxSizer1->Add(ListCtrl1, 1, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer1->Add(ListCtrl1, 1, wxEXPAND, 5);
     SetSizer(BoxSizer1);
     BoxSizer1->Fit(this);
     BoxSizer1->SetSizeHints(this);
