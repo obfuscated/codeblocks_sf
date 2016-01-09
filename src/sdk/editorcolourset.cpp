375f7faa6ced7edbc83b1bf31124f943180f5c30
T Petrov <tpetrov@codeblocks.org>
2016-01-09 12:44:58 +0000

    * revert rev 10634 because matching all files for plain text is wrong
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10640 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/editorcolourset.cpp
+++ src/sdk/editorcolourset.cpp
@@ -181,7 +181,7 @@ void EditorColourSet::LoadAvailableSets()
 
 HighlightLanguage EditorColourSet::AddHighlightLanguage(int lexer, const wxString& name)
 {
-    if (   lexer <= wxSCI_LEX_CONTAINER
+    if (   lexer <= wxSCI_LEX_NULL
         || lexer >  wxSCI_LEX_LAST // this is a C::B extension to wxscintilla.h
         || name.IsEmpty() )
     {
