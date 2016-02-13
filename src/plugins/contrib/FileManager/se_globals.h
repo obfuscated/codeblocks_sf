00ab966722eab880421a7f596d8abdc1503f16f5
T Petrov <tpetrov@codeblocks.org>
2016-02-13 16:01:51 +0000

    - FileManager: formatting
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10780 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/FileManager/se_globals.h
+++ src/plugins/contrib/FileManager/se_globals.h
@@ -27,12 +27,19 @@ bool WildCardListMatch(wxString list, wxString name, bool strip=false);
 bool PromptSaveOpenFile(wxString message, wxFileName path);
 
 inline void LogMessage(const wxString &msg)
-{ Manager::Get()->GetLogManager()->Log(msg); }
+{
+    Manager::Get()->GetLogManager()->Log(msg);
+}
 
 inline void LogErrorMessage(const wxString &msg)
-{ Manager::Get()->GetLogManager()->LogError(msg); }
+{
+    Manager::Get()->GetLogManager()->LogError(msg);
+}
 
-
-inline int MessageBox(wxWindow *parent, const wxString& message, const wxString& caption = wxEmptyString, int style = wxOK, int x = -1, int y = -1) { return cbMessageBox(message, caption, style, parent, x, y);}
+inline int MessageBox(wxWindow *parent, const wxString& message, const wxString& caption = wxEmptyString,
+                      int style = wxOK, int x = -1, int y = -1)
+{
+    return cbMessageBox(message, caption, style, parent, x, y);
+}
 
 #endif //IL_GLOBALS_H
