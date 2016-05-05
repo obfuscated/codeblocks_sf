f1e74cde468780d8073f78efa2e38a8faa30cf0b
T Petrov <tpetrov@codeblocks.org>
2016-05-05 14:29:18 +0000

    * ThreadSearch: Fix infinite filesystem traversal when there are cyclic symlinks present
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10846 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/ThreadSearch/ThreadSearchThread.cpp
+++ src/plugins/contrib/ThreadSearch/ThreadSearchThread.cpp
@@ -204,7 +204,7 @@ void ThreadSearchThread::OnExit()
 }
 
 
-wxDirTraverseResult ThreadSearchThread::OnDir(const wxString& WXUNUSED(dirName))
+wxDirTraverseResult ThreadSearchThread::OnDir(const wxString& dirName)
 {
     // Method is just used to test thread termination (user cancelled) and
     // stop recursive dir traversing if it is not required.
@@ -212,6 +212,12 @@ wxDirTraverseResult ThreadSearchThread::OnDir(const wxString& WXUNUSED(dirName))
     {
         return wxDIR_STOP;
     }
+
+    wxString path = cbResolveSymLinkedDirPathRecursive(dirName);
+    if (m_VisitedDirs.find(path) != m_VisitedDirs.end())
+        return wxDIR_IGNORE;
+    m_VisitedDirs.insert(path);
+
     return m_DefaultDirResult;
 }
 
