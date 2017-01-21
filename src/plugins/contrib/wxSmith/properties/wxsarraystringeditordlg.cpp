e34b37e6bd90a9f450e70966b3827ab12b7d1f9b
T Petrov <tpetrov@codeblocks.org>
2017-01-21 10:41:43 +0000

    * wxSmith: Fix the resizing of the ArrayStringEditor dialog
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10972 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmith/properties/wxsarraystringeditordlg.cpp
+++ src/plugins/contrib/wxSmith/properties/wxsarraystringeditordlg.cpp
@@ -25,8 +25,9 @@
 #include <wx/tokenzr.h>
 
 //(*InternalHeaders(wxsArrayStringEditorDlg)
-#include <wx/intl.h>
+#include <wx/button.h>
 #include <wx/string.h>
+#include <wx/intl.h>
 //*)
 
 //(*IdInit(wxsArrayStringEditorDlg)
@@ -44,32 +45,28 @@ wxsArrayStringEditorDlg::wxsArrayStringEditorDlg(wxWindow* parent,wxArrayString&
     Data(_Data)
 {
     //(*Initialize(wxsArrayStringEditorDlg)
-    wxButton* Button1;
-    wxBoxSizer* BoxSizer2;
-    wxButton* Button2;
     wxBoxSizer* BoxSizer1;
+    wxStdDialogButtonSizer* StdDialogButtonSizer1;
     wxStaticBoxSizer* StaticBoxSizer1;
 
     Create(parent, id, _("Edit items"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
     BoxSizer1 = new wxBoxSizer(wxVERTICAL);
     StaticBoxSizer1 = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Enter items (one item per line)"));
-    Items = new wxTextCtrl(this, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxSize(350,200), wxTE_MULTILINE, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    StaticBoxSizer1->Add(Items, 1, wxALIGN_CENTER_VERTICAL, 2);
-    BoxSizer1->Add(StaticBoxSizer1, 1, wxALIGN_CENTER_HORIZONTAL, 5);
-    BoxSizer2 = new wxBoxSizer(wxHORIZONTAL);
-    Button1 = new wxButton(this, wxID_OK, _("OK"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("wxID_OK"));
-    Button1->SetDefault();
-    BoxSizer2->Add(Button1, 1, wxTOP|wxBOTTOM|wxLEFT|wxALIGN_CENTER_VERTICAL, 5);
-    Button2 = new wxButton(this, wxID_CANCEL, _("Cancel"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("wxID_CANCEL"));
-    BoxSizer2->Add(Button2, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
-    BoxSizer1->Add(BoxSizer2, 0, wxALIGN_RIGHT, 5);
+    Items = new wxTextCtrl(this, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxSize(350,200), wxTE_MULTILINE|wxVSCROLL|wxHSCROLL, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+    StaticBoxSizer1->Add(Items, 1, wxEXPAND, 2);
+    BoxSizer1->Add(StaticBoxSizer1, 1, wxEXPAND, 5);
+    StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
+    StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_OK, wxEmptyString));
+    StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_CANCEL, wxEmptyString));
+    StdDialogButtonSizer1->Realize();
+    BoxSizer1->Add(StdDialogButtonSizer1, 0, wxALL|wxEXPAND, 5);
     SetSizer(BoxSizer1);
     BoxSizer1->Fit(this);
     BoxSizer1->SetSizeHints(this);
-
-    Connect(wxID_OK,wxEVT_COMMAND_BUTTON_CLICKED,wxCommandEventHandler(wxsArrayStringEditorDlg::OnOK));
-    Connect(wxID_CANCEL,wxEVT_COMMAND_BUTTON_CLICKED,wxCommandEventHandler(wxsArrayStringEditorDlg::OnCancel));
     //*)
+
+    Connect(wxID_OK,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&wxsArrayStringEditorDlg::OnOK);
+    Connect(wxID_CANCEL,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&wxsArrayStringEditorDlg::OnCancel);
 
     size_t Count = Data.Count();
     for ( size_t i = 0; i<Count; i++ )
--- src/plugins/contrib/wxSmith/properties/wxsarraystringeditordlg.h
+++ src/plugins/contrib/wxSmith/properties/wxsarraystringeditordlg.h
@@ -25,9 +25,8 @@
 
 //(*Headers(wxsArrayStringEditorDlg)
 #include <wx/sizer.h>
-#include <wx/textctrl.h>
-#include <wx/button.h>
 #include "scrollingdialog.h"
+#include <wx/textctrl.h>
 //*)
 
 
