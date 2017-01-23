4cf22f73f98ddc2a7e0002c147d83e8f398367a9
T Petrov <tpetrov@codeblocks.org>
2017-01-23 01:12:01 +0000

    - Fix issue introduced in rev10912
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@10978 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/editarrayfiledlg.cpp
+++ src/sdk/editarrayfiledlg.cpp
@@ -37,7 +37,7 @@ EditArrayFileDlg::EditArrayFileDlg(wxWindow* parent, wxArrayString& array, bool 
 {
     //ctor
     wxXmlResource::Get()->LoadObject(this, parent, _T("dlgEditArrayString"),_T("wxScrollingDialog"));
-    XRCCTRL(*this, "wxID_OK", wxButton)->Disable();
+    XRCCTRL(*this, "wxID_OK", wxButton)->SetDefault();
 
     wxListBox* list = XRCCTRL(*this, "lstItems", wxListBox);
     list->Clear();
