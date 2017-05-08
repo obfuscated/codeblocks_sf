1932e5e2f36dfe98a4e9104cf5714c1d740412e2
MortenMacFly <mortenmacfly@codeblocks.org>
2017-05-08 17:56:03 +0000

    * applied patch #182 by scarphin: Preserve open editors per target/project
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11060 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/openfileslist/openfileslistplugin.cpp
+++ src/plugins/openfileslist/openfileslistplugin.cpp
@@ -15,6 +15,8 @@
     #include <editormanager.h>
     #include <projectmanager.h>
     #include <logmanager.h>
+    #include <cbeditor.h>
+    #include <cbproject.h>
     #include <editorbase.h>
     #include <sdk_events.h>
     #include <misctreeitemdata.h>
@@ -27,6 +29,8 @@
 #endif
 
 #include "openfileslistplugin.h"
+#include "tinyxml.h"
+#include "tinywxuni.h"
 
 namespace
 {
@@ -35,6 +39,7 @@ namespace
 
     const int idOpenFilesTree = wxNewId();
     const int idViewOpenFilesTree = wxNewId();
+    const int idViewPreserveOpenEditors = wxNewId();
 
     class OpenFilesListData : public wxTreeItemData
     {
@@ -47,8 +52,9 @@ namespace
 }
 
 BEGIN_EVENT_TABLE(OpenFilesListPlugin, cbPlugin)
-    EVT_UPDATE_UI(idViewOpenFilesTree, OpenFilesListPlugin::OnUpdateUI)
+    EVT_UPDATE_UI_RANGE(idViewOpenFilesTree, idViewPreserveOpenEditors, OpenFilesListPlugin::OnUpdateUI)
     EVT_MENU(idViewOpenFilesTree, OpenFilesListPlugin::OnViewOpenFilesTree)
+    EVT_MENU(idViewPreserveOpenEditors, OpenFilesListPlugin::OnViewPreserveOpenEditors)
     EVT_TREE_ITEM_ACTIVATED(idOpenFilesTree, OpenFilesListPlugin::OnTreeItemActivated)
     EVT_TREE_ITEM_RIGHT_CLICK(idOpenFilesTree, OpenFilesListPlugin::OnTreeItemRightClick)
 END_EVENT_TABLE()
@@ -65,6 +71,11 @@ OpenFilesListPlugin::~OpenFilesListPlugin()
 
 void OpenFilesListPlugin::OnAttach()
 {
+    ConfigManager* cfg = Manager::Get()->GetConfigManager(_T("open_files_list"));
+    if (cfg)
+        m_PreserveOpenEditors = cfg->ReadBool(_T("preserve_open_editors"), false);
+    m_ProjectLoading = false;
+    m_pActiveProject = nullptr;
     m_ViewMenu = 0;
 
     m_EditorArray.Clear();
@@ -118,12 +129,19 @@ void OpenFilesListPlugin::OnAttach()
     pm->RegisterEventSink(cbEVT_EDITOR_MODIFIED, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorModified));
     pm->RegisterEventSink(cbEVT_EDITOR_OPEN, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorOpened));
     pm->RegisterEventSink(cbEVT_EDITOR_SAVE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnEditorSaved));
-    pm->RegisterEventSink(cbEVT_PROJECT_ACTIVATE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectOpened));
+    pm->RegisterEventSink(cbEVT_PROJECT_ACTIVATE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectActivated));
     pm->RegisterEventSink(cbEVT_PROJECT_OPEN, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectOpened));
+    pm->RegisterEventSink(cbEVT_PROJECT_CLOSE, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnProjectClosed));
+    pm->RegisterEventSink(cbEVT_BUILDTARGET_SELECTED, new cbEventFunctor<OpenFilesListPlugin, CodeBlocksEvent>(this, &OpenFilesListPlugin::OnBuildTargetSelected));
 }
 
 void OpenFilesListPlugin::OnRelease(bool appShutDown)
 {
+    // Write config
+    ConfigManager* cfg = Manager::Get()->GetConfigManager(_T("open_files_list"));
+    if (cfg)
+        cfg->Write(_T("preserve_open_editors"), m_PreserveOpenEditors);
+
     if (appShutDown)
         return;
     // remove registered event sinks
@@ -157,11 +175,20 @@ void OpenFilesListPlugin::BuildMenu(wxMenuBar* menuBar)
             if (items[i]->IsSeparator())
             {
                 m_ViewMenu->InsertCheckItem(i, idViewOpenFilesTree, _("&Open files list"), _("Toggle displaying the open files list"));
+                m_ViewPreserveChk = m_ViewMenu->InsertCheckItem(i + 2, idViewPreserveOpenEditors, _("&Preserve open editors"), _("Preserve open editors per target/project.\n"
+                                                                                                                                 "Only available the when the workspace is empty."));
+                m_ViewPreserveChk->Check(m_PreserveOpenEditors);
+                m_ViewMenu->InsertSeparator(i + 3);
                 return;
             }
         }
         // not found, just append
         m_ViewMenu->AppendCheckItem(idViewOpenFilesTree, _("&Open files list"), _("Toggle displaying the open files list"));
+        m_ViewMenu->AppendSeparator();
+        m_ViewPreserveChk = m_ViewMenu->AppendCheckItem(idViewPreserveOpenEditors, _("&Preserve open editors"), _("Preserve open editors per target/project\n"
+                                                                                                                  "Only available the when workspace is empty."));
+        m_ViewPreserveChk->Check(m_PreserveOpenEditors);
+        m_ViewMenu->AppendSeparator();
     }
 }
 
@@ -296,6 +323,11 @@ void OpenFilesListPlugin::OnViewOpenFilesTree(wxCommandEvent& event)
     Manager::Get()->ProcessEvent(evt);
 }
 
+void OpenFilesListPlugin::OnViewPreserveOpenEditors(wxCommandEvent& event)
+{
+    m_PreserveOpenEditors = event.IsChecked();
+}
+
 void OpenFilesListPlugin::OnUpdateUI(wxUpdateUIEvent& event)
 {
     if (m_ViewMenu)
@@ -303,6 +335,8 @@ void OpenFilesListPlugin::OnUpdateUI(wxUpdateUIEvent& event)
         bool isVis = IsWindowReallyShown((wxWindow*)m_pTree);
         m_ViewMenu->Check(idViewOpenFilesTree, isVis);
     }
+
+    m_ViewPreserveChk->Enable(Manager::Get()->GetProjectManager()->GetProjects()->IsEmpty());
 
     // must do...
     event.Skip();
@@ -351,8 +385,58 @@ void OpenFilesListPlugin::OnEditorSaved(CodeBlocksEvent& event)
     RefreshOpenFilesTree(event.GetEditor());
 }
 
-void OpenFilesListPlugin::OnProjectOpened(cb_unused CodeBlocksEvent& event)
+void OpenFilesListPlugin::OnProjectOpened(CodeBlocksEvent& event)
 {
+    // Load open editors list whether or not the preserve option selected not to lose any previous data
+    cbProject* prj = event.GetProject();
+    ProjectFilesMap pfm;
+    wxFileName fname(prj->GetFilename());
+    fname.SetExt(_T("layout"));
+    TiXmlDocument doc;
+    if (TinyXML::LoadDocument(fname.GetFullPath(), &doc))
+    {
+        TiXmlElement* root = doc.FirstChildElement("CodeBlocks_layout_file");
+        if (!root)
+            root = doc.FirstChildElement("Code::Blocks_layout_file");
+        if (root)
+        {
+            TiXmlElement* target = root->FirstChildElement("Target");
+            while (target)
+            {
+                wxString targetName = cbC2U(target->Attribute("name"));
+                wxString topFile = cbC2U(target->Attribute("top"));
+                if (topFile != wxEmptyString)
+                {
+                    ProjectFile* pf = prj->GetFileByFilename(topFile);
+                    if (pf)
+                        pfm[targetName].activeFile = pf;
+                }
+
+                int tp = 0;
+                TiXmlElement* file = target->FirstChildElement("File");
+                while (file)
+                {
+                    wxString fn = cbC2U(file->Attribute("name"));
+                    // Tab order may differ than the original, 'tp' is used as a cheap hack to restore the tab order
+                    ProjectFile* pf = prj->GetFileByFilename(fn);
+                    if (pf)
+                    {
+                        pf->editorTabPos = tp++;
+                        pfm[targetName].openFiles.insert(pf);
+                    }
+                    file = file->NextSiblingElement();
+                }
+
+                target = target->NextSiblingElement();
+            }
+        }
+    }
+
+    m_WorkspaceFilesMap.insert(std::pair<cbProject*, ProjectFilesMap>(prj, pfm));
+
+    if (m_PreserveOpenEditors)
+        m_ProjectLoading = true;
+
     if (m_EditorArray.IsEmpty())
         return;
 
@@ -365,4 +449,197 @@ void OpenFilesListPlugin::OnProjectOpened(cb_unused CodeBlocksEvent& event)
         }
     }
     m_EditorArray.Clear();
+}
+
+void OpenFilesListPlugin::OnProjectActivated(CodeBlocksEvent& event)
+{
+    if (m_PreserveOpenEditors)
+    {
+        cbProject* np = event.GetProject();
+        wxString nt = np->GetActiveBuildTarget();
+        if (m_ProjectLoading)
+            m_ProjectLoading = false;
+        else if (!m_pActiveProject) // Active project closed
+        {
+            if (m_WorkspaceFilesMap.find(np) != m_WorkspaceFilesMap.end() && m_WorkspaceFilesMap[np].find(nt) != m_WorkspaceFilesMap[np].end())
+                LoadEditors(event.GetProject(), event.GetProject()->GetActiveBuildTarget());
+        }
+        else // An already open project activated
+        {
+            SaveEditors(m_pActiveProject, m_ActiveTargetName);
+            CloseEditors(m_pActiveProject, m_ActiveTargetName);
+            if (m_WorkspaceFilesMap.find(np) != m_WorkspaceFilesMap.end() && m_WorkspaceFilesMap[np].find(nt) != m_WorkspaceFilesMap[np].end())
+                LoadEditors(event.GetProject(), event.GetProject()->GetActiveBuildTarget());
+        }
+
+        m_pActiveProject = event.GetProject();
+        m_ActiveTargetName = m_pActiveProject->GetActiveBuildTarget();
+    }
+
+    if (m_EditorArray.IsEmpty())
+        return;
+
+    for (size_t i = 0; i < m_EditorArray.GetCount(); ++i)
+    {
+        EditorBase* eb = m_EditorArray[i];
+        if (eb)
+        {
+            RefreshOpenFilesTree(eb);
+        }
+    }
+    m_EditorArray.Clear();
+}
+
+void OpenFilesListPlugin::OnProjectClosed(CodeBlocksEvent& event)
+{
+    cbProject* prj = event.GetProject();
+
+    if (m_PreserveOpenEditors)
+    {
+        if (event.GetProject() == m_pActiveProject) // Active project is closed
+        {
+            SaveEditors(m_pActiveProject, m_ActiveTargetName);
+            m_pActiveProject = nullptr;
+            m_ActiveTargetName = wxEmptyString;
+        }
+    }
+
+    // Save open editors list whether or not preserve option selected not to lose any previous data
+    if (!m_WorkspaceFilesMap[prj].empty())
+    {
+        ProjectFilesMap& pfm = m_WorkspaceFilesMap[prj];
+        wxFileName fname(prj->GetFilename());
+        fname.SetExt(_T("layout"));
+        TiXmlDocument doc;
+        doc.SetCondenseWhiteSpace(false);
+        if (TinyXML::LoadDocument(fname.GetFullPath(), &doc))
+        {
+            TiXmlElement* root = doc.FirstChildElement("CodeBlocks_layout_file");
+            for (ProjectFilesMap::iterator itt = pfm.begin(); itt != pfm.end(); itt++)
+            {
+                // Do not save renamed or deleted targets
+                if (prj->GetBuildTarget((*itt).first))
+                {
+                    TiXmlElement* target = static_cast<TiXmlElement*>(root->InsertEndChild(TiXmlElement("Target")));
+                    target->SetAttribute("name", cbU2C((*itt).first));
+                    if ((*itt).second.activeFile)
+                        target->SetAttribute("top", cbU2C((*itt).second.activeFile->relativeFilename));
+                    else
+                        target->SetAttribute("top", cbU2C(wxEmptyString));
+                    for (TargetFilesData::OpenFilesSet::iterator itf = (*itt).second.openFiles.begin(); itf != (*itt).second.openFiles.end(); ++itf)
+                    {
+                        TiXmlElement* file = static_cast<TiXmlElement*>(target->InsertEndChild(TiXmlElement("File")));
+                        file->SetAttribute("name", cbU2C((*itf)->relativeFilename));
+                    }
+                }
+            }
+            cbSaveTinyXMLDocument(&doc, fname.GetFullPath());
+        }
+    }
+
+    m_WorkspaceFilesMap.erase(event.GetProject());
+}
+
+void OpenFilesListPlugin::OnBuildTargetSelected(CodeBlocksEvent& event)
+{
+    if (m_PreserveOpenEditors && event.GetBuildTargetName() != wxEmptyString) // Make sure project is not closing
+    {
+        wxString nt = event.GetBuildTargetName();
+        if (event.GetProject() != m_pActiveProject) // Another project is opened or activated
+        {
+            if (!m_pActiveProject) // Workspace is empty, nothing to do
+                return;
+            else if (m_WorkspaceFilesMap.find(event.GetProject()) == m_WorkspaceFilesMap.end()) // New project is opened
+            {
+                SaveEditors(m_pActiveProject, m_ActiveTargetName);
+                CloseEditors(m_pActiveProject, m_ActiveTargetName);
+            }
+        }
+        else // Project's targets switched
+        {
+            SaveEditors(m_pActiveProject, m_ActiveTargetName);
+
+            // Load new target's editors if there is any, keep the open editors otherwise
+            if (m_WorkspaceFilesMap[m_pActiveProject].find(nt) != m_WorkspaceFilesMap[m_pActiveProject].end())
+            {
+                CloseEditors(m_pActiveProject, m_ActiveTargetName);
+                LoadEditors(m_pActiveProject, nt);
+            }
+            m_ActiveTargetName = nt;
+        }
+    }
+}
+
+void OpenFilesListPlugin::SaveEditors(cbProject* project, const wxString& targetName)
+{
+    EditorManager* em = Manager::Get()->GetEditorManager();
+    if (em)
+    {
+        // Save target's open editors sorted by their tab order
+        m_WorkspaceFilesMap[project][targetName].openFiles.clear();
+        em->UpdateProjectFiles(project);
+        for (int i = 0; i < em->GetEditorsCount(); i++)
+        {
+            EditorBase* eb = em->GetEditor(i);
+            if (eb->IsBuiltinEditor())
+            {
+                ProjectFile* pf = project->GetFileByFilename(eb->GetFilename(), false);
+                if (pf)
+                    m_WorkspaceFilesMap[project][targetName].openFiles.insert(pf);
+            }
+        }
+        // Save active editor
+        EditorBase* eb = em->GetActiveEditor();
+        m_WorkspaceFilesMap[project][targetName].activeFile = (eb && eb->IsBuiltinEditor() ? project->GetFileByFilename(eb->GetFilename(), false) : nullptr);
+    }
+}
+
+void OpenFilesListPlugin::CloseEditors(cbProject* project, const wxString& targetName)
+{
+    EditorManager* em = Manager::Get()->GetEditorManager();
+    if (em)
+    {
+        // Close old target's open editors, CloseAll() won't do good here because other non-project files may already be opened
+        TargetFilesData::OpenFilesSet::iterator it;
+        for (it = m_WorkspaceFilesMap[project][targetName].openFiles.begin(); it != m_WorkspaceFilesMap[project][targetName].openFiles.end(); ++it)
+            em->Close((*it)->file.GetFullPath());
+    }
+}
+
+void OpenFilesListPlugin::LoadEditors(cbProject* project, const wxString& targetName)
+{
+    EditorManager* em = Manager::Get()->GetEditorManager();
+    if (em)
+    {
+        // Save and close non-project built-in editors
+        std::set<wxString> otherEditors;
+        for (int i = 0; i < em->GetEditorsCount(); i++)
+        {
+            EditorBase* eb = em->GetEditor(i);
+            if (eb->IsBuiltinEditor())
+            {
+                wxString fn = em->GetBuiltinEditor(eb)->GetFilename();
+                if (fn != wxEmptyString)
+                {
+                    otherEditors.insert(fn);
+                    em->Close(fn);
+                }
+            }
+        }
+
+        TargetFilesData::OpenFilesSet::iterator it;
+        // Load target's open editors
+        for (it = m_WorkspaceFilesMap[project][targetName].openFiles.begin(); it != m_WorkspaceFilesMap[project][targetName].openFiles.end(); ++it)
+            em->Open((*it)->file.GetFullPath());
+
+        // Reopen non-project built-in editors to maintain consistency with initial project startup
+        std::set<wxString>::iterator its;
+        for (its = otherEditors.begin(); its != otherEditors.end(); ++its)
+            em->Open(*its);
+
+        // Activate target's active editor
+        ProjectFile* pf = m_WorkspaceFilesMap[project][targetName].activeFile;
+        if (pf)
+            em->Open(pf->file.GetFullPath());
+    }
 }
--- src/plugins/openfileslist/openfileslistplugin.h
+++ src/plugins/openfileslist/openfileslistplugin.h
@@ -7,6 +7,7 @@
 #define OPENFILESLISTPLUGIN_H
 
 #include <cbplugin.h>
+#include <projectfile.h>
 
 #include <wx/dynarray.h>
 
@@ -16,7 +17,22 @@ class wxMenuBar;
 class wxImageList;
 class EditorBase;
 
+struct TargetFilesData
+{
+    TargetFilesData() : activeFile(nullptr) {} // ctor
+    // Functor for the std::set predicate to sort the opened editor files according to their tab order
+    struct compareLess
+    {
+        bool operator()(const ProjectFile* lhs, const ProjectFile* rhs) { return lhs->editorTabPos < rhs->editorTabPos; }
+    };
+    typedef std::set<ProjectFile*, compareLess> OpenFilesSet;
+    ProjectFile* activeFile;
+    OpenFilesSet openFiles;
+};
+
 WX_DEFINE_ARRAY(EditorBase*, EditorArray);
+typedef std::map<wxString, TargetFilesData> ProjectFilesMap;
+typedef std::map<cbProject*, ProjectFilesMap> WorkspaceFilesMap;
 
 class OpenFilesListPlugin : public cbPlugin
 {
@@ -38,6 +54,7 @@ class OpenFilesListPlugin : public cbPlugin
         void OnTreeItemActivated(wxTreeEvent& event);
         void OnTreeItemRightClick(wxTreeEvent& event);
         void OnViewOpenFilesTree(wxCommandEvent& event);
+        void OnViewPreserveOpenEditors(wxCommandEvent& event);
         void OnUpdateUI(wxUpdateUIEvent& event);
 
         void OnEditorActivated(CodeBlocksEvent& event);
@@ -48,12 +65,25 @@ class OpenFilesListPlugin : public cbPlugin
         void OnEditorSaved(CodeBlocksEvent& event);
 
         void OnProjectOpened(CodeBlocksEvent& event);
+        void OnProjectActivated(CodeBlocksEvent& event);
+        void OnProjectClosed(CodeBlocksEvent& event);
+        void OnBuildTargetSelected(CodeBlocksEvent& event);
 
         wxTreeCtrl* m_pTree;
         wxImageList* m_pImages;
         wxMenu* m_ViewMenu;
+        wxMenuItem* m_ViewPreserveChk;
     private:
+        void SaveEditors(cbProject* project, const wxString& targetName);
+        void CloseEditors(cbProject* project, const wxString& targetName);
+        void LoadEditors(cbProject* project, const wxString& targetName);
+
         EditorArray m_EditorArray;
+        bool m_PreserveOpenEditors;
+        bool m_ProjectLoading;
+        cbProject* m_pActiveProject;
+        wxString m_ActiveTargetName;
+        WorkspaceFilesMap m_WorkspaceFilesMap;
         DECLARE_EVENT_TABLE();
 };
 
