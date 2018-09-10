f7d83598bc06de0477067732028c7a5a383ffd3c
T Petrov <tpetrov@codeblocks.org>
2018-09-10 18:30:10 +0000

    - rndgen: Replace tabs with spaces (ticket #736)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11462 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/rndgen/rndgen.cpp
+++ src/plugins/contrib/rndgen/rndgen.cpp
@@ -17,92 +17,92 @@ namespace
 
 void RndGen::OnAttach()
 {
-	Manager::Get()->RegisterEventSink(cbEVT_EDITOR_BEFORE_SAVE, new cbEventFunctor<RndGen, CodeBlocksEvent>(this, &RndGen::OnSave));
+    Manager::Get()->RegisterEventSink(cbEVT_EDITOR_BEFORE_SAVE, new cbEventFunctor<RndGen, CodeBlocksEvent>(this, &RndGen::OnSave));
 }
 
 void RndGen::OnSave(CodeBlocksEvent& event)
 {
-	cbEditor* ed = (cbEditor*) event.GetEditor();
-	cbStyledTextCtrl* ctrl = ed->GetControl();
+    cbEditor* ed = (cbEditor*) event.GetEditor();
+    cbStyledTextCtrl* ctrl = ed->GetControl();
 
-	if (ctrl->GetText().Contains(_T("RANDGEN:")) == false)
-		return;
+    if (ctrl->GetText().Contains(_T("RANDGEN:")) == false)
+        return;
 
-	const int Pos = ctrl->GetCurrentPos();
-	ctrl->SetUndoCollection(false);
+    const int Pos = ctrl->GetCurrentPos();
+    ctrl->SetUndoCollection(false);
 
-	wxRegEx int_re(_T("([0-9]+)\\ *;?\\ */\\*(\\ *RANDGEN:INT\\((.*))\\*/"));
+    wxRegEx int_re(_T("([0-9]+)\\ *;?\\ */\\*(\\ *RANDGEN:INT\\((.*))\\*/"));
 //	wxRegEx alnum_re(_T("\\\"([^\"]+)\\\"\\ *;?\\ */\\*(\\ *RANDGEN:ALNUM\\((.*))\\*/"));
-	wxRegEx alnum_re(_T("\\\"([^\\\"]+)\\\"\\ *;?\\ */\\*(\\ *RANDGEN:(ALNUM|DIGITS|CHARS|UPPERCHARS|LOWERCHARS)\\((.*))\\*/"));
+    wxRegEx alnum_re(_T("\\\"([^\\\"]+)\\\"\\ *;?\\ */\\*(\\ *RANDGEN:(ALNUM|DIGITS|CHARS|UPPERCHARS|LOWERCHARS)\\((.*))\\*/"));
 
-	const wxString Chars(_T("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+    const wxString Chars(_T("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
 
-	assert(alnum_re.IsValid() && int_re.IsValid());
+    assert(alnum_re.IsValid() && int_re.IsValid());
 
-	const unsigned int n = ctrl->GetLineCount();
-	for (unsigned int i = 0; i < n; ++i)
-	{
-		const int a = ctrl->GetLineIndentPosition(i);
-		const int b = ctrl->GetLineEndPosition(i);
-		wxString s(ctrl->GetTextRange(a, b));
+    const unsigned int n = ctrl->GetLineCount();
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        const int a = ctrl->GetLineIndentPosition(i);
+        const int b = ctrl->GetLineEndPosition(i);
+        wxString s(ctrl->GetTextRange(a, b));
 
-		// This is rather crude and will only match a single hit per line.
-		// However, that should be good enough for any sensible use, as you will not want more than that anyway.
-		// Still, if more correct matching is needed, feel free to do this as homework :-)
-		if (int_re.Matches(s))
-		{
-			const wxString Search(int_re.GetMatch(s, 1));
-			long Arg;
-			int_re.GetMatch(s, 3).ToLong(&Arg);
-			wxString Replace;
-			Replace.Printf(_T("%u"), RandGen() % (Arg+1));
-			s.Replace(Search, Replace, false);
+        // This is rather crude and will only match a single hit per line.
+        // However, that should be good enough for any sensible use, as you will not want more than that anyway.
+        // Still, if more correct matching is needed, feel free to do this as homework :-)
+        if (int_re.Matches(s))
+        {
+            const wxString Search(int_re.GetMatch(s, 1));
+            long Arg;
+            int_re.GetMatch(s, 3).ToLong(&Arg);
+            wxString Replace;
+            Replace.Printf(_T("%u"), RandGen() % (Arg+1));
+            s.Replace(Search, Replace, false);
 
-			ctrl->SetTargetStart(a);
-			ctrl->SetTargetEnd(b);
-			ctrl->ReplaceTarget(s);
-		}
-		else if (alnum_re.Matches(s))
-		{
-			const wxString Search(alnum_re.GetMatch(s, 1));
-			const wxString What(alnum_re.GetMatch(s, 3));
-			long Arg;
-			alnum_re.GetMatch(s, 4).ToLong(&Arg);
-			wxString Replace;
-			if (What == _T("ALNUM"))
-			{
-				for (int j = 0; j < Arg; ++j)
-					Replace += Chars[RandGen() % Chars.length()];
-			}
-			else if (What == _T("DIGITS"))
-			{
-				for (int j = 0; j < Arg; ++j)
-					Replace += Chars[RandGen() % 10];
-			}
-			else if (What == _T("CHARS"))
-			{
-				for (int j = 0; j < Arg; ++j)
-					Replace += Chars[10+ RandGen() % (Chars.length() - 10)];
-			}
-			else if (What == _T("UPPERCHARS"))
-			{
-				for (int j = 0; j < Arg; ++j)
-					Replace += Chars[36 + RandGen() % 26];
-			}
-			else if(What == _T("LOWERCHARS"))
-			{
-				for (int j = 0; j < Arg; ++j)
-					Replace += Chars[10 + RandGen() % 26];
-			}
+            ctrl->SetTargetStart(a);
+            ctrl->SetTargetEnd(b);
+            ctrl->ReplaceTarget(s);
+        }
+        else if (alnum_re.Matches(s))
+        {
+            const wxString Search(alnum_re.GetMatch(s, 1));
+            const wxString What(alnum_re.GetMatch(s, 3));
+            long Arg;
+            alnum_re.GetMatch(s, 4).ToLong(&Arg);
+            wxString Replace;
+            if (What == _T("ALNUM"))
+            {
+                for (int j = 0; j < Arg; ++j)
+                    Replace += Chars[RandGen() % Chars.length()];
+            }
+            else if (What == _T("DIGITS"))
+            {
+                for (int j = 0; j < Arg; ++j)
+                    Replace += Chars[RandGen() % 10];
+            }
+            else if (What == _T("CHARS"))
+            {
+                for (int j = 0; j < Arg; ++j)
+                    Replace += Chars[10+ RandGen() % (Chars.length() - 10)];
+            }
+            else if (What == _T("UPPERCHARS"))
+            {
+                for (int j = 0; j < Arg; ++j)
+                    Replace += Chars[36 + RandGen() % 26];
+            }
+            else if(What == _T("LOWERCHARS"))
+            {
+                for (int j = 0; j < Arg; ++j)
+                    Replace += Chars[10 + RandGen() % 26];
+            }
 
-			s.Replace(Search, Replace, false);
+            s.Replace(Search, Replace, false);
 
-			ctrl->SetTargetStart(a);
-			ctrl->SetTargetEnd(b);
-			ctrl->ReplaceTarget(s);
-		}
-	}
+            ctrl->SetTargetStart(a);
+            ctrl->SetTargetEnd(b);
+            ctrl->ReplaceTarget(s);
+        }
+    }
 
-	ctrl->SetUndoCollection(true);
-	ctrl->SetCurrentPos(Pos);
+    ctrl->SetUndoCollection(true);
+    ctrl->SetCurrentPos(Pos);
 }
