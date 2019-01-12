9c106293cd128be3e8a0fd7ac89c5f3d8afab869
T Petrov <tpetrov@codeblocks.org>
2019-01-12 11:22:05 +0000

    - build: Add override in the WatchesDlg
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11549 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/src/watchesdlg.h
+++ src/src/watchesdlg.h
@@ -26,12 +26,12 @@ class WatchesDlg : public wxPanel, public cbWatchesDlg
 
         wxWindow* GetWindow() { return this; }
 
-        void UpdateWatches();
-        void AddWatch(cb::shared_ptr<cbWatch> watch);
-        void AddSpecialWatch(cb::shared_ptr<cbWatch> watch, bool readonly);
-        void RemoveWatch(cb::shared_ptr<cbWatch> watch);
-        void RenameWatch(wxObject *prop, const wxString &newSymbol);
-        void RefreshUI();
+        void UpdateWatches() override;
+        void AddWatch(cb::shared_ptr<cbWatch> watch) override;
+        void AddSpecialWatch(cb::shared_ptr<cbWatch> watch, bool readonly) override;
+        void RemoveWatch(cb::shared_ptr<cbWatch> watch) override;
+        void RenameWatch(wxObject *prop, const wxString &newSymbol) override;
+        void RefreshUI() override;
     private:
         void OnExpand(wxPropertyGridEvent &event);
         void OnCollapse(wxPropertyGridEvent &event);
