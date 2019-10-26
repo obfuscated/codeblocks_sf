b8692c78b8203167e6b51547668752d085b6f8ff
T Petrov <tpetrov@codeblocks.org>
2019-10-26 00:57:15 +0000

    - build: Fix the update.bat script in the SpellChecker plugin (ticket #882, thanks Miguel Gimenez)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11881 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/SpellChecker/update.bat
+++ src/plugins/contrib/SpellChecker/update.bat
@@ -14,14 +14,14 @@ xcopy /D /Y dictionaries\*.dic "%CB_DEVEL_RESDIR%\SpellChecker\"
 xcopy /D /Y dictionaries\th_*.dat "%CB_DEVEL_RESDIR%\SpellChecker\"
 xcopy /D /Y dictionaries\th_*.idx "%CB_DEVEL_RESDIR%\SpellChecker\"
 
-xcopy /D /Y *.png "%CB_DEVEL_RESDIR%\images\settings\*"
-call:copyImageFiles rc "%CB_DEVEL_RESDIR%\SpellChecker\"
+xcopy /D /Y *.png "%CB_DEVEL_RESDIR%\images\settings\"
+call:copyImageFiles rc "%CB_DEVEL_RESDIR%\SpellChecker"
 
 set CB_OUTPUT_DIR=output%1
 set CB_OUTPUT_RESDIR=..\..\..\%CB_OUTPUT_DIR%\share\CodeBlocks
 
-xcopy /D /Y "%CB_DEVEL_RESDIR%\SpellChecker\*" "%CB_OUTPUT_RESDIR%\SpellChecker\*"
-xcopy /D /Y *.png "%CB_OUTPUT_RESDIR%\images\settings\*"
+xcopy /D /Y "%CB_DEVEL_RESDIR%\SpellChecker\*" "%CB_OUTPUT_RESDIR%\SpellChecker"
+xcopy /D /Y *.png "%CB_OUTPUT_RESDIR%\images\settings"
 
 ::--------------------------------------------------------
 ::-- Function section starts below here
@@ -35,12 +35,12 @@ GOTO:EOF
 
 :copyImageFiles - create a directory and copy image files to it
 setlocal
-echo Copy image files from %~1 to %~1
+echo Copy image files from %~1 to %~2
 REM call mkdirSilent %~2
 for %%g in (16x16,20x20,24x24,28x28,32x32,40x40,48x48,56x56,64x64) do (
     echo From %~1\%%g to %~2\%%g
     call:mkdirSilent %~2\%%g
-    xcopy /D /y %~1\%%g\*.png %~2\%%g > nul
+    xcopy /D /Y %~1\%%g\*.png %~2\%%g > nul
 )
 REM     call:mkdirSilent "%CB_OUTPUT_RESDIR%\images\ThreadSearch\32x32"
 REM     xcopy /D /y "%CB_DEVEL_RESDIR%\images\ThreadSearch\32x32\*.png" "%CB_OUTPUT_RESDIR%\images\ThreadSearch\32x32" > nul
