b0ef63b9aa05032fa65ecd4ff1e0430a6037a0e8
T Petrov <tpetrov@codeblocks.org>
2019-11-04 19:35:19 +0000

    * sdk: Small optimization in addFile (don't call wxFileName::GetExt multiple times)
    > We already have the extension in a variable, so reuse it.
    
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@11899 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/cbproject.cpp
+++ src/sdk/cbproject.cpp
@@ -809,7 +809,7 @@ ProjectFile* cbProject::AddFile(int targetIndex, const wxString& filename, bool 
         Compiler* c = CompilerFactory::GetCompiler( GetCompilerID() );
         if (c)
         {
-            const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, fname.GetExt());
+            const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, ext);
             if (t && t->generatedFiles.GetCount())
                 GenFilesHackMap[c] = t;
         }
@@ -822,7 +822,7 @@ ProjectFile* cbProject::AddFile(int targetIndex, const wxString& filename, bool 
 
             if (c)
             {
-                const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, fname.GetExt());
+                const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, ext);
                 if (t && t->generatedFiles.GetCount())
                     GenFilesHackMap[c] = t;
             }
@@ -834,7 +834,7 @@ ProjectFile* cbProject::AddFile(int targetIndex, const wxString& filename, bool 
         Compiler* c = CompilerFactory::GetCompiler(m_Targets[targetIndex]->GetCompilerID());
         if (c)
         {
-            const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, fname.GetExt());
+            const CompilerTool* t = c->GetCompilerTool(isResource ? ctCompileResourceCmd : ctCompileObjectCmd, ext);
             if (t && t->generatedFiles.GetCount())
                 GenFilesHackMap[c] = t;
         }
