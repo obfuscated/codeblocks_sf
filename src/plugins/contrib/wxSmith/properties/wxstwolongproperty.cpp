9c60a2aa488b1ef5538800144ffb5d5144fb98fb
bluehazzard <bluehazzard2@gmail.com>
2020-04-21 22:40:13 +0000

    - wxSmith: Add double long property (Ticket #926)
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12070 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/wxSmith/properties/wxstwolongproperty.cpp
@@ -0,0 +1,236 @@
+
+/*
+* This file is part of wxSmith plugin for Code::Blocks Studio
+* Copyright (C) 2006-2020
+*
+* wxSmith is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* $Revision: $
+* $Id: $
+* $HeadURL: $
+*/
+
+#include "wxstwolongproperty.h"
+#include <globals.h>
+
+wxString wxsTwoLongData::GetLongCode(wxsCoderContext* Context)
+{
+    switch ( Context->m_Language )
+    {
+        case wxsCPP:
+        {
+            return wxString::Format(_T("%ld, %ld"), Value1, Value2);
+        }
+
+        case wxsUnknownLanguage: // fall-through
+        default:
+        {
+            wxsCodeMarks::Unknown(_T("wxsTwoLongData::GetLongCode"),Context->m_Language);
+        }
+    }
+    return wxEmptyString;
+}
+
+namespace
+{
+    // Some helper values to create compound properties
+    enum
+    {
+        DIM_DEF    = 1,
+        DIM_VALUE1 = 2,
+        DIM_VALUE2 = 3,
+    };
+}
+
+
+
+// Helper macros for fetching variables
+#define VALUE1   (wxsVARIABLE(Object,Offset,wxsTwoLongData).Value1)
+#define VALUE2   (wxsVARIABLE(Object,Offset,wxsTwoLongData).Value2)
+#define DEFVALUE   (wxsVARIABLE(Object,Offset,wxsTwoLongData).DefValue)
+
+
+wxsTowLongProperty::wxsTowLongProperty(
+            const wxString& PGName,
+            const wxString& PGValue1Name,
+            const wxString& PGValue2Name,
+            const wxString& DataName,
+            long _Offset,
+            long _DefaultValue1,
+            long _DefaultValue2,
+            int Priority) :
+        wxsProperty(PGName,DataName,Priority),
+        Offset(_Offset),
+        DefaultValue1(_DefaultValue1),
+        DefaultValue2(_DefaultValue2),
+        Value1Name(PGValue1Name),
+        Value2Name(PGValue2Name)
+{}
+
+
+void wxsTowLongProperty::PGCreate(wxsPropertyContainer* Object,wxPropertyGridManager* Grid,wxPGId Parent)
+{
+    wxPGId DefId = Grid->AppendIn(Parent,NEW_IN_WXPG14X wxBoolProperty(GetPGName(),wxPG_LABEL,DEFVALUE));
+    wxPGId V1Id = Grid->AppendIn(Parent,NEW_IN_WXPG14X wxIntProperty(Value1Name,wxPG_LABEL,VALUE1));
+    wxPGId V2Id = Grid->AppendIn(Parent,NEW_IN_WXPG14X wxIntProperty(Value2Name,wxPG_LABEL,VALUE2));
+
+    Grid->SetPropertyAttribute(DefId,wxPG_BOOL_USE_CHECKBOX,1L,wxPG_RECURSE);
+
+    PGRegister(Object,Grid,DefId,DIM_DEF);
+    PGRegister(Object,Grid,V1Id,DIM_VALUE1);
+    PGRegister(Object,Grid,V2Id,DIM_VALUE2);
+
+    if ( DEFVALUE )
+    {
+        Grid->DisableProperty(V1Id);
+        Grid->DisableProperty(V2Id);
+    }
+
+}
+
+bool wxsTowLongProperty::PGRead(wxsPropertyContainer* Object,wxPropertyGridManager* Grid,wxPGId Id,long Index)
+{
+    switch ( Index )
+    {
+        case DIM_DEF:
+            DEFVALUE = Grid->GetPropertyValue(Id).GetBool();
+            break;
+
+        case DIM_VALUE1:
+            VALUE1 = Grid->GetPropertyValue(Id).GetLong();
+            break;
+
+        case DIM_VALUE2:
+            VALUE2 = Grid->GetPropertyValue(Id).GetLong();
+            break;
+
+        default:
+            break;
+    }
+
+    return true;
+}
+
+bool wxsTowLongProperty::PGWrite(wxsPropertyContainer* Object,wxPropertyGridManager* Grid,wxPGId Id,long Index)
+{
+    switch ( Index )
+    {
+        case DIM_DEF:
+            Grid->SetPropertyValue(Id,DEFVALUE);
+            break;
+
+        case DIM_VALUE1:
+            if ( DEFVALUE )
+            {
+                Grid->DisableProperty(Id);
+            }
+            else
+            {
+                Grid->EnableProperty(Id);
+            }
+            Grid->SetPropertyValue(Id,VALUE1);
+            break;
+
+        case DIM_VALUE2:
+            if ( DEFVALUE )
+            {
+                Grid->DisableProperty(Id);
+            }
+            else
+            {
+                Grid->EnableProperty(Id);
+            }
+            Grid->SetPropertyValue(Id,VALUE2);
+            break;
+
+        default:
+            break;
+    }
+    return true;
+}
+
+bool wxsTowLongProperty::XmlRead(wxsPropertyContainer* Object,TiXmlElement* Element)
+{
+    if ( !Element )
+    {
+        DEFVALUE = true;
+        VALUE1 = -1;
+        VALUE2 = -1;
+        return false;
+    }
+
+    const char* Text = Element->GetText();
+
+    // If no node or empty text, using default values
+    if ( !Text || !Text[0] )
+    {
+        DEFVALUE = true;
+        VALUE1 = -1;
+        VALUE2 = -1;
+        return false;
+    }
+    wxString Str = cbC2U(Text);
+
+    if ( !Str.BeforeFirst(_T(',')).ToLong(&VALUE1) ||
+         !Str.AfterLast(_T(',')).ToLong(&VALUE2) )
+    {
+        DEFVALUE = true;
+        VALUE1 = -1;
+        VALUE2 = -1;
+        return false;
+    }
+    DEFVALUE = false;
+
+    return true;
+}
+
+bool wxsTowLongProperty::XmlWrite(wxsPropertyContainer* Object,TiXmlElement* Element)
+{
+    if ( !DEFVALUE )
+    {
+        wxString Str;
+        Str.Printf(_T("%ld,%ld"),VALUE1,VALUE2);
+        Element->InsertEndChild(TiXmlText(cbU2C(Str)));
+        return true;
+    }
+    return false;
+}
+
+bool wxsTowLongProperty::PropStreamRead(wxsPropertyContainer* Object,wxsPropertyStream* Stream)
+{
+    bool Ret = true;
+    Stream->SubCategory(GetDataName());
+    if ( !Stream->GetBool(_T("default"),DEFVALUE,true) ) Ret = false;
+    if ( !DEFVALUE )
+    {
+        if ( !Stream->GetLong(_T("value1"),VALUE1,-1) ) Ret = false;
+        if ( !Stream->GetLong(_T("value2"),VALUE2,-1) ) Ret = false;
+    }
+    Stream->PopCategory();
+    return Ret;
+}
+
+bool wxsTowLongProperty::PropStreamWrite(wxsPropertyContainer* Object,wxsPropertyStream* Stream)
+{
+    bool Ret = true;
+    Stream->SubCategory(GetDataName());
+    if ( !Stream->PutBool(_T("default"),DEFVALUE,true) ) Ret = false;
+    if ( !DEFVALUE )
+    {
+        if ( !Stream->PutLong(_T("value1"),VALUE1,-1) ) Ret = false;
+        if ( !Stream->PutLong(_T("value2"),VALUE2,-1) ) Ret = false;
+    }
+    Stream->PopCategory();
+    return Ret;
+}
---
+++ src/plugins/contrib/wxSmith/properties/wxstwolongproperty.h
@@ -0,0 +1,149 @@
+/*
+* This file is part of wxSmith plugin for Code::Blocks Studio
+* Copyright (C) 2006-2020
+*
+* wxSmith is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* $Revision: $
+* $Id: $
+* $HeadURL:$
+*/
+
+#ifndef WXSTWOLONGPROPERTY_H
+#define WXSTWOLONGPROPERTY_H
+
+#include "wxsproperties.h"
+#include "../wxWidgets/wxscodercontext.h"
+
+struct wxsTwoLongData
+{
+    long Value1;
+    long Value2;
+    bool DefValue;
+
+    wxsTwoLongData(): Value1(0),  Value2(0), DefValue(true)  {}
+
+    /** \brief Function to get the first value
+     */
+    inline long GetValue1()
+    {
+        return Value1;
+    }
+
+    /** \brief Function to get the second value
+     */
+    inline long GetValue2()
+    {
+        return Value2;
+    }
+
+    /** \brief Return true if the default value is used
+     */
+    inline bool GetDefValue()
+    {
+        return DefValue;
+    }
+
+    /** \brief Function getting code returning valid value
+     *
+     * This function may be used when generating code. It will return
+     * correct representation of value which may be directly passed
+     * to created object.
+     *
+     */
+    wxString GetLongCode(wxsCoderContext* Context);
+
+};
+
+/** \brief Dimension property (long integer value which may be in pixel or dialog units) */
+class wxsTowLongProperty: public wxsProperty
+{
+    public:
+        /** \brief Ctor
+         *  \param PGName               name of property in Property Grid
+         *  \param PGDUName             name of "use dialog units" property
+         *  \param DataName             name of property in data stuctures
+         *  \param Offset               offset of wxsTwoLongData value (taken from wxsOFFSET macro)
+         *  \param DefaultValue1        default value 1 applied on read errors
+         *  \param DefaultValue2        default value 2 applied on read errors
+         *  \param Priority             priority of this property
+         */
+        wxsTowLongProperty(
+            const wxString& PGName,
+            const wxString& PGValue1Name,
+            const wxString& PGValue2Name,
+            const wxString& DataName,
+            long Offset,
+            long DefaultValue1=0,
+            long DefaultValue2=0,
+            int Priority=100);
+
+        /** \brief Returning type name */
+        virtual const wxString GetTypeName() { return _T("wxsTowLongProperty"); }
+
+    protected:
+
+        virtual void PGCreate(wxsPropertyContainer* Object,wxPropertyGridManager* Grid,wxPGId Parent);
+        virtual bool PGRead(wxsPropertyContainer* Object,wxPropertyGridManager* Grid, wxPGId Id,long Index);
+        virtual bool PGWrite(wxsPropertyContainer* Object,wxPropertyGridManager* Grid, wxPGId Id,long Index);
+        virtual bool XmlRead(wxsPropertyContainer* Object,TiXmlElement* Element);
+        virtual bool XmlWrite(wxsPropertyContainer* Object,TiXmlElement* Element);
+        virtual bool PropStreamRead(wxsPropertyContainer* Object,wxsPropertyStream* Stream);
+        virtual bool PropStreamWrite(wxsPropertyContainer* Object,wxsPropertyStream* Stream);
+
+    private:
+        long Offset;
+        long DefaultValue1;
+        long DefaultValue2;
+        bool DefValue;
+
+        wxString Value1Name;
+        wxString Value2Name;
+};
+
+/** \addtogroup ext_properties_macros
+ *  \{ */
+
+/** \brief Macro automatically declaring two long property
+ *  \param ClassName name of class holding this property
+ *  \param VarName name of wxsTwoLongData variable inside class
+ *  \param PGName name used in property grid
+ *  \param PGValue1Name name of value 1 property
+ *  \param PGValue2Name name of value 2 property
+ *  \param DataName name used in Xml / Data Streams
+ *  \param Default1 default value 1 (integer)
+ *  \param Default2 default value 2 (integer)
+ */
+#define WXS_TWOLONG(ClassName,VarName,PGName,PGValue1Name,PGValue2Name,DataName,Default1,Default2) \
+    { static wxsTowLongProperty _Property(PGName,PGValue1Name,PGValue2Name,DataName,wxsOFFSET(ClassName,VarName),Default1, Default2); \
+      Property(_Property); }
+
+/** \brief Macro automatically declaring two long property with custom priority
+ *  \param ClassName name of class holding this property
+ *  \param VarName name of wxsTwoLongData variable inside class
+ *  \param PGName name used in property grid
+ *  \param PGValue1Name name of value 1 property
+ *  \param PGValue2Name name of value 2 property
+ *  \param DataName name used in Xml / Data Streams
+ *  \param Default1 default value 1 (integer)
+ *  \param Default2 default value 2 (integer)
+ *  \param Priority priority of this property
+ */
+#define WXS_TWOLONG_P(ClassName,VarName,PGName,PGValue1Name,PGValue2Name,DataName,Default1,Default2,Priority) \
+    { static wxsTowLongProperty _Property(PGName,PGValue1Name,PGValue2Name,DataName,wxsOFFSET(ClassName,VarName),Default1, Default2,Priority); \
+      Property(_Property); }
+
+/** \} */
+
+#endif
