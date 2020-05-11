0f63defca29d0068da770d8ec74007787e3e2aed
Pecan Heber <pecan@codeblocks.org>
2020-05-11 18:21:54 +0000

    * Personality Manager - personality ask list should return only <personality>.conf names, excluding <personality>.some.other.conf, ie, exclude KeyBinder .conf files
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12075 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/personalitymanager.cpp
+++ src/sdk/personalitymanager.cpp
@@ -40,11 +40,16 @@ const wxString PersonalityManager::GetPersonality()
 
 const wxArrayString PersonalityManager::GetPersonalitiesList()
 {
-    wxArrayString list;
-    wxDir::GetAllFiles(ConfigManager::GetConfigFolder(), &list, _T("*.conf"), wxDIR_FILES);
+    wxArrayString list, allConf;
+    wxDir::GetAllFiles(ConfigManager::GetConfigFolder(), &allConf, _T("*.conf"), wxDIR_FILES);
 
-    for(size_t i = 0; i < list.GetCount(); ++i)
-        list[i] = wxFileName(list[i]).GetName();
+    for(size_t i = 0; i < allConf.GetCount(); ++i)
+    {
+        // return only <personality>.conf names, exclude <personality>.some.other.conf
+        if (wxFileName(allConf[i]).GetName().Contains(wxT(".")))
+            continue;
+        list.Add(wxFileName(allConf[i]).GetName());
+    }
 
     return list;
 }
