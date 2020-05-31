5a7bd1085d7cf8c206b5744e7df3f4880d94d4bf
T Petrov <tpetrov@codeblocks.org>
2020-05-31 15:54:12 +0000

    - ThreadSearch: Remove duplicate doxygen comments and some formatting changes
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12134 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/ThreadSearch/ThreadSearchViewManagerMessagesNotebook.h
+++ src/plugins/contrib/ThreadSearch/ThreadSearchViewManagerMessagesNotebook.h
@@ -25,42 +25,16 @@ class ThreadSearchLogger;
 class ThreadSearchViewManagerMessagesNotebook : public ThreadSearchViewManagerBase
 {
 public:
-    /** Constructor. */
     ThreadSearchViewManagerMessagesNotebook(ThreadSearchView* pThreadSearchView)
         : ThreadSearchViewManagerBase(pThreadSearchView), m_Bitmap(nullptr)
     {}
-
-    /** Destructor. */
     ~ThreadSearchViewManagerMessagesNotebook() override;
 
     eManagerTypes GetManagerType() override { return TypeMessagesNotebook; }
-
-    /** By default, view is not managed by the manager.
-      * This method adds view to manager if not already managed.
-      * No parameters because only m_pThreadSearchView is managed
-      * and given in constructor.
-      */
     void AddViewToManager() override;
-
-    /** By default, view is not managed by the manager.
-      * This method removes view from manager if managed.
-      * No parameters because only m_pThreadSearchView is managed
-      * and given in constructor.
-      * m_pThreadSearchView is not modified.
-      */
     void RemoveViewFromManager() override;
-
-    /** Return true if success. Fails if view is not managed.
-      * @param show : true => show, false => hide
-      * @return true if success.
-      */
     bool ShowView(bool show, bool preserveFocus) override;
-
-    /** Return true if view is visible.
-      * @return true if view is visible.
-      */
     bool IsViewShown() override;
-
     void Raise() override;
 private:
     wxBitmap *m_Bitmap;
