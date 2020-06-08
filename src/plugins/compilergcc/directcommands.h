f434b05e1eba0ae8195268e2208e674fda3943c5
bluehazzard <bluehazzard2@gmail.com>
2020-06-09 01:23:49 +0200

    Implement suggestions form obfuscated on ticket 953

--- src/plugins/compilergcc/directcommands.h
+++ src/plugins/compilergcc/directcommands.h
@@ -13,6 +13,8 @@ extern const wxString COMPILER_WAIT;
 extern const wxString COMPILER_WAIT_LINK;
 
 extern const wxString COMPILER_NOTE_ID_LOG;
+/// Print a NOTE log message to the build log, without advancing the progress counter
+extern const wxString COMPILER_ONLY_NOTE_ID_LOG;
 extern const wxString COMPILER_WARNING_ID_LOG;
 extern const wxString COMPILER_ERROR_ID_LOG;
 
@@ -59,6 +61,30 @@ class DirectCommands
         void         DepsSearchStart(ProjectBuildTarget* target) const;
         MyFilesArray GetProjectFilesSortedByWeight(ProjectBuildTarget* target, bool compile, bool link) const;
         void         AddCommandsToArray(const wxString& cmds, wxArrayString& array, bool isWaitCmd = false, bool isLinkCmd = false) const;
+
+        /// \brief Check if the command line is too long for the current running system and create a response file if necessary
+        ///
+        /// If the command line (executableCmd) used to execute a command is longer than the operating system limit
+        /// this function breaks the command line at the limit of the OS and generates a response file in the path folder.
+        /// Then it replaces the original command line with a constructed command line with the added response file and a pre-appended @.
+        /// This is the common marker for response files for many compilers.
+        /// The command is spitted, so that the final command with the response file does not exceed the command line length limit.
+        /// The filename of the response file is generated from the executable name (string part until the first space from left)
+        /// and the base name. This name has to be unique.
+        /// The length limit of the operating system can be overwritten with the CB_COMMAND_LINE_MAX_LENGTH defined during compile time
+        /// If it is not defined at compile time reasonable limits for windows and UNIX are used.
+        ///
+        /// For example:
+        ///  mingw32-gcc.exe -i this/is/in/the/lime -i this/is/longer/then/the/operating/system/supports
+        ///
+        /// gets transformed to
+        ///  mingw32-gcc.exe -i this/is/in/the/lime -i @path/to/response/file.respfile
+        ///
+        /// \param[in,out] executableCmd he command line input to check. Returns the modified command line if it was too long
+        /// \param[in,out] outputCommandArray The command queue. Some logging information is added so it can appear in the output log
+        /// \param[in] basename A base name, used to create a unique name for the response file
+        /// \param[in] path Base path where the response file is created
+        void CheckForToLongCommandLine(wxString& executableCmd, wxArrayString& outputCommandArray, const wxString& basename ,const wxString& path) const;
 
         int                       m_PageIndex;
         CompilerGCC*              m_pCompilerPlugin;
