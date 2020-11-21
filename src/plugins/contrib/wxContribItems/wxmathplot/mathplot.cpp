c5151676a0eb4a7c36f30b1fddba039f7a5870d2
T Petrov <tpetrov@codeblocks.org>
2020-11-21 17:53:43 +0000

    * wxContribItems: Remove warnings about deprecated wxPen and wxBrush styles in wxContribItems (ticket #1037, thanks  Miguel Gimenez)
    > The rest of the changes
    
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12238 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxContribItems/wxmathplot/mathplot.cpp
+++ src/plugins/contrib/wxContribItems/wxmathplot/mathplot.cpp
@@ -92,7 +92,7 @@ wxBitmap mpLayer::GetColourSquare(int side)
 {
     wxBitmap square(side, side, -1);
     wxColour filler = m_pen.GetColour();
-    wxBrush brush(filler, wxSOLID);
+    wxBrush brush(filler, wxBRUSHSTYLE_SOLID);
     wxMemoryDC dc;
     dc.SelectObject(square);
     dc.SetBackground(brush);
@@ -822,7 +822,7 @@ void mpScaleX::Plot(wxDC & dc, mpWindow & w)
                     else
                         dc.DrawLine( p, orgy, p, orgy+4);
                 } else { // draw grid dotted lines
-                    m_pen.SetStyle(wxDOT);
+                    m_pen.SetStyle(wxPENSTYLE_DOT);
                     dc.SetPen(m_pen);
                     if ((m_flags == mpALIGN_BOTTOM) && !m_drawOutsideMargins) {
                         dc.DrawLine( p, orgy+4, p, minYpx );
@@ -833,7 +833,7 @@ void mpScaleX::Plot(wxDC & dc, mpWindow & w)
                             dc.DrawLine( p, 0/*-w.GetScrY()*/, p, w.GetScrY() );
                         }
                     }
-                    m_pen.SetStyle(wxSOLID);
+                    m_pen.SetStyle(wxPENSTYLE_SOLID);
                     dc.SetPen(m_pen);
                 }
                 // Write ticks labels in s string
@@ -1059,7 +1059,7 @@ void mpScaleY::Plot(wxDC & dc, mpWindow & w)
                     dc.DrawLine( orgx-4, p, orgx, p); //( orgx, p, orgx+4, p);
                 }
             } else {
-                m_pen.SetStyle(wxDOT);
+                m_pen.SetStyle(wxPENSTYLE_DOT);
                 dc.SetPen( m_pen);
                 if ((m_flags == mpALIGN_LEFT) && !m_drawOutsideMargins) {
                     dc.DrawLine( orgx-4, p, endPx, p);
@@ -1070,7 +1070,7 @@ void mpScaleY::Plot(wxDC & dc, mpWindow & w)
                     dc.DrawLine( 0/*-w.GetScrX()*/, p, w.GetScrX(), p);
                         }
                 }
-                m_pen.SetStyle(wxSOLID);
+                m_pen.SetStyle(wxPENSTYLE_SOLID);
                 dc.SetPen( m_pen);
             }
             // Print ticks labels
@@ -1320,7 +1320,7 @@ void mpWindow::OnMouseMove(wxMouseEvent     &event)
         if (event.m_leftDown) {
             if (m_movingInfoLayer == NULL) {
                 wxClientDC dc(this);
-                wxPen pen(*wxBLACK, 1, wxDOT);
+                wxPen pen(*wxBLACK, 1, wxPENSTYLE_DOT);
                 dc.SetPen(pen);
                 dc.SetBrush(*wxTRANSPARENT_BRUSH);
                 dc.DrawRectangle(m_mouseLClick_X, m_mouseLClick_Y, event.GetX() - m_mouseLClick_X, event.GetY() - m_mouseLClick_Y);
