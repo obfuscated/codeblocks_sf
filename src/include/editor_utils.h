bfbb426c94c2d3e1fbab554747794533da3c2756
asmwarrior <asmwarrior@gmail.com>
2020-11-28 20:16:11 +0800

    - sdk: export editor_utils functions in codeblocks.dll

--- src/include/editor_utils.h
+++ src/include/editor_utils.h
@@ -6,15 +6,17 @@
 #ifndef CODEBLOCKS_EDITOR_HELPER_H
 #define CODEBLOCKS_EDITOR_HELPER_H
 
+#include "settings.h" // DLLIMPORT
+
 class wxScintilla;
 
 namespace cb
 {
 /// Function which is used to setup the markers for the folding margin in an editor control.
 /// The id is the value stored in editor:folding/indicator config
-void SetFoldingMarkers(wxScintilla *stc, int id);
+DLLIMPORT void SetFoldingMarkers(wxScintilla *stc, int id);
 
-void UnderlineFoldedLines(wxScintilla *stc, bool underline);
+DLLIMPORT void UnderlineFoldedLines(wxScintilla *stc, bool underline);
 } // namespace cb
 
 #endif // CODEBLOCKS_EDITOR_HELPER_H
