obfuscated/codeblocks_sf

commit 397c196568ccb7846e1bdea20d4b464de351d9d0

* wxSmith: Add wxDataViewCtrl, wxDataViewListCtrl and wxDataViewTreeCtrl widgets
git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12448 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/contrib/wxSmith/wxwidgets/defitems/Makefile.am
@@ -102,7 +102,12 @@ libwxsmith_wxwidgets_defitems_la_SOURCES = \
 ./wxsrichtextstylecomboctrl.cpp \
 ./wxsrichtextstylelistbox.cpp \
 ./wxsrichtextstylelistctrl.cpp \
-./wxsspinctrldouble.cpp
+./wxsspinctrldouble.cpp \
+./wxsdataviewctrl.cpp \
+./wxsdataviewlistctrl.cpp \
+./wxsdataviewtreectrl.cpp
+
+
 
 noinst_HEADERS = \
 ./wxsgauge.h \
@@ -199,5 +204,8 @@ noinst_HEADERS = \
 ./wxsrichtextstylelistbox.h \
 ./wxsrichtextstylelistctrl.h \
 ./wxsspinctrldouble.h
+./wxsdataviewctrl.h \
+./wxsdataviewtreectrl.h \
+./wxsdataviewlistctrl.h
 
 EXTRA_DIST = $(srcdir)/wxsmediactrl.*

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewctrl.cpp
@@ -1,0 +1,143 @@
+/*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* This code is based in part on wxsimagecombobox from the wxSmithImage plug-in,
+* copyright Ron Collins and released under the GPL.
+*
+* $Revision$
+* $Id$
+* $HeadURL$
+*/
+
+#include "wxsdataviewctrl.h"
+#include <wx/dataview.h>
+
+namespace
+{
+    wxsRegisterItem<wxsDataViewCtrl> Reg(_T("DataViewCtrl"), wxsTWidget, _T("Standard"), 10);
+
+    WXS_ST_BEGIN(wxsDataViewCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
+    WXS_ST_CATEGORY("wxwxDataViewCtrl")
+    WXS_ST(wxDV_SINGLE)
+    WXS_ST(wxDV_MULTIPLE)
+    WXS_ST(wxDV_ROW_LINES)
+    WXS_ST(wxDV_HORIZ_RULES)
+    WXS_ST(wxDV_VERT_RULES)
+    WXS_ST(wxDV_VARIABLE_LINE_HEIGHT)
+    WXS_ST(wxDV_NO_HEADER)
+    WXS_ST_DEFAULTS()
+    WXS_ST_END()
+
+    WXS_EV_BEGIN(wxsDataViewCtrlEvents)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_SELECTION_CHANGED, wxDataViewEvent, RowSelected)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_ACTIVATED, wxDataViewEvent, ItemActivated)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_START_EDITING, wxDataViewEvent, ItemStartEditing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_STARTED, wxDataViewEvent, ItemEditingStarted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_DONE, wxDataViewEvent, ItemEditingDone)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSING, wxDataViewEvent, ItemCollapsing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSED, wxDataViewEvent, ItemCollapsed)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDING, wxDataViewEvent, ItemExpanding)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDED, wxDataViewEvent, ItemExpanded)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_VALUE_CHANGED, wxDataViewEvent, ItemValueChanged)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_CONTEXT_MENU, wxDataViewEvent, ItemContextMenu)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_CLICK, wxDataViewEvent, HeaderClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_RIGHT_CLICK, wxDataViewEvent, HeaderRightClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_SORTED, wxDataViewEvent, ColSorted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_REORDERED, wxDataViewEvent, ColReordered)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_BEGIN_DRAG, wxDataViewEvent, ItemBeginDrag)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP_POSSIBLE, wxDataViewEvent, ItemDropPossible)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP, wxDataViewEvent, ItemDrop)
+
+    WXS_EV_END()
+}
+
+wxsDataViewCtrl::wxsDataViewCtrl(wxsItemResData *Data):
+    wxsWidget(
+        Data,
+        &Reg.Info,
+        wxsDataViewCtrlEvents,
+        wxsDataViewCtrlStyles)
+{
+
+}
+
+/*! \brief Create the initial control.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewCtrl::OnBuildCreatingCode()
+{
+
+    switch(GetLanguage())
+    {
+        case wxsCPP:
+            {
+                AddHeader(_T("<wx/dataview.h>"), GetInfo().ClassName, 0);
+                AddHeader(_T("<wx/dataview.h>"), _T("wxDataViewCtrl"), 0);
+                Codef(_T("%C(%W, %I, %P, %S, %T, %V);\n"));
+
+                // assign the image-list -- AFTER the image list has been built
+                BuildSetupWindowCode();
+                return;
+            }
+
+        case wxsUnknownLanguage: // fall-through
+        default:
+            {
+                wxsCodeMarks::Unknown(_T("wxsDataViewCtrl::OnBuildCreatingCode"), GetLanguage());
+            }
+    }
+}
+
+/*! \brief    Build the control preview.
+ *
+ * \param parent wxWindow*    The parent window.
+ * \param flags long          The control flags.
+ * \return wxObject*          The constructed control.
+ *
+ */
+wxObject *wxsDataViewCtrl::OnBuildPreview(wxWindow *Parent, long Flags)
+{
+    wxDataViewCtrl *preview = new wxDataViewCtrl(Parent, GetId(), Pos(Parent), Size(Parent), Style());
+
+    return SetupWindow(preview, Flags);
+}
+
+/*! \brief Enumerate the control's properties.
+ *
+ * \param flags long    The control flags.
+ * \return void
+ *
+ */
+void wxsDataViewCtrl::OnEnumWidgetProperties(cb_unused long Flags)
+{
+
+}
+
+/*! \brief Find all tools that are image lists and return their names.
+ *
+ * \param aNames wxArrayString&
+ * \return void
+ *
+ */
+void wxsDataViewCtrl::FindAllImageLists(wxArrayString &aNames)
+{
+
+}
+
+/*! \brief .Update the list of combo items to send to the wsxImageTreeEditorDlg.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewCtrl::UpdateTreeItemList()
+{
+
+}

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewlistctrl.cpp
@@ -1,0 +1,143 @@
+/*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* This code is based in part on wxsimagecombobox from the wxSmithImage plug-in,
+* copyright Ron Collins and released under the GPL.
+*
+* $Revision$
+* $Id$
+* $HeadURL$
+*/
+
+#include "wxsdataviewlistctrl.h"
+#include <wx/dataview.h>
+
+namespace
+{
+    wxsRegisterItem<wxsDataViewListCtrl> Reg(_T("DataViewListCtrl"), wxsTWidget, _T("Standard"), 10);
+
+    WXS_ST_BEGIN(wxsDataViewListCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
+    WXS_ST_CATEGORY("wxDataViewListCtrl")
+    WXS_ST(wxDV_SINGLE)
+    WXS_ST(wxDV_MULTIPLE)
+    WXS_ST(wxDV_ROW_LINES)
+    WXS_ST(wxDV_HORIZ_RULES)
+    WXS_ST(wxDV_VERT_RULES)
+    WXS_ST(wxDV_VARIABLE_LINE_HEIGHT)
+    WXS_ST(wxDV_NO_HEADER)
+    WXS_ST_DEFAULTS()
+    WXS_ST_END()
+
+    WXS_EV_BEGIN(wxsDataViewListCtrlEvents)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_SELECTION_CHANGED, wxDataViewEvent, RowSelected)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_ACTIVATED, wxDataViewEvent, ItemActivated)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_START_EDITING, wxDataViewEvent, ItemStartEditing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_STARTED, wxDataViewEvent, ItemEditingStarted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_DONE, wxDataViewEvent, ItemEditingDone)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSING, wxDataViewEvent, ItemCollapsing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSED, wxDataViewEvent, ItemCollapsed)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDING, wxDataViewEvent, ItemExpanding)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDED, wxDataViewEvent, ItemExpanded)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_VALUE_CHANGED, wxDataViewEvent, ItemValueChanged)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_CONTEXT_MENU, wxDataViewEvent, ItemContextMenu)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_CLICK, wxDataViewEvent, HeaderClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_RIGHT_CLICK, wxDataViewEvent, HeaderRightClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_SORTED, wxDataViewEvent, ColSorted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_REORDERED, wxDataViewEvent, ColReordered)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_BEGIN_DRAG, wxDataViewEvent, ItemBeginDrag)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP_POSSIBLE, wxDataViewEvent, ItemDropPossible)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP, wxDataViewEvent, ItemDrop)
+
+    WXS_EV_END()
+}
+
+wxsDataViewListCtrl::wxsDataViewListCtrl(wxsItemResData *Data):
+    wxsWidget(
+        Data,
+        &Reg.Info,
+        wxsDataViewListCtrlEvents,
+        wxsDataViewListCtrlStyles)
+{
+
+}
+
+/*! \brief Create the initial control.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewListCtrl::OnBuildCreatingCode()
+{
+
+    switch(GetLanguage())
+    {
+        case wxsCPP:
+            {
+                AddHeader(_T("<wx/dataview.h>"), GetInfo().ClassName, 0);
+                AddHeader(_T("<wx/dataview.h>"), _T("wxDataViewCtrl"), 0);
+                Codef(_T("%C(%W, %I, %P, %S, %T, %V);\n"));
+
+                // assign the image-list -- AFTER the image list has been built
+                BuildSetupWindowCode();
+                return;
+            }
+
+        case wxsUnknownLanguage: // fall-through
+        default:
+            {
+                wxsCodeMarks::Unknown(_T("wxsDataViewListCtrl::OnBuildCreatingCode"), GetLanguage());
+            }
+    }
+}
+
+/*! \brief    Build the control preview.
+ *
+ * \param parent wxWindow*    The parent window.
+ * \param flags long          The control flags.
+ * \return wxObject*          The constructed control.
+ *
+ */
+wxObject *wxsDataViewListCtrl::OnBuildPreview(wxWindow *Parent, long Flags)
+{
+    wxDataViewCtrl *preview = new wxDataViewCtrl(Parent, GetId(), Pos(Parent), Size(Parent), Style());
+
+    return SetupWindow(preview, Flags);
+}
+
+/*! \brief Enumerate the control's properties.
+ *
+ * \param flags long    The control flags.
+ * \return void
+ *
+ */
+void wxsDataViewListCtrl::OnEnumWidgetProperties(cb_unused long Flags)
+{
+
+}
+
+/*! \brief Find all tools that are image lists and return their names.
+ *
+ * \param aNames wxArrayString&
+ * \return void
+ *
+ */
+void wxsDataViewListCtrl::FindAllImageLists(wxArrayString &aNames)
+{
+
+}
+
+/*! \brief .Update the list of combo items to send to the wsxImageTreeEditorDlg.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewListCtrl::UpdateTreeItemList()
+{
+
+}

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewtreectrl.cpp
@@ -1,0 +1,143 @@
+/*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* This code is based in part on wxsimagecombobox from the wxSmithImage plug-in,
+* copyright Ron Collins and released under the GPL.
+*
+* $Revision$
+* $Id$
+* $HeadURL$
+*/
+
+#include "wxsdataviewtreectrl.h"
+#include <wx/dataview.h>
+
+namespace
+{
+    wxsRegisterItem<wxsDataViewTreeCtrl> Reg(_T("DataViewTreeCtrl"), wxsTWidget, _T("Standard"), 10);
+
+    WXS_ST_BEGIN(wxsDataViewTreeCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
+    WXS_ST_CATEGORY("wxDataViewTreeCtrl")
+    WXS_ST(wxDV_SINGLE)
+    WXS_ST(wxDV_MULTIPLE)
+    WXS_ST(wxDV_ROW_LINES)
+    WXS_ST(wxDV_HORIZ_RULES)
+    WXS_ST(wxDV_VERT_RULES)
+    WXS_ST(wxDV_VARIABLE_LINE_HEIGHT)
+    WXS_ST(wxDV_NO_HEADER)
+    WXS_ST_DEFAULTS()
+    WXS_ST_END()
+
+    WXS_EV_BEGIN(wxsDataViewTreeCtrlEvents)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_SELECTION_CHANGED, wxDataViewEvent, RowSelected)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_ACTIVATED, wxDataViewEvent, ItemActivated)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_START_EDITING, wxDataViewEvent, ItemStartEditing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_STARTED, wxDataViewEvent, ItemEditingStarted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EDITING_DONE, wxDataViewEvent, ItemEditingDone)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSING, wxDataViewEvent, ItemCollapsing)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_COLLAPSED, wxDataViewEvent, ItemCollapsed)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDING, wxDataViewEvent, ItemExpanding)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_EXPANDED, wxDataViewEvent, ItemExpanded)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_VALUE_CHANGED, wxDataViewEvent, ItemValueChanged)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_CONTEXT_MENU, wxDataViewEvent, ItemContextMenu)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_CLICK, wxDataViewEvent, HeaderClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_HEADER_RIGHT_CLICK, wxDataViewEvent, HeaderRightClick)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_SORTED, wxDataViewEvent, ColSorted)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_COLUMN_REORDERED, wxDataViewEvent, ColReordered)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_BEGIN_DRAG, wxDataViewEvent, ItemBeginDrag)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP_POSSIBLE, wxDataViewEvent, ItemDropPossible)
+    WXS_EVI_STD_TYPE(EVT_DATAVIEW_ITEM_DROP, wxDataViewEvent, ItemDrop)
+
+    WXS_EV_END()
+}
+
+wxsDataViewTreeCtrl::wxsDataViewTreeCtrl(wxsItemResData *Data):
+    wxsWidget(
+        Data,
+        &Reg.Info,
+        wxsDataViewTreeCtrlEvents,
+        wxsDataViewTreeCtrlStyles)
+{
+
+}
+
+/*! \brief Create the initial control.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewTreeCtrl::OnBuildCreatingCode()
+{
+
+    switch(GetLanguage())
+    {
+        case wxsCPP:
+            {
+                AddHeader(_T("<wx/dataview.h>"), GetInfo().ClassName, 0);
+                AddHeader(_T("<wx/dataview.h>"), _T("wxDataViewTreeCtrl"), 0);
+                Codef(_T("%C(%W, %I, %P, %S, %T, %V);\n"));
+
+                // assign the image-list -- AFTER the image list has been built
+                BuildSetupWindowCode();
+                return;
+            }
+
+        case wxsUnknownLanguage: // fall-through
+        default:
+            {
+                wxsCodeMarks::Unknown(_T("wxsDataViewTreeCtrl::OnBuildCreatingCode"), GetLanguage());
+            }
+    }
+}
+
+/*! \brief    Build the control preview.
+ *
+ * \param parent wxWindow*    The parent window.
+ * \param flags long          The control flags.
+ * \return wxObject*          The constructed control.
+ *
+ */
+wxObject *wxsDataViewTreeCtrl::OnBuildPreview(wxWindow *Parent, long Flags)
+{
+    wxDataViewCtrl *preview = new wxDataViewCtrl(Parent, GetId(), Pos(Parent), Size(Parent), Style());
+
+    return SetupWindow(preview, Flags);
+}
+
+/*! \brief Enumerate the control's properties.
+ *
+ * \param flags long    The control flags.
+ * \return void
+ *
+ */
+void wxsDataViewTreeCtrl::OnEnumWidgetProperties(cb_unused long Flags)
+{
+
+}
+
+/*! \brief Find all tools that are image lists and return their names.
+ *
+ * \param aNames wxArrayString&
+ * \return void
+ *
+ */
+void wxsDataViewTreeCtrl::FindAllImageLists(wxArrayString &aNames)
+{
+
+}
+
+/*! \brief .Update the list of combo items to send to the wsxImageTreeEditorDlg.
+ *
+ * \return void
+ *
+ */
+void wxsDataViewTreeCtrl::UpdateTreeItemList()
+{
+
+}

src/plugins/contrib/wxSmith/wxSmith_wx30-unix.cbp
@@ -405,6 +405,24 @@
 		<Unit filename="wxwidgets/defitems/wxscustomwidget.h">
 			<Option target="wxSmithLib" />
 		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.cpp" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.h" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
 		<Unit filename="wxwidgets/defitems/wxsdatepickerctrl.cpp">
 			<Option target="wxSmithLib" />
 		</Unit>

src/plugins/contrib/wxSmith/wxSmith_wx31.cbp
@@ -413,6 +413,24 @@
 		<Unit filename="wxwidgets/defitems/wxscustomwidget.h">
 			<Option target="wxSmithLib" />
 		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.cpp" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.h" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
 		<Unit filename="wxwidgets/defitems/wxsdatepickerctrl.cpp">
 			<Option target="wxSmithLib" />
 		</Unit>

src/plugins/contrib/wxSmith/wxSmith_wx31_64.cbp
@@ -414,6 +414,24 @@
 		<Unit filename="wxwidgets/defitems/wxscustomwidget.h">
 			<Option target="wxSmithLib" />
 		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewctrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.cpp" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewlistctrl.h" >
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.cpp">
+			<Option target="wxSmithLib" />
+		</Unit>
+		<Unit filename="wxwidgets/defitems/wxsdataviewtreectrl.h">
+			<Option target="wxSmithLib" />
+		</Unit>
 		<Unit filename="wxwidgets/defitems/wxsdatepickerctrl.cpp">
 			<Option target="wxSmithLib" />
 		</Unit>
@@ -1252,7 +1270,6 @@
 			<Option target="wxSmithLib" />
 		</Unit>
 		<Extensions>
-			<code_completion />
 			<wxsmith version="1">
 				<gui name="wxWidgets" src="" main="" init_handlers="necessary" language="CPP" />
 				<resources>
@@ -1278,8 +1295,6 @@
 					<wxScrollingDialog wxs="wxsmith/wxsDeleteItemRes.wxs" src="wxwidgets/wxsdeleteitemres.cpp" hdr="wxwidgets/wxsdeleteitemres.h" fwddecl="0" i18n="1" name="wxsDeleteItemRes" language="CPP" />
 				</resources>
 			</wxsmith>
-			<envvars />
-			<debugger />
 		</Extensions>
 	</Project>
 </CodeBlocks_project_file>

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewctrl.h
@@ -1,0 +1,46 @@
+/** \file wxstreectrl.h
+*
+* This file is part of wxSmith plugin for Code::Blocks Studio
+* Copyright (C) 2006-2007  Bartlomiej Swiecki
+*
+* wxSmith is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* wxSmith is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with wxSmith. If not, see <http://www.gnu.org/licenses/>.
+*
+* $Revision$
+* $Id$
+* $HeadURL$
+*/
+
+#ifndef WXSDATAVIEWCTRL_H
+#define WXSDATAVIEWCTRL_H
+
+#include "../wxswidget.h"
+
+/** \brief Class for wxsDataViewCtrl widget */
+class wxsDataViewCtrl: public wxsWidget
+{
+    public:
+
+        wxsDataViewCtrl(wxsItemResData* Data);
+
+    private:
+
+        virtual void OnBuildCreatingCode();
+        virtual wxObject* OnBuildPreview(wxWindow* Parent,long Flags);
+        virtual void OnEnumWidgetProperties(long Flags);
+        void FindAllImageLists(wxArrayString &aNames);
+        void UpdateTreeItemList(void);
+
+};
+
+#endif

commit 0e2703661d7504f093fc0458ddb1f5da4c23fa92

* wxSmith: Fix regressions introduced in r12448
> Order wxSmith toolbar items alphabetically
> Fix makefile 

git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12449 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/contrib/wxSmith/wxwidgets/defitems/Makefile.am
@@ -203,7 +203,7 @@ noinst_HEADERS = \
 ./wxsrichtextstylecomboctrl.h \
 ./wxsrichtextstylelistbox.h \
 ./wxsrichtextstylelistctrl.h \
-./wxsspinctrldouble.h
+./wxsspinctrldouble.h \
 ./wxsdataviewctrl.h \
 ./wxsdataviewtreectrl.h \
 ./wxsdataviewlistctrl.h

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewctrl.cpp
@@ -20,7 +20,7 @@
 
 namespace
 {
-    wxsRegisterItem<wxsDataViewCtrl> Reg(_T("DataViewCtrl"), wxsTWidget, _T("Standard"), 10);
+    wxsRegisterItem<wxsDataViewCtrl> Reg(_T("DataViewCtrl"), wxsTWidget, _T("Standard"), 278);
 
     WXS_ST_BEGIN(wxsDataViewCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
     WXS_ST_CATEGORY("wxwxDataViewCtrl")

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewlistctrl.cpp
@@ -20,7 +20,7 @@
 
 namespace
 {
-    wxsRegisterItem<wxsDataViewListCtrl> Reg(_T("DataViewListCtrl"), wxsTWidget, _T("Standard"), 10);
+    wxsRegisterItem<wxsDataViewListCtrl> Reg(_T("DataViewListCtrl"), wxsTWidget, _T("Standard"), 277);
 
     WXS_ST_BEGIN(wxsDataViewListCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
     WXS_ST_CATEGORY("wxDataViewListCtrl")

src/plugins/contrib/wxSmith/wxwidgets/defitems/wxsdataviewtreectrl.cpp
@@ -20,7 +20,7 @@
 
 namespace
 {
-    wxsRegisterItem<wxsDataViewTreeCtrl> Reg(_T("DataViewTreeCtrl"), wxsTWidget, _T("Standard"), 10);
+    wxsRegisterItem<wxsDataViewTreeCtrl> Reg(_T("DataViewTreeCtrl"), wxsTWidget, _T("Standard"), 276);
 
     WXS_ST_BEGIN(wxsDataViewTreeCtrlStyles, _T("wxDV_DEFAULT_STYLE"))
     WXS_ST_CATEGORY("wxDataViewTreeCtrl")